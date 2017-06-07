9e6607bcde0da23527e419368101d98e7701a9e5
vivian <vivian@eff.org>
2017-06-07 14:16:16 -0400

    Transform columns by regex

--- visidata/addons/regex.py
+++ visidata/addons/regex.py
@@ -1,4 +1,5 @@
 command(';', 'splitColumnByRegex(columns, cursorColIndex, cursorCol, cursorValue, input("split regex: "))', 'split column by regex')
+command('.', 'columns.insert(cursorColIndex+1, regexTransform(cursorCol, input("transform column by regex: ")))', 'transform column by regex')
 
 def splitColumnByRegex(columns, colIndex, origcol, exampleVal, regexstr):
     regex = re.compile(regexstr)  # TODO: regex_flags
@@ -10,3 +11,9 @@ def splitColumnByRegex(columns, colIndex, origcol, exampleVal, regexstr):
                 getter=lambda row, regex=regex, i=i, origcol=origcol: regex.search(origcol.getValue(row)).group(i+1))
 
         columns.insert(colIndex+i+1, c)
+
+def regexTransform(col, instr):
+    before, after = instr.split('/')
+    newCol = Column(col.name + '_new',
+                    getter=lambda row, col=col, before=before, after=after: re.sub(before, after, col.getValue(row)))
+    return newCol
