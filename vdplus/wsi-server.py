f8c385c9461a7faa5767b1a54704d8b31c9e970a
Saul Pwanson <code@saul.pw>
2017-06-08 15:33:06 -0400

    Small refactor for generic_handler

--- vdplus/wsi-server.py
+++ vdplus/wsi-server.py
@@ -16,6 +16,11 @@ planets = [
 # 2nd-time ...                           -> compare creds, return new sessionid
 #  /other_url sessionid                  -> gets user from sessionid
 
+class MyException(Exception):
+    def __init__(self, errcode, text):
+        super().__init__(text)
+        self.errcode = errcode
+
 class WSIServer(http.server.HTTPServer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -24,50 +29,52 @@ class WSIServer(http.server.HTTPServer):
         self.sessions = {}
 
 class WSIHandler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        if self.path == '/join':
-            length = int(self.headers['content-length'])
-            field_data = self.rfile.read(length)
-            fields = urllib.parse.parse_qs(field_data)
-            playername = fields[b'username'][0]
-            if playername in self.server.players:
-                md5_password, sessionid = self.server.players[playername]
-                if fields[b'password'][0] == md5_password:
-                    self.send_response(200)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
-                    self.wfile.write(sessionid.encode('utf-8'))
-                else:
-                    self.send_response(403)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
-                    self.wfile.write(('Sorry, wrong password').encode('utf-8'))
-            else:
-                if self.server.game_started:
-                    self.send_response(403)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
-                    self.wfile.write(('Sorry, game has already started, no new players').encode('utf-8'))
-                else:
-                    sessionid = uuid.uuid1().hex
-                    self.send_response(200)
-                    self.send_header('Content-type', 'text/plain')
-                    self.end_headers()
-                    self.server.players[playername] = (fields[b'password'][0], sessionid)
-                    self.server.sessions[sessionid] = playername
-                    self.wfile.write(sessionid.encode('utf-8'))
-
-
-
-    def do_GET(self):
+    def generic_handler(self, reqtype, *args, **kwargs):
         toplevel = self.path.split('/')[1]
         if toplevel:
-            self.send_response(200)
-            self.send_header('Content-type', 'text/json')
-            self.end_headers()
-            query = urllib.parse.parse_qs(self.path)
-            ret = getattr(self, 'GET_' + toplevel)(query)
-            self.wfile.write(json.dumps(ret).encode('utf-8'))
+            try:
+                ret = getattr(self, '%s_%s' % (reqtype, toplevel))(*args, **kwargs)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(ret).encode('utf-8'))
+            except MyException as e:
+                self.send_response(e.errcode)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(str(e).encode('utf-8'))
+            except:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+
+    def do_GET(self):
+        query = urllib.parse.parse_qs(self.path)
+        self.generic_handler('GET', query)
+
+    def do_POST(self):
+        length = int(self.headers['content-length'])
+        field_data = self.rfile.read(length)
+        fields = urllib.parse.parse_qs(field_data)
+        fields = dict((k.decode('utf-8'), v) for k, v in fields.items())
+        self.generic_handler('POST', fields)
+
+    def POST_join(self, fields):
+        playername = fields['username'][0]
+        if playername in self.server.players:
+            md5_password, sessionid = self.server.players[playername]
+            if fields['password'][0] == md5_password:
+                return sessionid
+            else:
+                raise MyException(403, 'Sorry, wrong password')
+        else:
+            if self.server.game_started:
+                raise MyException(403, 'Sorry, game has already started, no new players')
+            else:
+                sessionid = uuid.uuid1().hex
+                self.server.players[playername] = (fields['password'][0], sessionid)
+                self.server.sessions[sessionid] = playername
+                return sessionid
 
     def GET_planets(self, query):
         return planets
