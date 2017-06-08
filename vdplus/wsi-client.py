eba9a851e51499afd99a3d543b4bb6aad00aff24
Saul Pwanson <code@saul.pw>
2017-06-08 17:20:18 -0400

    Add Players sheet and basic game start functionality

--- vdplus/wsi-client.py
+++ vdplus/wsi-client.py
@@ -4,25 +4,61 @@ import sys
 import requests
 import getpass
 import hashlib
+import builtins
 
-import visidata
+from visidata import *
 
-game_url = sys.argv[1]
+class WSIClient:
+    def __init__(self, url):
+        self.sessionid = None
+        self.server_url = url
 
-username = input('player name: ')
-password = getpass.getpass('password: ')
+    def login(self):  # before curses init
+        username = builtins.input('player name: ')
+        password = getpass.getpass('password: ')
 
-session = requests.Session()
-data = { 'username': username, 'password': hashlib.md5(password.encode('utf-8')).hexdigest() }
+        data = { 'username': username, 'password': hashlib.md5(password.encode('utf-8')).hexdigest() }
 
-r = session.post(game_url + '/join', data=data)
+        r = requests.post(self.server_url + '/join', data=data)
 
-if r.status_code == 200:
-    sessionid = r.text
-    r = session.get(game_url + '/planets', data={'session':sessionid})
-    print(r.text)
-else:
-    status('[%s] %s' % (r.status_code, r.text))
+        if r.status_code == 200:
+            self.sessionid = r.text
+        else:
+            error('[%s] %s' % (r.status_code, r.text))
+
+    def get(self, path):
+        if not self.sessionid:
+            error('not logged in')
+
+        r = requests.get(self.server_url + path, params={'session':self.sessionid})
+
+        if r.status_code != 200:
+            error('[%s] %s' % (r.status_code, r.text))
+
+        return r
 
 
-#visidata.run()
+class PlayersSheet(Sheet):
+    def __init__(self):
+        super().__init__('players')
+
+        self.columns = [
+            ColumnItem('name', 1),
+            ColumnItem('ready', 3),
+        ]
+        self.command(ENTER, 'status(g_client.get("/ready").text); reload()', 'Indicate ready to start')
+
+        self.colorizers.append(lambda sheet,col,row,value: row and (row[2], 8))
+
+    def reload(self):
+        r = g_client.get('/players')
+        self.rows = r.json()
+
+g_players = PlayersSheet()
+g_client = WSIClient(sys.argv[1])
+
+if __name__ == '__main__':
+    g_client.login()
+    set_global('g_client', g_client)
+    run([g_players])
+
--- vdplus/wsi-server.py
+++ vdplus/wsi-server.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python3
 
+import sys
+import json
 import http.server
 import urllib.parse
 import uuid
-import json
+import random
 
-planets = [
-# name, x, y, prod, killpct, owner, nships
-    ['A', 5, 5, 10, 40, None, 10],
-    ['B', 15, 15, 10, 40, None, 10],
-    ['C', 20, 20, 10, 40, None, 10],
-]
 
-# 1st-time /join username md5(password)  -> create player, return sessionid
-# 2nd-time ...                           -> compare creds, return new sessionid
-#  /other_url sessionid                  -> gets user from sessionid
+player_colors = 'green yellow cyan magenta red blue'.split()
+planet_names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+map_width = 10
+map_height = 10
+rand = random.randrange
 
-class MyException(Exception):
+class Player:
+    def __init__(self, number, name, md5_password, sessionid):
+        self.number = number
+        self.name = name
+        self.md5_password = md5_password
+        self.sessionid = sessionid
+        self.ready = False
+
+    def as_tuple(self):
+        return (self.number, self.name, player_colors[self.number], self.ready)
+
+
+class Planet:
+    def __init__(self, name, x, y, prod, killpct, owner=None):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.prod = prod
+        self.killpct = killpct
+        self.owner = owner
+        self.nships = prod
+
+    def as_tuple(self):
+        return (self.name, self.x, self.y, self.prod, self.killpct, self.owner, self.nships)
+
+class HTTPException(Exception):
     def __init__(self, errcode, text):
         super().__init__(text)
         self.errcode = errcode
@@ -25,63 +48,109 @@ class WSIServer(http.server.HTTPServer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.game_started = False
-        self.players = {}
+        self.players = {}   # [playername] -> Player()
         self.sessions = {}
+        self.planets = {}
+
+    def start_game(self):
+        self.generate_planets()
+
+    @property
+    def started(self):
+        return bool(self.planets)
+
+    def generate_planets(self):
+        # name, x, y, prod, killpct, owner, nships
+        nplayers = len(self.players)
+        for i, (name, pl) in enumerate(self.players.items()):
+            planet_name = planet_names[i]
+            self.planets[name] = Planet(name, rand(map_width), rand(map_height), 10, 40, pl)
+
+        for name in planet_names[nplayers:]:
+            self.planets[name] = Planet(name, rand(map_width), rand(map_height), rand(10), rand(40))
+
 
 class WSIHandler(http.server.BaseHTTPRequestHandler):
-    def generic_handler(self, reqtype, *args, **kwargs):
-        toplevel = self.path.split('/')[1]
+    def generic_handler(self, reqtype, path, *args, **kwargs):
+        toplevel = path.split('/')[1]
         if toplevel:
             try:
                 ret = getattr(self, '%s_%s' % (reqtype, toplevel))(*args, **kwargs)
                 self.send_response(200)
                 self.send_header('Content-type', 'text/json')
                 self.end_headers()
-                self.wfile.write(json.dumps(ret).encode('utf-8'))
-            except MyException as e:
+                if isinstance(ret, list) or isinstance(ret, dict):
+                    ret = json.dumps(ret)
+
+                self.wfile.write(ret.encode('utf-8'))
+            except HTTPException as e:
                 self.send_response(e.errcode)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
                 self.wfile.write(str(e).encode('utf-8'))
-            except:
+            except Exception as e:
+                import traceback
+                print(traceback.format_exc())
                 self.send_response(404)
                 self.send_header('Content-type', 'text/plain')
                 self.end_headers()
+                self.wfile.write(str(e).encode('utf-8'))
 
     def do_GET(self):
-        query = urllib.parse.parse_qs(self.path)
-        self.generic_handler('GET', query)
+        parsed_url = urllib.parse.urlparse(self.path)
+        query = urllib.parse.parse_qs(parsed_url.query)
+        sessionid = query['session'][0]
+        pl = self.server.sessions.get(sessionid)
+        if not pl:
+            raise HTTPException(403, 'Invalid sessionid')
+
+        self.generic_handler('GET', parsed_url.path, pl, query)
 
     def do_POST(self):
         length = int(self.headers['content-length'])
         field_data = self.rfile.read(length)
         fields = urllib.parse.parse_qs(field_data)
         fields = dict((k.decode('utf-8'), v) for k, v in fields.items())
-        self.generic_handler('POST', fields)
+        self.generic_handler('POST', self.path, fields)
 
     def POST_join(self, fields):
-        playername = fields['username'][0]
+        playername = fields['username'][0].decode('utf-8')
         if playername in self.server.players:
-            md5_password, sessionid = self.server.players[playername]
-            if fields['password'][0] == md5_password:
-                return sessionid
+            pl = self.server.players[playername]
+            if fields['password'][0] == pl.md5_password:
+                return pl.sessionid
             else:
-                raise MyException(403, 'Sorry, wrong password')
+                raise HTTPException(403, 'Sorry, wrong password')
         else:
             if self.server.game_started:
-                raise MyException(403, 'Sorry, game has already started, no new players')
+                raise HTTPException(403, 'Sorry, game has already started, no new players')
             else:
                 sessionid = uuid.uuid1().hex
-                self.server.players[playername] = (fields['password'][0], sessionid)
-                self.server.sessions[sessionid] = playername
+                pl = Player(len(self.server.players), playername, fields['password'][0], sessionid)
+                self.server.players[playername] = pl
+                self.server.sessions[sessionid] = pl
                 return sessionid
 
-    def GET_planets(self, query):
-        return planets
+    def GET_ready(self, pl, query):
+        pl.ready = True
+        if len(self.server.players) > 1 and all(pl.ready for pl in self.server.players.values()):
+            self.server.start_game()
+            return 'game started'
+        return 'ok'
 
-server = WSIServer(('', 8080), WSIHandler)
+    def GET_players(self, pl, query):
+        return [x.as_tuple() for x in self.server.players.values()]
 
-print("http://localhost:8080")
-server.serve_forever()
+    def GET_planets(self, pl, query):
+        return [x.as_tuple() for x in self.server.planets.values()]
 
+
+def main():
+    server = WSIServer(('', 8080), WSIHandler)
+
+    print("http://localhost:8080")
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
 
