629918e5d3874666eaefb774ea802bfc6edef6e2
Your Name <brannerchinese@users.noreply.github.com>
2017-06-19 10:54:10 -0400

    Merge with most recent changes to branch develop

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __version__ = '0.61'
 
 setup(name='visidata',
       version=__version__,
-      install_requires=['openpyxl', 'python-dateutil'],
+      install_requires='python-dateutil openpyxl'.split(),
       description='curses interface for exploring and arranging tabular data',
       long_description=open('README.md').read(),
       author='Saul Pwanson',
