4cd701caa7315595aaa78e437eaa82d97e050a28
Saul Pwanson <code@saul.pw>
2017-08-24 02:25:36 +0000

    WIP: merge view

--- merge.py
+++ merge.py
@@ -21,6 +21,8 @@ from git import GitSheet
 #     c) if '1' stages a grand reversion of everything local
 #     d) then '4' should bring the index back to the same exact state as after (b)
 
+command('0', 'vd.push(TextSheet("statuses", vd.statusHistory))', '')
+
 def _parseStartCount(s):
     sc = s.split(',')
     if len(sc) == 2: return sc
@@ -71,15 +73,68 @@ def parseDiff(left, right, lines):
         else:
             status(line)
 
+# generates leftlinenum, rightlinenum, leftline, rightline
+def parseHunk(gh):
+    linePairs = []
+    nextDelIdx = None
+    leftlinenum = gh.leftstart
+    rightlinenum = gh.rightstart  # start of linePairs
+
+    for line in gh.difflines:
+        typech = line[0]
+        line = line[1:]
+        if typech == ' ':
+            for a, b in linePairs:
+                r = [None, None, a, b]
+                if a:
+                    leftlinenum += 1
+                    r[0] = leftlinenum
+                if b:
+                    rightlinenum += 1
+                    r[1] = rightlinenum
+                yield r
+
+            leftlinenum += 1
+            rightlinenum += 1
+            yield (leftlinenum, rightlinenum, line, line)
+            nextDelIdx = None
+            linePairs = []
+        elif typech == '-':
+            if nextDelIdx is None:
+                assert not linePairs
+                nextDelIdx = 0
+            linePairs.append([line, None])
+        elif typech == '+':
+            if nextDelIdx is not None:
+                if nextDelIdx < len(linePairs):
+                    linePairs[nextDelIdx][1] = line
+                    nextDelIdx += 1
+                    continue
+            leftlinenum += 0.0001
+            rightlinenum += 1
+            yield leftlinenum, rightlinenum, None, line
+            nextDelIdx = None
+
+    for a, b in linePairs:
+        r = [None, None, a, b]
+        if a:
+            leftlinenum += 1
+            r[0] = leftlinenum
+        if b:
+            rightlinenum += 1
+            r[1] = rightlinenum
+        yield r
 
 class GitMerge(GitSheet):
     def __init__(self, gf):
         super().__init__(str(gf)+'_merge', gf)
         self.columns = [
-            Column('1/remote', width=10, getter=lambda r: r[0][1]),
-            Column('2/HEAD', getter=lambda r: r[1][1]),
-            Column('3/index', getter=lambda r: r[2][1]),
-            Column('4/working', getter=lambda r: r[3][1]),
+            Column('#:HEAD', getter=lambda r: r[0]),
+            Column('#:index', getter=lambda r: r[1]),
+            Column('#:working', getter=lambda r: r[2]),
+            Column('1:HEAD', getter=lambda r: r[3]),
+            Column('2:index', getter=lambda r: r[4]),
+            Column('3:working', getter=lambda r: r[5]),
         ]
 
         self.command('1', '', '')
@@ -88,17 +143,81 @@ class GitMerge(GitSheet):
         self.command('4', '', '')
 
 #        self.addColorizer('col', 4, lambda s,c,r,v: 'green' if c is s.columns[2] else None)
-        self.addColorizer('cell', 5, self.colorDiffCell)
+#        self.addColorizer('cell', 5, self.colorDiffCell)
 
+    # green for index cells not in HEAD; or working cells not in index
+    # yellow for lines with content differing in both
+    # red for index cells not in working; or HEAD cells not in index
     @staticmethod
     def colorDiffCell(sheet, col, row, val):
-        if val != row[1][1]:       # diff from HEAD
-            if val == row[2][1]:   # but same as index
-                return 'green'  #   will be committed
-            else:
-                return 'red'    # changed and not in index
+        if col is sheet.columns[2]: # index
+            if row[1] is None:     # index not in HEAD
+                return 'green'
+            elif row[3] is None:   # index not in working
+                return 'red'
+            elif row[2] != row[1] or row[2] != row[3]:
+                return 'yellow'
+        elif col is sheet.columns[3]:  # working
+            if row[2] is None:     # working not in index
+                return 'green'
+            elif row[3] != row[2]:
+                return 'yellow'
+        elif col is sheet.columns[1]:
+            if row[2] is None:     # HEAD not in index
+                return 'red'
+            elif row[2] != row[1]:
+                return 'yellow'
 
     def reload(self):
+        def _getIndex(rows, srcnum, linenum):
+            for i, r in enumerate(rows):
+                assert r[srcnum] is not None
+                if r[srcnum] == linenum:
+                    return i
+                elif r[srcnum] > linenum:
+                    break
+
+            r = [None, None, None, None, None, None]
+            r[srcnum] = linenum
+            rows.insert(i, r)
+            return i
+
+        fn = self.source.filename
+
+        self.rows = []
+        for i, line in enumerate(git_lines('show', 'HEAD:' + fn)):
+            self.rows.append([i+1, None, None, line, None, None])
+
+        for gh in parseDiff(0, 1, git_lines('diff', '--patch', '--cached', '--no-color', '--no-prefix', fn)):
+#            a = _getIndex(self.rows, gh.left, gh.leftstart)
+#            b = _getIndex(self.rows, gh.left, gh.leftstart + gh.leftcount)
+            for leftlinenum, rightlinenum, leftline, rightline in parseHunk(gh):
+                r = self.rows[_getIndex(self.rows, gh.left, leftlinenum)]
+                r[gh.right] = rightlinenum
+                r[gh.right+3] = rightline
+                assert r[gh.left+3] == leftline, (r[gh.left+3], leftline)
+
+#        parseDiff(1, 2, git_lines('diff', '--patch', '--no-color', '--no-prefix', fn)))
+        '''
+        for gh in hunks:
+            for i in range(lastLines[gh.left], gh.leftstart):
+
+
+
+            lastLines[gh.left] = gh.leftstart + gh.leftcount
+
+            for leftline, rightline, left, right in parseHunk(gh):
+                rowIdx = _getIndex(self.rows, gh.left, leftline)
+
+                if r[gh.left] is not None:
+                    assert r[gh.left] == left, (gh.left, r[gh.left], left)
+                if r[gh.right] is not None:
+                    assert r[gh.right] == right, (gh.right, r[gh.right], right)
+                r[gh.left] = left
+                r[gh.right] = right
+'''
+
+    def reload1(self):
         fn = self.source.filename
 
         # every column is that compared with index
@@ -114,20 +233,28 @@ class GitMerge(GitSheet):
         # sort GitHunks by fn, startline
         # until next startline, append rows from each file verbatim (no changes)
 
-        nextLineIdx = 0
-
         # diff of index and working
         allHunks = list(parseDiff(2, 3, git_lines('diff', '--patch', '--no-color', '--no-prefix', fn)))
+        lineIndexes = {}  # [(1/2/3, linenum)] -> 
+        for gh in sorted(allHunks, key=lambda gh: (gh.fn, min(gh.leftstart, gh.rightstart))):
+            while allIndexes[gh.left] < gh.leftstart-1 or allIndexes[gh.right] < gh.rightstart-1:
+                # add context lines before this hunk starts
+                r = [None] * len(allFiles)
+                self.rows.append(r)
+                if allIndexes[gh.left] < gh.leftstart-1:
+                    r[gh.left] = allFiles[gh.left][allIndexes[gh.left]]
+                    allIndexes[gh.left] += 1
+                if allIndexes[gh.right] < gh.rightstart-1:
+                    r[gh.right] = allFiles[gh.right][allIndexes[gh.right]]
+                    allIndexes[gh.right] += 1
 
-#        for gh in sorted(allHunks, key=lambda gh: (gh.fn, min(gh.leftstart, gh.rightstart))):
-#            while max(allIndexes) < gh.leftstart:
-#                r = [nextLineIdx] + [f[
-#                self.rows.append(r)
-#            for line in gh.difflines:
-#                typech = line[0]
-#                line = line[1:]
-#                if typech == ' ':
-#
+            hunklines = list(parseHunk(gh))
+            for left, right in hunklines:
+                r = [None] * len(allFiles)
+                self.rows.append(r)
+                r[gh.left] = left
+                r[gh.right] = right
 
-
+            allIndexes[gh.left] += gh.leftcount
+            allIndexes[gh.right] += gh.rightcount
 
