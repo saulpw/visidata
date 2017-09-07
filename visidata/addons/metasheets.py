e30dbe680257ea3a0df6795bdb86a928cb0468ab
Saul Pwanson <code@saul.pw>
2017-09-07 04:30:00 +0000

    fix paren imbalance

--- visidata/addons/metasheets.py
+++ visidata/addons/metasheets.py
@@ -19,7 +19,7 @@ vd().optionsSheet = OptionsSheet(options)
 def combineColumns(cols):
     'Return Column object formed by joining fields in given columns.'
     return Column("+".join(c.name for c in cols),
-                  getter=lambda r,cols=cols,ch=' ': ch.join(c.getDisplayValue(r) for c in cols))))
+                  getter=lambda r,cols=cols,ch=' ': ch.join(c.getDisplayValue(r) for c in cols))
 
 
 class SheetsSheet(Sheet):
