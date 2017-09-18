3a78d72a94883ef43756d3ac183f4fba3ff0e045
Saul Pwanson <code@saul.pw>
2017-09-18 03:19:16 +0000

    Reformat options.

--- docs/man/parse_options.py
+++ docs/man/parse_options.py
@@ -19,11 +19,11 @@ else:
 
 print(vd.__version__)
 
-options_cli_skel = '''.It Sy --{cli_optname} Ns ={type}
-.No {description} (default Sy {default} Ns )
+options_cli_skel = '''.It Sy --{cli_optname} Ns = Ns Ar "{type}" No {default}
+.No {description}
 '''
 
-options_menu_skel = '''.It Sy "{optname:<18}" No "{default:<16}"
+options_menu_skel = '''.It Sy "{optname:<19}" No "{default}"
 {description} 
 '''
 
@@ -31,15 +31,26 @@ with open('{0}-cli.inc'.format(man), 'w') as cliOut:
     with open('{0}-menu.inc'.format(man), 'w') as menuOut:
         opts = vd.baseOptions.keys()
 
+        colwidth = max((len(optname)+len(str(default))) for optname, default, _, _ in vd.baseOptions.values())
+        menuOut.write('.Bl -tag -width %s -compact\n' % ('X'*(colwidth+2)))
+
+        cliwidth = max(29+len(str(default)) for _, default, _, _ in vd.baseOptions.values())
+        cliOut.write('.Bl -tag -width %s -compact\n' % ('X'*(cliwidth+2)))
+
         for optname in opts:
             optname, default, value, desc = vd.baseOptions[optname]
             if optname[:5] in ['color', 'disp_'] or man == 'vdtui':
                 options_menu = options_menu_skel.format(optname=optname,type=type(value).__name__,default = str(default), description = desc)
                 menuOut.write(options_menu)
             else:
-                cliOut.write(options_cli_skel.format(cli_optname=optname.replace('_', '-'),
-                                                optname = optname, 
-                                                type=type(value).__name__,
-                                                 default=default, 
+                cli_optname=optname.replace('_', '-')
+                cli_type=type(value).__name__
+                optlen = len(cli_optname)+len(cli_type)+1
+                cliOut.write(options_cli_skel.format(cli_optname=cli_optname,
+                                                optname = optname,
+                                                type=cli_type+" "*(29-optlen),
+                                                 default=default,
                                                  description=desc))
 
+        menuOut.write('.El')
+        cliOut.write('.El')
