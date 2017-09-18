5e36363a1b615bc07636351223dac6e43231be53
Saul Pwanson <code@saul.pw>
2017-09-18 03:19:43 +0000

    Refactor and disable freqtbl numeric binning.

--- visidata/addons/freqtbl.py
+++ visidata/addons/freqtbl.py
@@ -8,8 +8,8 @@ globalCommand('zF', 'vd.push(SheetFreqTable(sheet, Column("Total", getter=lambda
 
 theme('disp_histogram', '*')
 option('disp_histolen', 80, 'width of histogram column')
-option('histogram_bins', 0, 'number of bins for histogram of numeric columns')
-option('histogram_even_interval', False, 'if histogram bins should have even distribution of rows')
+#option('histogram_bins', 0, 'number of bins for histogram of numeric columns')
+#option('histogram_even_interval', False, 'if histogram bins should have even distribution of rows')
 
 ColumnsSheet.commands += [
     Command(ENTER, 'vd.push(SheetFreqTable(source, cursorRow))', 'open frequency table for this column in the source sheet')
@@ -35,7 +35,7 @@ class SheetFreqTable(Sheet):
         Command('u', 'unselect([cursorRow]); cursorDown(1)', 'unselect these entries in the source sheet'),
 
         Command(ENTER, 'vs = copy(source); vs.name += "_"+valueNames(cursorRow[0]); vs.rows=copy(cursorRow[1]); vd.push(vs)', 'push new sheet with only source rows for this value'),
-        Command('w', 'options.histogram_even_interval = not options.histogram_even_interval; reload()', 'toggle histogram_even_interval option')
+#        Command('w', 'options.histogram_even_interval = not options.histogram_even_interval; reload()', 'toggle histogram_even_interval option')
     ]
 
     def __init__(self, sheet, *columns):
@@ -77,87 +77,94 @@ class SheetFreqTable(Sheet):
         self.source.unselect(row[1])
         return super().unselectRow(row)
 
+    def numericBinning(self):
+        nbins = options.histogram_bins or int(len(self.source.rows) ** (1./2))
+
+        origCol = self.origCols[0]
+        self.columns[0].type = str
+
+        # separate rows with errors at the column from those without errors
+        errorbin = []
+        allbin = []
+        for row in self.genProgress(self.source.rows):
+            v = origCol.getTypedValue(row)
+            if not v:
+                errorbin.append(row)
+            else:
+                allbin.append((v, row))
+
+        # find bin pivots from non-error values
+        binPivots = []
+        sortedValues = sorted(allbin, key=lambda x: x[0])
+
+        if options.histogram_even_interval:
+            binsize = len(sortedValues)/nbins
+            pivotIdx = 0
+            for i in range(math.ceil(len(sortedValues)/binsize)):
+                firstVal = sortedValues[int(pivotIdx)][0]
+                binPivots.append(firstVal)
+                pivotIdx += binsize
+
+        else:
+            minval, maxval = sortedValues[0][0], sortedValues[-1][0]
+            binWidth = (maxval - minval)/nbins
+            binPivots = list((minval + binWidth*i) for i in range(0, nbins))
+
+        binPivots.append(None)
+
+        # put rows into bins (as self.rows) based on values
+        binMinIdx = 0
+        binMin = 0
+
+        for binMax in binPivots[1:-1]:
+            binrows = []
+            for i, (v, row) in enumerate(sortedValues[binMinIdx:]):
+                if binMax != binPivots[-2] and v > binMax:
+                    break
+                binrows.append(row)
+
+            binMaxDispVal = origCol.format(binMax)
+            binMinDispVal = origCol.format(binMin)
+            if binMinIdx == 0:
+                binName = '<=%s' % binMaxDispVal
+            elif binMax == binPivots[-2]:
+                binName = '>=%s' % binMinDispVal
+            else:
+                binName = '%s-%s' % (binMinDispVal, binMaxDispVal)
+
+            self.addRow((binName, binrows))
+            binMinIdx += i
+            binMin = binMax
+
+        if errorbin:
+            self.rows.insert(0, ('errors', errorbin))
+
+        ntallied = sum(len(x[1]) for x in self.rows)
+        assert ntallied == len(self.source.rows), (ntallied, len(self.source.rows))
+
+    def discreteBinning(self):
+        rowidx = {}
+        for r in self.genProgress(self.source.rows):
+            v = tuple(getValueOrError(c, r) for c in self.origCols)
+            histrow = rowidx.get(v)
+            if histrow is None:
+                histrow = (v, [])
+                rowidx[v] = histrow
+                self.addRow(histrow)
+            histrow[1].append(r)
+
+        self.rows.sort(key=lambda r: len(r[1]), reverse=True)  # sort by num reverse
+
+
     @async
     def reload(self):
         'Generate histrow for each row and then reverse-sort by length.'
-        rowidx = {}
         self.rows = []
 
-        nbins = options.histogram_bins or int(len(self.source.rows) ** (1./2))
-
-        if nbins and len(self.origCols) == 1 and self.origCols[0].type in (int, float, currency):
-            origCol = self.origCols[0]
-            self.columns[0].type = str
-
-            # separate rows with errors at the column from those without errors
-            errorbin = []
-            allbin = []
-            for row in self.genProgress(self.source.rows):
-                v = origCol.getTypedValue(row)
-                if not v:
-                    errorbin.append(row)
-                else:
-                    allbin.append((v, row))
-
-            # find bin pivots from non-error values
-            binPivots = []
-            sortedValues = sorted(allbin, key=lambda x: x[0])
-
-            if options.histogram_even_interval:
-                binsize = len(sortedValues)/nbins
-                pivotIdx = 0
-                for i in range(math.ceil(len(sortedValues)/binsize)):
-                    firstVal = sortedValues[int(pivotIdx)][0]
-                    binPivots.append(firstVal)
-                    pivotIdx += binsize
-
-            else:
-                minval, maxval = sortedValues[0][0], sortedValues[-1][0]
-                binWidth = (maxval - minval)/nbins
-                binPivots = list((minval + binWidth*i) for i in range(0, nbins))
-
-            binPivots.append(None)
-
-            # put rows into bins (as self.rows) based on values
-            binMinIdx = 0
-            binMin = 0
-
-            for binMax in binPivots[1:-1]:
-                binrows = []
-                for i, (v, row) in enumerate(sortedValues[binMinIdx:]):
-                    if binMax != binPivots[-2] and v > binMax:
-                        break
-                    binrows.append(row)
-
-                binMaxDispVal = origCol.format(binMax)
-                binMinDispVal = origCol.format(binMin)
-                if binMinIdx == 0:
-                    binName = '<=%s' % binMaxDispVal
-                elif binMax == binPivots[-2]:
-                    binName = '>=%s' % binMinDispVal
-                else:
-                    binName = '%s-%s' % (binMinDispVal, binMaxDispVal)
-
-                self.addRow((binName, binrows))
-                binMinIdx += i
-                binMin = binMax
-
-            if errorbin:
-                self.rows.insert(0, ('errors', errorbin))
-
-            ntallied = sum(len(x[1]) for x in self.rows)
-            assert ntallied == len(self.source.rows), (ntallied, len(self.source.rows))
-        else:
-            for r in self.genProgress(self.source.rows):
-                v = tuple(getValueOrError(c, r) for c in self.origCols)
-                histrow = rowidx.get(v)
-                if histrow is None:
-                    histrow = (v, [])
-                    rowidx[v] = histrow
-                    self.addRow(histrow)
-                histrow[1].append(r)
-
-            self.rows.sort(key=lambda r: len(r[1]), reverse=True)  # sort by num reverse
+#        if len(self.origCols) == 1 and self.origCols[0].type in (int, float, currency):
+#            self.numericBinning()
+#        else:
+        self.discreteBinning()
 
         self.largest = len(self.rows[0][1])+1
 
