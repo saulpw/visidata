1bf53544124d00ec1668a652d88be501d2db20d6
Saul Pwanson <code@saul.pw>
2017-09-19 18:44:59 -0700

    Update autogenerated manpage .inc files

--- docs/mkdocs.sh
+++ docs/mkdocs.sh
@@ -11,5 +11,4 @@ export PATH=$VDDIR/bin:$PATH
 $MANDIR/parse_options.py vd
 $MANDIR/parse_options.py
 
-#groff -s -mandoc -Thtml $MANDIR/vd-skel.1
 MAN_KEEP_FORMATTING=1 COLUMNS=80 man $MANDIR/vd-skel.1 | ul | aha > $MANDIR/vd-man.html
