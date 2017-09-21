eeb5e29434bcec7cc90c5eb440da1220a878f24f
Saul Pwanson <code@saul.pw>
2017-09-20 17:16:26 -0700

    [manpage] use preconv to properly handle unicode chars
    also generates full vd.1

--- docs/mkdocs.sh
+++ docs/mkdocs.sh
@@ -11,4 +11,6 @@ export PATH=$VDDIR/bin:$PATH
 $MANDIR/parse_options.py vd
 $MANDIR/parse_options.py
 
-MAN_KEEP_FORMATTING=1 COLUMNS=80 man -E utf8 $MANDIR/vd-skel.1 | ul | aha > $MANDIR/vd-man.html
+soelim $MANDIR/vd-skel.1 > $MANDIR/vd-pre.1
+preconv -e utf8 $MANDIR/vd-pre.1 > $MANDIR/vd.1
+MAN_KEEP_FORMATTING=1 COLUMNS=100 man $MANDIR/vd.1 | ul | aha > $MANDIR/vd-man.html
