6df5854b23e7eb40ff9fc35e5cd7e47c2338f147
anjakefala <anja.kefala@gmail.com>
2017-10-05 02:58:24 +0000

    Update docs
    - Move manpages to visidata/man
    - Update website
    - Delete outdated images

--- build.sh
+++ build.sh
@@ -5,9 +5,7 @@ set -e
 VD=~/git/visidata
 BUILD=$VD/_build
 WWW=$VD/_build/www
-MAN=$VD/docs/man
-
-mkdir -p $WWW/man/vd
+MAN=$VD/visidata/man
 
 export PYTHONPATH=$VD:$VD/visidata
 export PATH=$VD/bin:$PATH
@@ -17,7 +15,6 @@ export PATH=$VD/bin:$PATH
 cp $MAN/* $BUILD/
 $MAN/parse_options.py $BUILD/vd-cli.inc $BUILD/vd-opts.inc
 
-soelim -I $BUILD $BUILD/vd-skel.1 > $BUILD/vd-pre.1
-preconv -e utf8 $BUILD/vd-pre.1 > $MAN/vd.1   # checked in
-gzip -c $MAN/vd.1 > $BUILD/vd.1.gz
+soelim -rt -I $BUILD $BUILD/vd-skel.1 > $BUILD/vd-pre.1
+preconv -r -e utf8 $BUILD/vd-pre.1 > $MAN/vd.1   # checked in
 
--- mkwww.sh
+++ mkwww.sh
@@ -7,8 +7,8 @@ set -e
 VD=~/git/visidata
 BUILD=$VD/_build
 WWW=$BUILD/www
-DEMO=$VD/www/tour
-MAN=$VD/docs/man
+TOUR=$VD/www/tour
+MAN=$VD/visidata/man
 
 mkdir -p $WWW/man
 mkdir -p $WWW/tour
@@ -27,25 +27,25 @@ $VD/strformat.py body=$VD/www/frontpage-body.html title="VisiData" head='' < $VD
 #
 
 # Builds tours
-$DEMO/mkindex.py $DEMO/*.yaml > $BUILD/demo-index-body.html
+$TOUR/mkindex.py $TOUR/*.yaml > $BUILD/demo-index-body.html
 # Which main css file is it referencing?
 $VD/strformat.py body=$BUILD/demo-index-body.html title="Tour Index" head='' < $VD/www/template.html > $WWW/tour/index.html
 
-for tpath in `find $DEMO -name '*.yaml'`; do
-    tyaml=${tpath##$DEMO/}
+for tpath in `find $TOUR -name '*.yaml'`; do
+    tyaml=${tpath##$TOUR/}
     tfolder=${tyaml%.yaml}
     mkdir -p $WWW/tour/$tfolder
-    $VD/strformat.py body=<($DEMO/mkdemo.py $DEMO/$tyaml) title="VisiData tour: $tfolder" head=$DEMO/demo-head-inc.html < $VD/www/template.html > $WWW/tour/$tfolder/index.html
+    $VD/strformat.py body=<($TOUR/mkdemo.py $TOUR/$tyaml) title="VisiData tour: $tfolder" head=$TOUR/demo-head-inc.html < $VD/www/template.html > $WWW/tour/$tfolder/index.html
 
     cp $VD/www/*.* $WWW
-    cp $DEMO/$tfolder/*.* $WWW/tour/$tfolder
-    cp $DEMO/asciinema-player.* $WWW/tour
-    cp $DEMO/*.css $WWW/tour
+    cp $TOUR/$tfolder/*.* $WWW/tour/$tfolder
+    cp $TOUR/asciinema-player.* $WWW/tour
+    cp $TOUR/*.css $WWW/tour
 done
 
 #### At the end
 # add analytics to .html files
 for fn in `find $WWW -name '*.html'` ; do
-    sed -i -e "/<head>/I{r $VD/docs/analytics.thtml" -e 'd}' $fn
+    sed -i -e "/<head>/I{r $VD/www/analytics.thtml" -e 'd}' $fn
 done
 
