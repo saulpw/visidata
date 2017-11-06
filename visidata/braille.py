e42a78aa6bf8619ebf5d8e54e6c5807299ae2baa
Saul Pwanson <code@saul.pw>
2017-11-06 00:26:02 -0800

    braille canvas cleanup; line()

--- visidata/braille.py
+++ visidata/braille.py
@@ -7,7 +7,7 @@ left_margin_chars = 10
 right_margin_chars = 6
 bottom_margin_chars = 1  # reserve bottom line for x axis
 
-globalCommand('m', 'vd.push(GraphSheet(sheet.name+"_graph", selectedRows or rows, keyCols and keyCols[0], cursorCol))', 'graph the current column vs the first key column (or row number)')
+globalCommand('m', 'vd.push(GraphSheet(sheet.name+"_graph", selectedRows or rows, keyCols and keyCols[0] or None, cursorCol))', 'graph the current column vs the first key column (or row number)')
 globalCommand('gm', 'vd.push(GraphSheet(sheet.name+"_graph", selectedRows or rows, keyCols and keyCols[0], *numericCols(nonKeyVisibleCols)))', 'graph all numeric columns vs the first key column (or row number)')
 
 graphColors = 'green red yellow cyan magenta white 38 136 168'.split()
@@ -37,8 +37,27 @@ class BrailleCanvas(Sheet):
     def set_pixel(self, pix_x, pix_y, colorname):
         self.pixels[pix_y][pix_x] = colors[colorname]
 
-    def label(self, pix_x, pix_y, txt, color):
-        self.labels.append((pix_x, pix_y, txt, color))
+    def line(self, x1, y1, x2, y2, colorname):
+        'Draws coords of the line from (x1, y1) to (x2, y2)'
+        xdiff = max(x1, x2) - min(x1, x2)
+        ydiff = max(y1, y2) - min(y1, y2)
+        xdir = 1 if x1 <= x2 else -1
+        ydir = 1 if y1 <= y2 else -1
+
+        r = max(xdiff, ydiff)
+
+        for i in range(r+1):
+            x = x1
+            y = y1
+
+            if r:
+                y += ydir * (i * ydiff) / r
+                x += xdir * (i * xdiff) / r
+
+            self.set_pixel(x, y, colorname)
+
+    def label(self, pix_x, pix_y, txt, colorname):
+        self.labels.append((pix_x, pix_y, txt, colors[colorname]))
 
     def draw(self, scr):
         if not self.pixels:
@@ -69,8 +88,8 @@ class BrailleCanvas(Sheet):
                     scr.addstr(y, x, chr(0x2800+braille_num), colormode)
 
         if options.show_graph_labels:
-            for pix_x, pix_y, txt, colorname in self.labels:
-                clipdraw(scr, int(pix_y/4), int(pix_x/2), txt, colors[colorname], len(txt))
+            for pix_x, pix_y, txt, attr in self.labels:
+                clipdraw(scr, int(pix_y/4), int(pix_x/2), txt, attr, len(txt))
 
 
 # provides unit->pixel conversion, axis labels, legend
@@ -79,8 +98,14 @@ class GraphSheet(BrailleCanvas):
         Command('^L', 'refresh()', 'redraw all pixels on canvas'),
         Command('w', 'options.show_graph_labels = not options.show_graph_labels', 'toggle show_graph_labels')
     ]
-    def __init__(self, name, *sources, **kwargs):
-        super().__init__(name, *sources, **kwargs)
+    def __init__(self, name, rows, xcol, *ycols, **kwargs):
+        super().__init__(name, rows, **kwargs)
+        self.xcol = xcol
+        self.ycols = ycols
+        if xcol:
+            isNumeric(self.xcol) or error('%s type is non-numeric' % xcol.name)
+        for col in ycols:
+            isNumeric(col) or error('%s type is non-numeric' % col.name)
         self.unit_leftx = None
         self.unit_rightx = None
         self.unit_topy = None
@@ -106,8 +131,8 @@ class GraphSheet(BrailleCanvas):
             pix_y = self.pix_topy + (self.unit_topy-unit_y)/scale_y  # invert y axis
             self.pixels[round(pix_y)][round(pix_x)] = color
 
-    def legend(self, i, txt, color):
-        self.label(self.pix_rightx-30, self.pix_topy+i*4, txt, color)
+    def legend(self, i, txt, colorname):
+        self.label(self.pix_rightx-30, self.pix_topy+i*4, txt, colorname)
 
     def plot(self, x, y, attr):
         if self.unit_leftx is None or x < self.unit_leftx:     self.unit_leftx = x
@@ -119,38 +144,26 @@ class GraphSheet(BrailleCanvas):
 
     @async
     def reload(self):
-        rows = self.sources[0]
-        xcol = self.sources[1]
-        ycols = self.sources[2:]
-
-        assert isNumeric(xcol)
+#        rows = self.sources[0]
 
         self.rows = []
         nerrors = 0
         nplotted = 0
-        for i, ycol in enumerate(ycols):
-            colorname = graphColors[i]
+        for i, ycol in enumerate(self.ycols):
+            colorname = graphColors[i % len(graphColors)]
             attr = colors[colorname]
 
-            if xcol:
-                for row in self.genProgress(rows):
-                    try:
-                        unit_x = xcol.getTypedValue(row)
-                        unit_y = ycol.getTypedValue(row)
-                        self.plot(unit_x, unit_y, attr)
-                        nplotted += 1
-                    except Exception:
-                        nerrors += 1
-            else:
-                for unit_x, row in enumerate(self.genProgress(rows)):
-                    try:
-                        unit_y = ycol.getTypedValue(row)
-                        self.plot(unit_x, unit_y, attr)
-                        nplotted += 1
-                    except Exception:
-                        nerrors += 1
+            for rownum, row in enumerate(self.genProgress(self.source)):
+                try:
+                    unit_x = self.xcol.getTypedValue(row) if self.xcol else rownum
+                    unit_y = ycol.getTypedValue(row)
+                    self.plot(unit_x, unit_y, attr)
+                    nplotted += 1
+                except Exception:
+                    nerrors += 1
 
-        status('plotted %d points (%d errors)' % (nplotted, nerrors))
+        status('plotting %d points (%d errors)' % (nplotted, nerrors))
+        self.refresh()
 
     def add_y_axis_label(self, frac):
         amt = self.unit_bottomy + frac*(self.unit_topy-self.unit_bottomy)
@@ -176,10 +189,6 @@ class GraphSheet(BrailleCanvas):
     def create_labels(self):
         self.labels = []
 
-        for i, ycol in enumerate(self.sources[2:]):
-            colorname = graphColors[i]
-            self.legend(i, ycol.name, colorname)
-
         # y-axis
         self.add_y_axis_label(1.00)
         self.add_y_axis_label(0.75)
@@ -194,5 +203,9 @@ class GraphSheet(BrailleCanvas):
         self.add_x_axis_label(0.25)
         self.add_x_axis_label(0.00)
 
-        self.label(0, self.pix_bottomy+4, '%*s»' % (left_margin_chars-2, self.sources[1].name), options.color_graph_axis)
+        xname = self.xcol.name if self.xcol else 'row#'
+        self.label(0, self.pix_bottomy+4, '%*s»' % (left_margin_chars-2, xname), options.color_graph_axis)
 
+        for i, ycol in enumerate(self.ycols):
+            colorname = graphColors[i]
+            self.legend(i, ycol.name, colorname)
