21a603782a01fb5cde6598ba1e51a1ca95a0c762
Saul Pwanson <code@saul.pw>
2017-11-20 20:11:45 -0800

    [sqlite] reload must not be async; conns are single-threaded

--- visidata/loaders/sqlite.py
+++ visidata/loaders/sqlite.py
@@ -18,7 +18,7 @@ class SqliteSheet(Sheet):
             import sqlite3
             self.conn = sqlite3.connect(pathOrSheet.resolve())
 
-    @async
+    # must not be @async due to sqlite lib being single-threaded
     def reload(self):
         tblname = self.tableName
         self.columns = self.getColumns(tblname)
