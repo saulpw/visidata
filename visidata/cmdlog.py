0b8d236f77474d24a635078471f3cdfd6ad959bf
Saul Pwanson <code@saul.pw>
2018-01-10 01:17:12 -0800

    [cmdlog] omit help (z? F1) from commandlog

--- visidata/cmdlog.py
+++ visidata/cmdlog.py
@@ -24,8 +24,7 @@ globalCommand('status', 'status(input("status: ", display=False))', 'show given 
 nonLogKeys = 'KEY_DOWN KEY_UP KEY_NPAGE KEY_PPAGE kDOWN kUP j k gj gk ^F ^B r < > { } / ? n N gg G g/ g?'.split()
 nonLogKeys += 'KEY_LEFT KEY_RIGHT h l gh gl c'.split()
 nonLogKeys += 'zk zj zt zz zb zh zl zKEY_LEFT zKEY_RIGHT'.split()
-nonLogKeys += '^L ^C ^U ^K ^I ^D KEY_RESIZE'.split()
-
+nonLogKeys += '^L ^C ^U ^K ^I ^D KEY_RESIZE KEY_F(1) z?'.split()
 option('rowkey_prefix', 'キ', 'string prefix for rowkey in the cmdlog')
 
 def itemsetter(i):
