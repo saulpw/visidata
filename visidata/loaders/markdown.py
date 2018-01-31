7c99b12ed6e901fcd9438247dab1d38a492b9812
Saul Pwanson <code@saul.pw>
2018-01-30 22:57:09 -0800

    [markdown] save only visibleCols

--- visidata/loaders/markdown.py
+++ visidata/loaders/markdown.py
@@ -20,11 +20,11 @@ def save_md(vs, fn):
     'pipe tables compatible with org-mode'
 
     with open(fn, 'w', encoding=options.encoding, errors=options.encoding_errors) as fp:
-        fp.write('|' + '|'.join('%-*s' % (col.width, markdown_escape(col.name)) for col in vs.columns) + '|\n')
-        fp.write('|' + '+'.join(markdown_colhdr(col) for col in vs.columns) + '|\n')
+        fp.write('|' + '|'.join('%-*s' % (col.width, markdown_escape(col.name)) for col in vs.visibleCols) + '|\n')
+        fp.write('|' + '+'.join(markdown_colhdr(col) for col in vs.visibleCols) + '|\n')
 
         for row in Progress(vs.rows):
-            fp.write('|' + '|'.join('%-*s' % (col.width, markdown_escape(col.getDisplayValue(row))) for col in vs.columns) + '|\n')
+            fp.write('|' + '|'.join('%-*s' % (col.width, markdown_escape(col.getDisplayValue(row))) for col in vs.visibleCols) + '|\n')
 
     status('%s save finished' % fn)
 
