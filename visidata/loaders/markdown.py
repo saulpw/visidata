6120c841943f38114a5c983890b716e003b0c83b
anjakefala <anja.kefala@gmail.com>
2018-03-06 01:12:21 +0000

    [markdown] Fix TypeError with column width
    Co-authored-by: Saul Pwanson <code@saul.pw>
    Co-authored-by: Anja Boskovic <anja.kefala@gmail.com>

--- visidata/loaders/markdown.py
+++ visidata/loaders/markdown.py
@@ -13,18 +13,18 @@ def markdown_colhdr(col):
     if isNumeric(col):
         return ('-' * (col.width-1)) + ':'
     else:
-        return '-' * col.width
+        return '-' * (col.width or options.default_width)
 
 @async
 def save_md(vs, fn):
     'pipe tables compatible with org-mode'
 
     with open(fn, 'w', encoding=options.encoding, errors=options.encoding_errors) as fp:
-        fp.write('|' + '|'.join('%-*s' % (col.width, markdown_escape(col.name)) for col in vs.visibleCols) + '|\n')
+        fp.write('|' + '|'.join('%-*s' % (col.width or options.default_width, markdown_escape(col.name)) for col in vs.visibleCols) + '|\n')
         fp.write('|' + '+'.join(markdown_colhdr(col) for col in vs.visibleCols) + '|\n')
 
         for row in Progress(vs.rows):
-            fp.write('|' + '|'.join('%-*s' % (col.width, markdown_escape(col.getDisplayValue(row))) for col in vs.visibleCols) + '|\n')
+            fp.write('|' + '|'.join('%-*s' % (col.width or options.default_width, markdown_escape(col.getDisplayValue(row))) for col in vs.visibleCols) + '|\n')
 
     status('%s save finished' % fn)
 
