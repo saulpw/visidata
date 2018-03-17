df065a88d40f08a032fd832a12097a7a779fd9b3
Saul Pwanson <code@saul.pw>
2018-03-17 00:49:02 -0700

    [DirSheet] apply edits to filesystem

--- visidata/data.py
+++ visidata/data.py
@@ -1,4 +1,7 @@
 import random
+import stat
+import pwd
+import grp
 
 from .vdtui import *
 
@@ -162,21 +165,39 @@ def saveSheet(vs, fn, confirm_overwrite=False):
 
 
 class DirSheet(Sheet):
-    'Sheet displaying directory, using ENTER to open a particular file.'
-    rowtype = 'files'
+    'Sheet displaying directory, using ENTER to open a particular file.  Edited fields are applied to the filesystem.'
+    rowtype = 'files' # rowdef: (Path, stat)
     commands = [
-        Command(ENTER, 'vd.push(openSource(cursorRow[0]))', 'open current row', 'sheet-open-row')  # path, filename
+        Command(ENTER, 'vd.push(openSource(cursorRow[0]))', 'open current row', 'sheet-open-row'),
+#        Command('modify-delete-row', 'path, _ = rows.pop(cursorRowIndex); os.rmdir(path.resolve()) if path.is_dir() else os.remove(path.resolve())'),
     ]
     columns = [
-        Column('filename', getter=lambda col,row: row[0].name + row[0].ext),
+        # these setters all either raise or return None, so this is a non-idiomatic 'or' to squeeze in a reload
+        Column('filename', getter=lambda col,row: row[0].name + row[0].ext,
+                           setter=lambda col,row,val: os.rename(row[0].resolve(), val)),  # BUG
         Column('type', getter=lambda col,row: row[0].is_dir() and '/' or row[0].suffix),
-        Column('size', type=int, getter=lambda col,row: row[1].st_size),
-        Column('mtime', type=date, getter=lambda col,row: row[1].st_mtime)
+        Column('size', type=int, getter=lambda col,row: row[1].st_size,
+                                 setter=lambda col,row,val: os.truncate(row[0].resolve(), int(val)) or col.sheet.reload()),
+        Column('mtime', type=date, getter=lambda col,row: row[1].st_mtime,
+               setter=lambda col,row,val: os.utime(row[0].resolve(), times=((row[1].st_atime, float(val)))) or col.sheet.reload()),
+        Column('owner', width=0, cache=True, getter=lambda col,row: pwd.getpwuid(row[1].st_uid).pw_name,
+            setter=lambda col,row,val: os.chown(row[0].resolve(), pwd.getpwnam(val).pw_uid, -1) or col.sheet.reload()),
+        Column('group', width=0, cache=True, getter=lambda col,row: grp.getgrgid(row[1].st_gid).gr_name,
+            setter=lambda col,row,val: os.chown(row[0].resolve(), -1, grp.getgrnam(val).pw_gid) or col.sheet.reload()),
+        Column('mode', width=0, type=int, fmtstr='{:o}', getter=lambda col,row: row[1].st_mode),
     ]
+    colorizers = [
+        Colorizer('cell', 4, lambda s,c,r,v: 'yellow' if c.name=='group' and r[1].st_mode & stat.S_IRGRP else None),
+        Colorizer('cell', 5, lambda s,c,r,v: 'green' if c.name=='group' and r[1].st_mode & stat.S_IWGRP else None),
+        Colorizer('cell', 4, lambda s,c,r,v: 'yellow' if c.name=='owner' and r[1].st_mode & stat.S_IRUSR else None),
+        Colorizer('cell', 5, lambda s,c,r,v: 'green' if c.name=='owner' and r[1].st_mode & stat.S_IWUSR else None),
+        Colorizer('cell', 5, lambda s,c,r,v: 'bold' if c.name=='owner' and r[1].st_mode & stat.S_IXUSR else None),
+        Colorizer('cell', 5, lambda s,c,r,v: 'bold' if c.name=='group' and r[1].st_mode & stat.S_IXGRP else None),
+    ]
+    nKeys = 1
 
     def reload(self):
-        self.rows = [(p, p.stat()) for p in self.source.iterdir()]  #  if not p.name.startswith('.')]
-
+        self.rows = sorted((p, p.stat()) for p in self.source.iterdir())  #  if not p.name.startswith('.')]
 
 def openSource(p, filetype=None):
     'calls open_ext(Path) or openurl_scheme(UrlPath, filetype)'
