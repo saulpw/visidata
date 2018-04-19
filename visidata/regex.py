31c2b0b4391dc0eb40937c440d28d8855da9ed77
Saul Pwanson <code@saul.pw>
2018-04-19 09:41:38 -0700

    [regex] add 'g*' command to replace selected row cells with regex transform

--- visidata/regex.py
+++ visidata/regex.py
@@ -2,7 +2,8 @@ from visidata import *
 
 globalCommand(':', 'addRegexColumns(makeRegexSplitter, sheet, cursorColIndex, cursorCol, cursorRow, input("split regex: ", type="regex-split"))', 'add new columns from regex split; # columns determined by example row at cursor', 'modify-add-column-regex-split')
 globalCommand(';', 'addRegexColumns(makeRegexMatcher, sheet, cursorColIndex, cursorCol, cursorRow, input("match regex: ", type="regex-capture"))', 'add new column from capture groups of regex; requires example row', 'modify-add-column-regex-capture')
-globalCommand('*', 'addColumn(regexTransform(cursorCol, input("transform column by regex: ", type="regex-subst")), cursorColIndex+1)', 'regex/subst - replace regex with subst, which may include backreferences (\\1 etc)', 'modify-add-column-regex-transform')
+globalCommand('*', 'addColumn(Column(cursorCol.name + "_re", getter=regexTransform(cursorCol, input("transform column by regex: ", type="regex-subst"))), cursorColIndex+1)', 'regex/subst - replace regex with subst, which may include backreferences (\\1 etc)', 'modify-add-column-regex-transform')
+globalCommand('g*', 'rex=input("transform column by regex: ", type="regex-subst"); setValuesFromRegex(cursorCol, selectedRows or rows, rex)', 'regex/subst - replace regex with subst, which may include backreferences (\\1 etc)', 'modify-add-column-regex-transform-foo')
 
 replayableOption('regex_maxsplit', 0, 'maxsplit to pass to regex.split')
 
@@ -31,9 +32,7 @@ def regexTransform(origcol, instr):
     else:
         before = instr[:i]
         after = instr[i+1:]
-    newCol = Column(origcol.name + '_re',
-                    getter=lambda col,row,origcol=origcol, before=before, after=after: re.sub(before, after, origcol.getDisplayValue(row), flags=regex_flags()))
-    return newCol
+    return lambda col,row,origcol=origcol,before=before, after=after: re.sub(before, after, origcol.getDisplayValue(row), flags=regex_flags())
 
 def indexWithEscape(s, char, escape_char='\\'):
     i=0
@@ -45,3 +44,9 @@ def indexWithEscape(s, char, escape_char='\\'):
         i += 1
 
     return None
+
+
+def setValuesFromRegex(col, rows, rex):
+    transform = regexTransform(col, rex)
+    for r in rows:
+        col.setValue(r, transform(col, r))
