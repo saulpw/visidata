c615c8f4ab3d6850e640fcb1eec225e473fa26fa
Saul Pwanson <code@saul.pw>
2018-04-20 00:04:10 +0000

    [doc] update parse_options.py to use new options objects

--- visidata/man/parse_options.py
+++ visidata/man/parse_options.py
@@ -26,27 +26,27 @@ with open(fncli, 'w') as cliOut:
     with open(fnopts, 'w') as menuOut:
         opts = visidata.baseOptions.keys()
 
-        widestoptwidth, widestopt = sorted((len(optname)+len(str(default)), optname) for optname, default, _, _ in visidata.baseOptions.values())[-1]
+        widestoptwidth, widestopt = sorted((len(opt.name)+len(str(opt.default)), opt.name) for opt in visidata.baseOptions.values())[-1]
         print('widest option+default is "%s", width %d' % (widestopt, widestoptwidth))
         menuOut.write('.Bl -tag -width %s -compact\n' % ('X'*(widestoptwidth+3)))
 
-        cliwidth = max(padding+len(str(default)) for _, default, _, _ in visidata.baseOptions.values())
+        cliwidth = max(padding+len(str(opt.default)) for opt in visidata.baseOptions.values())
         cliOut.write('.Bl -tag -width %s -compact\n' % ('X'*(cliwidth+3)))
 
         for optname in opts:
-            optname, default, value, desc = visidata.baseOptions[optname]
-            if optname[:5] in ['color', 'disp_']:
-                options_menu = options_menu_skel.format(optname=optname,type=type(value).__name__,default = str(default), description = desc)
+            opt = visidata.baseOptions[optname]
+            if opt.name[:5] in ['color', 'disp_']:
+                options_menu = options_menu_skel.format(optname=opt.name,type=type(opt.value).__name__,default = str(opt.default), description = opt.helpstr)
                 menuOut.write(options_menu)
             else:
-                cli_optname=optname.replace('_', '-')
-                cli_type=type(value).__name__
+                cli_optname=opt.name.replace('_', '-')
+                cli_type=type(opt.value).__name__
                 optlen = len(cli_optname)+len(cli_type)+1
                 cliOut.write(options_cli_skel.format(cli_optname=cli_optname,
-                                                optname = optname,
+                                                optname = opt.name,
                                                 type=cli_type+" "*(padding-optlen),
-                                                 default=default,
-                                                 description=desc))
+                                                 default=opt.default,
+                                                 description=opt.helpstr))
 
         menuOut.write('.El')
         cliOut.write('.El')
