58c48c9f9d35b7f7508f6ac25133e9520cf5b668
Saul Pwanson <code@saul.pw>
2018-05-27 22:59:21 +0000

    [slide] Move slide cmds to Sheet.Command and add zH, zL, zJ, and zK
    - zH slide current column left n
    - zL slide current column right n
    - zJ slide current row down n
    - zK slide current row up n

--- visidata/slide.py
+++ visidata/slide.py
@@ -2,58 +2,24 @@
 
 from visidata import Sheet, moveListItem, globalCommand
 
+Sheet.Command('H', 'i = sheet.cursorVisibleColIndex; sheet.cursorVisibleColIndex = moveVisibleCol(sheet, i, i-1)', 'slide current column left', 'slide-col-left')
+Sheet.Command('L', 'i = sheet.cursorVisibleColIndex; sheet.cursorVisibleColIndex = moveVisibleCol(sheet, i, i+1)', 'slide current column right', 'slide-col-right')
+Sheet.Command('J', 'i = sheet.cursorRowIndex; sheet.cursorRowIndex = moveListItem(rows, i, i+1)', 'slide current row down', 'slide-row-down')
+Sheet.Command('K', 'i = sheet.cursorRowIndex; sheet.cursorRowIndex = moveListItem(rows, i, i-1)', 'slide current row up', 'slide-row-up')
+Sheet.Command('gH', 'columns.insert(0, columns.pop(cursorColIndex))', 'slide current column all the way to the left of sheet', 'slide-col-leftmost')
+Sheet.Command('gL', 'columns.append(columns.pop(cursorColIndex))', 'slide current column all the way to the right of sheet', 'slide-col-rightmost')
+Sheet.Command('gJ', 'rows.append(rows.pop(cursorRowIndex))', 'slide current row to the bottom of sheet', 'slide-row-bottom')
+Sheet.Command('gK', 'rows.insert(0, rows.pop(cursorRowIndex))', 'slide current row the top of sheet', 'slide-row-top')
+Sheet.Command('zH', 'i = sheet.cursorVisibleColIndex; n=int(input("slide col left n=", value=1)); sheet.cursorVisibleColIndex = moveVisibleCol(sheet, i, i-n)', 'slide current column left n', 'slide-col-left-n')
+Sheet.Command('zL', 'i = sheet.cursorVisibleColIndex; n=int(input("slide col right n=", value=1)); sheet.cursorVisibleColIndex = moveVisibleCol(sheet, i, i+n)', 'slide current column right n', 'slide-col-right-n')
+Sheet.Command('zJ', 'i = sheet.cursorRowIndex; n=int(input("slide row down n=", value=1)); sheet.cursorRowIndex = moveListItem(rows, i, i+n)', 'slide current row down n', 'slide-row-down-n')
+Sheet.Command('zK', 'i = sheet.cursorRowIndex; n=int(input("slide row up n=", value=1)); sheet.cursorRowIndex = moveListItem(rows, i, i-n)', 'slide current row up n', 'slide-row-up-n')
 
-def _moveVisibleCol(sheet, fromVisColIdx, toVisColIdx):
+
+def moveVisibleCol(sheet, fromVisColIdx, toVisColIdx):
     'Move visible column to another visible index in sheet.'
+    toVisColIdx = min(max(toVisColIdx, 0), sheet.nVisibleCols)
     fromColIdx = sheet.columns.index(sheet.visibleCols[fromVisColIdx])
     toColIdx = sheet.columns.index(sheet.visibleCols[toVisColIdx])
-    return moveListItem(sheet.columns, fromColIdx, toColIdx)
-
-def slide_col_left(sheet, n=1):
-    'slide current column left'
-    i = sheet.cursorVisibleColIndex
-    sheet.cursorVisibleColIndex = _moveVisibleCol(sheet, i, max(i-1, 0))
-
-def slide_col_right(sheet, n=1):
-    'slide current column right'
-    i = sheet.cursorVisibleColIndex
-    sheet.cursorVisibleColIndex = _moveVisibleCol(sheet, i, min(i+1, sheet.nVisibleCols-1))
-
-def slide_row_down(sheet, n=1):
-    'slide current row down'
-    i = sheet.cursorRowIndex
-    sheet.cursorRowIndex = moveListItem(sheet.rows, i, min(i+1, sheet.nRows-1))
-
-def slide_row_up(sheet, n=1):
-    'slide current row up'
-    i = sheet.cursorRowIndex
-    sheet.cursorRowIndex = moveListItem(sheet.rows, i, max(i-1, 0))
-
-def slide_col_leftmost(sheet):
-    'slide current column all the way to the left of sheet'
-    moveListItem(sheet.columns, sheet.cursorColIndex, 0)
-
-def slide_col_rightmost(sheet):
-    'slide current column all the way to the right of sheet'
-    moveListItem(sheet.columns, sheet.cursorColIndex, sheet.nCols)
-
-def slide_row_bottom(sheet):
-    'slide current row to the bottom of sheet'
-    moveListItem(sheet.rows, sheet.cursorRowIndex, sheet.nRows)
-
-def slide_row_top(sheet):
-    'slide current row the top of sheet'
-    moveListItem(sheet.rows, sheet.cursorRowIndex, 0)
-
-
-Sheet.bind('H', slide_col_left)
-Sheet.bind('L', slide_col_right)
-Sheet.bind('J', slide_row_down)
-Sheet.bind('K', slide_row_up)
-Sheet.bind('gH', slide_col_leftmost)
-Sheet.bind('gL', slide_col_rightmost)
-Sheet.bind('gJ', slide_row_bottom)
-Sheet.bind('gK', slide_row_top)
-
-#menu('modify move column left', slide_col_left)
-
+    moveListItem(sheet.columns, fromColIdx, toColIdx)
+    return toVisColIdx
