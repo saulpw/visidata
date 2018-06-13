saulpw/visidata

commit 76971b72f45a3075f640b5cf0e8c43f69e984cd8

[cosmetic] remove trailing separator on status line

visidata/vdtui.py
@@ -697,13 +697,14 @@ class VisiData:
         if maxwidth > 0:
             s = middleTruncate(s, maxwidth//2)
 
+        sep = options.disp_status_sep
         for _, n, x in sorted(self.statuses, key=lambda y: -y[0]):
             if n == 1:
                 s += '%s' % x
             else:
                 s += '[%sx] %s' % (n, x)
-            s += options.disp_status_sep
-        return s
+            s += sep
+        return s[:-len(sep)]
 
     def rightStatus(self, sheet):
         'Compose right side of status bar.'

commit 02bb2c3469e14ac1a7eabfb7533add6de0fdc9a5

fix status truncation bug introduced in 76971b7

visidata/vdtui.py
@@ -694,19 +694,24 @@ class VisiData:
 
     def leftStatus(self, vs):
         'Compose left side of status bar and add status messages.'
-        s = vs.leftStatus()
+        ret = vs.leftStatus()
         maxwidth = options.disp_lstatus_max
         if maxwidth > 0:
-            s = middleTruncate(s, maxwidth//2)
+            ret = middleTruncate(ret, maxwidth//2)
 
         sep = options.disp_status_sep
+        msgs = ''
         for _, n, x in sorted(self.statuses, key=lambda y: -y[0]):
+            if msgs:
+                msgs += sep
+
             if n == 1:
-                s += '%s' % x
+                msgs += '%s' % x
             else:
-                s += '[%sx] %s' % (n, x)
-            s += sep
-        return s[:-len(sep)]
+                msgs += '[%sx] %s' % (n, x)
+
+        ret += msgs
+        return ret
 
     def rightStatus(self, sheet):
         'Compose right side of status bar.'