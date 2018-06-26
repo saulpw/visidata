6e50f6df67301c37a5f37ca884bcf4fd6232b918
Saul Pwanson <code@saul.pw>
2018-06-25 21:26:15 -0700

    [xml] save; 'za' adds new attribute column; sub-sheets include ancestors

--- visidata/loaders/xml.py
+++ visidata/loaders/xml.py
@@ -5,16 +5,29 @@ import re
 
 def open_xml(p):
     from lxml import etree, objectify
-    root=etree.parse(p.resolve())
+    root = etree.parse(p.resolve())
     objectify.deannotate(root, cleanup_namespaces=True)
     return XmlSheet(p.name, source=root)
 open_svg = open_xml
+
+
+@async
+def save_xml(p, vs):
+    from lxml import etree, objectify
+    vs.source.write(p.resolve(), encoding=options.encoding, standalone=False, pretty_print=True)
+save_svg = save_xml
 
 
 def unns(k):
     if '}' in k:
         return k[k.find('}')+1:]
     return k
+
+
+def AttribColumn(name, k, **kwargs):
+    return Column(name, getter=lambda c,r,k=k: r.attrib.get(k),
+                        setter=lambda c,r,v,k=k: setitem(r.attrib, k, v), **kwargs)
+
 
 class XmlSheet(Sheet):
     rowtype = 'elements'   # rowdef: lxml.xml.Element
@@ -23,6 +36,7 @@ class XmlSheet(Sheet):
         ColumnAttr('sourceline', type=int, width=0),
         ColumnAttr('prefix', width=0),
         ColumnAttr('nstag', 'tag', width=0),
+        Column('path', width=0, getter=lambda c,r: c.sheet.source.getpath(r)),
         Column('tag', getter=lambda c,r: unns(r.tag)),
         Column('children', type=len, getter=lambda c,r: r.getchildren()),
         ColumnAttr('text'),
@@ -30,8 +44,13 @@ class XmlSheet(Sheet):
     ]
 
     commands = [
+        Command('za', 'attr=input("add attribute: "); addColumn(AttribColumn(attr, attr), cursorColIndex+1)', 'add column for xml attribute'),
         Command('v', 'showColumnsBasedOnRow(cursorRow)', 'show only columns in current row attributes', ),
         Command(ENTER, 'r=cursorRow; vd.push(XmlSheet("%s_%s" % (unns(r.tag), r.attrib.get("id")), source=r))', 'dive into this element')
+    ]
+
+    colorizers = [
+            Colorizer('row', 8, lambda self,c,r,v: 'green' if r is self.source else None)
     ]
 
     def showColumnsBasedOnRow(self, row):
@@ -45,6 +64,10 @@ class XmlSheet(Sheet):
         self.columns = copy(XmlSheet.columns)
         self.rows = []
 
+        if getattr(self.source, 'iterancestors', None):
+            for elem in list(self.source.iterancestors())[::-1]:
+                self.addRow(elem)
+
         for elem in self.source.iter():
             self.addRow(elem)
 
@@ -52,8 +75,7 @@ class XmlSheet(Sheet):
         self.rows.append(elem)
         for k in elem.attrib:
             if k not in self.attribcols:
-                c = Column(unns(k), getter=lambda c,r,k=k: r.attrib.get(k),
-                                    setter=lambda c,r,v,k=k: setitem(r.attrib, k, v))
+                c = AttribColumn(unns(k), k)
                 self.addColumn(c)
                 self.attribcols[k] = c
                 c.nstag = k
