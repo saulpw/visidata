d63a65dad59395c345dc5c1dfc6122531f6de54a
anjakefala <anja.kefala@gmail.com>
2018-07-08 22:50:08 +0000

    [commands] Migrate tidydata.py to new format

--- visidata/tidydata.py
+++ visidata/tidydata.py
@@ -1,7 +1,7 @@
 from visidata import *
 
-globalCommand('M', 'vd.push(MeltedSheet(sheet))', 'open melted sheet (unpivot)', 'data-melt')
-globalCommand('gM', 'vd.push(MeltedSheet(sheet, regex=input("regex to split colname: ", value="(.*)_(.*)", type="regex-capture")))', 'open melted sheet (unpivot), factoring columns', 'data-melt-regex')
+globalCommand('M', 'melt', 'vd.push(MeltedSheet(sheet))')
+globalCommand('gM', 'melt-regex', 'vd.push(MeltedSheet(sheet, regex=input("regex to split colname: ", value="(.*)_(.*)", type="regex-capture")))')
 
 melt_var_colname = 'Variable' # column name to use for the melted variable name
 melt_value_colname = 'Value'  # column name to use for the melted value
