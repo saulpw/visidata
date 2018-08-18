c5760420dd7c26a2a04e2e7e9918f9faf99f6b8c
Saul Pwanson <code@saul.pw>
2018-08-18 03:40:29 +0000

    [http] add content_filetypes mapping

--- visidata/loaders/http.py
+++ visidata/loaders/http.py
@@ -1,5 +1,10 @@
 from visidata import *
 
+content_filetypes = {
+    'tab-separated-values': 'tsv'  # thanks @lindner
+}
+
+
 class HttpPath(PathFd):
     def __init__(self, url, req):
         from urllib.parse import urlparse
@@ -13,7 +18,8 @@ def openurl_http(p, filetype=None):
     r = requests.get(p.url, stream=True)
     if not filetype:
         contenttype = r.headers['content-type']
-        filetype = contenttype.split(';')[0].split('/')[-1]
+        subtype = contenttype.split(';')[0].split('/')[-1]
+        filetype = content_filetypes.get(subtype, subtype)
     return openSource(HttpPath(p.url, r.iter_lines(decode_unicode=True)), filetype=filetype)
 
 openurl_https = openurl_http
