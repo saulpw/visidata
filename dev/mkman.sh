acae2dfecc0f4446459670515459eb90e218e13b
Saul Pwanson <code@saul.pw>
2018-09-13 21:49:35 -0700

    [dev man] mkman.sh; remove vd-man.html

---
+++ dev/mkman.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Usage: $0
+#    builds vd(1) man page in src repo (to be checked in)
+
+# TODO:
+#   - BUILD should be tmpdir and cleaned up afterwards
+#   - parse_options should be moved to bin/
+
+set -e
+
+MAN=$VD/visidata/man
+BUILD=$VD/_build   # should be tmpdir
+
+cp $MAN/* $BUILD/
+$MAN/parse_options.py $BUILD/vd-cli.inc $BUILD/vd-opts.inc
+
+soelim -rt -I $BUILD $BUILD/vd.inc > $BUILD/vd-pre.1
+preconv -r -e utf8 $BUILD/vd-pre.1 > $MAN/vd.1
