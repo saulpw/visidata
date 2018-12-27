83af7afb299cb21d2f745558aa20d907559e705d
Saul Pwanson <code@saul.pw>
2018-12-26 22:17:57 -0800

    [sqlite] asyncthread reload

--- visidata/loaders/sqlite.py
+++ visidata/loaders/sqlite.py
@@ -1,36 +1,34 @@
 from visidata import ENTER, Sheet, ColumnItem, anytype, status, clean_to_id, Progress, asyncthread, currency, Path
 
 def open_sqlite(path):
-    vs = SqliteSheet(path.name + '_tables', path, 'sqlite_master')
-    vs.columns = vs.getColumns('sqlite_master')
-    vs.addCommand(ENTER, 'dive-row', 'vd.push(SqliteSheet(joinSheetnames(source.name, cursorRow[1]), sheet, cursorRow[1]))')
+    vs = SqliteSheet(path.name + '_tables', source=path, tableName='sqlite_master')
+    vs.addCommand(ENTER, 'dive-row', 'vd.push(SqliteSheet(joinSheetnames(source.name, cursorRow[1]), source=sheet, tableName=cursorRow[1]))')
     return vs
 open_db = open_sqlite
 
 
 class SqliteSheet(Sheet):
     'Provide functionality for importing SQLite databases.'
-    def __init__(self, name, pathOrSheet, tableName):
-        super().__init__(name, source=pathOrSheet, tableName=tableName)
-        if isinstance(pathOrSheet, Sheet):
-            self.conn = pathOrSheet.conn
-        elif isinstance(pathOrSheet, Path):
-            import sqlite3
-            self.conn = sqlite3.connect(pathOrSheet.resolve())
 
-    # must not be @asyncthread due to sqlite lib being single-threaded
+    def resolve(self):
+        'Resolve all the way back to the original source Path.'
+        return self.source.resolve()
+
+    @asyncthread
     def reload(self):
+        import sqlite3
+        conn = sqlite3.connect(self.resolve())
         tblname = self.tableName
-        self.columns = self.getColumns(tblname)
-        r = self.conn.execute('SELECT COUNT(*) FROM %s' % tblname).fetchall()
+        self.columns = self.getColumns(tblname, conn)
+        r = conn.execute('SELECT COUNT(*) FROM %s' % tblname).fetchall()
         rowcount = r[0][0]
         self.rows = []
-        for row in Progress(self.conn.execute("SELECT * FROM %s" % tblname), total=rowcount-1):
+        for row in Progress(conn.execute("SELECT * FROM %s" % tblname), total=rowcount-1):
             self.addRow(row)
 
-    def getColumns(self, tableName):
+    def getColumns(self, tableName, conn):
         cols = []
-        for i, r in enumerate(self.conn.execute('PRAGMA TABLE_INFO(%s)' % tableName)):
+        for i, r in enumerate(conn.execute('PRAGMA TABLE_INFO(%s)' % tableName)):
             c = ColumnItem(r[1], i)
             t = r[2].lower()
             if t == 'integer':
