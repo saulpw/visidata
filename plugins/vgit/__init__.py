486b5e652717fbfa4e5ff9e9f5b9ce30013de634
Saul Pwanson <code@saul.pw>
2019-01-06 22:53:42 -0800

    [vgit] many fixes in status and branches sheets

--- plugins/vgit/__init__.py
+++ plugins/vgit/__init__.py
@@ -2,7 +2,13 @@ from .vgit import *
 
 def open_dir(p):
     if p.joinpath('.git').is_dir():
-        return GitStatus(p)
+        vs = GitStatus(p)
+        vs.gitBranchesSheet = GitBranches('branches', source=vs)
+        vs.gitOptionsSheet = GitOptions('git-options', source=vs)
+        vs.gitStashesSheet = GitStashes('stashes', source=vs)
+        vs.gitRemotesSheet = GitRemotes('remotes', source=vs)
+        return vs
+
     return DirSheet(p.name, source=p)
 
 addGlobals(globals())
