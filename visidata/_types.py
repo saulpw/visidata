e5b75a8dd2080536267ac4413325b2641131c0bb
Saul Pwanson <code@saul.pw>
2019-02-03 23:22:09 -0800

    [vlen] allow constructor to take int also

--- visidata/_types.py
+++ visidata/_types.py
@@ -34,7 +34,7 @@ def currency(s=''):
 
 class vlen(int):
     def __new__(cls, v):
-        if isinstance(v, vlen):
+        if isinstance(v, (vlen, int)):
             return super(vlen, cls).__new__(cls, v)
         else:
             return super(vlen, cls).__new__(cls, len(v))
