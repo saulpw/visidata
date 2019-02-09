ec54a592d215f6858d50ce2280725071dfb2caf4
Saul Pwanson <code@saul.pw>
2019-02-08 19:18:01 -0800

    [shell bugfix] commit() takes dicts not .values()

--- visidata/shell.py
+++ visidata/shell.py
@@ -113,7 +113,7 @@ class DeferredSaveSheet(Sheet):
 
         confirm('really %s? ' % cstr)
 
-        self.commit(self.addedRows.values(), changes.values(), self.toBeDeleted.values())
+        self.commit(self.addedRows, changes, self.toBeDeleted)
 
         self.toBeDeleted.clear()
         self.addedRows.clear()
