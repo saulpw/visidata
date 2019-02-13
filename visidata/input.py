40b4e22185d3c77bd04bb98679fa518ebb445f4c
Saul Pwanson <code@saul.pw>
2019-02-12 21:57:18 -0800

    [input nfc] move helpers outside of editline

--- visidata/input.py
+++ visidata/input.py
@@ -10,96 +10,99 @@ vd.lastInputs = collections.defaultdict(collections.OrderedDict)  # [input_type]
 __all__ = ['confirm', 'editline', 'chooseOne', 'chooseMany', 'input_longname']
 
 
+# editline helpers
+
+def until_get_wch(scr):
+    'Ignores get_wch timeouts'
+    ret = None
+    while not ret:
+        try:
+            ret = scr.get_wch()
+        except curses.error:
+            pass
+
+    return ret
+
+def splice(v, i, s):
+    'Insert `s` into string `v` at `i` (such that v[i] == s[0]).'
+    return v if i < 0 else v[:i] + s + v[i:]
+
+def clean_printable(s):
+    'Escape unprintable characters.'
+    return ''.join(c if c.isprintable() else ('<%04X>' % ord(c)) for c in str(s))
+
+def delchar(s, i, remove=1):
+    'Delete `remove` characters from str `s` beginning at position `i`.'
+    return s if i < 0 else s[:i] + s[i+remove:]
+
+class CompleteState:
+    def __init__(self, completer_func):
+        self.comps_idx = -1
+        self.completer_func = completer_func
+        self.former_i = None
+        self.just_completed = False
+
+    def complete(self, v, i, state_incr):
+        self.just_completed = True
+        self.comps_idx += state_incr
+
+        if self.former_i is None:
+            self.former_i = i
+        try:
+            r = self.completer_func(v[:self.former_i], self.comps_idx)
+        except Exception as e:
+            # beep/flash; how to report exception?
+            return v, i
+
+        if not r:
+            # beep/flash to indicate no matches?
+            return v, i
+
+        v = r + v[i:]
+        return v, len(v)
+
+    def reset(self):
+        if self.just_completed:
+            self.just_completed = False
+        else:
+            self.former_i = None
+            self.comps_idx = -1
+
+class HistoryState:
+    def __init__(self, history):
+        self.history = history
+        self.hist_idx = None
+        self.prev_val = None
+
+    def up(self, v, i):
+        if self.hist_idx is None:
+            self.hist_idx = len(self.history)
+            self.prev_val = v
+        if self.hist_idx > 0:
+            self.hist_idx -= 1
+            v = self.history[self.hist_idx]
+        i = len(v)
+        return v, i
+
+    def down(self, v, i):
+        if self.hist_idx is None:
+            return v, i
+        elif self.hist_idx < len(self.history)-1:
+            self.hist_idx += 1
+            v = self.history[self.hist_idx]
+        else:
+            v = self.prev_val
+            self.hist_idx = None
+        i = len(v)
+        return v, i
+
+
 # history: earliest entry first
 def editline(scr, y, x, w, i=0, attr=curses.A_NORMAL, value='', fillchar=' ', truncchar='-', unprintablechar='.', completer=lambda text,idx: None, history=[], display=True):
     'A better curses line editing widget.'
     ESC='^['
     ENTER='^J'
     TAB='^I'
-
-    def until_get_wch():
-        'Ignores get_wch timeouts'
-        ret = None
-        while not ret:
-            try:
-                ret = scr.get_wch()
-            except curses.error:
-                pass
-
-        return ret
-
-    def splice(v, i, s):
-        'Insert `s` into string `v` at `i` (such that v[i] == s[0]).'
-        return v if i < 0 else v[:i] + s + v[i:]
-
-    def clean_printable(s):
-        'Escape unprintable characters.'
-        return ''.join(c if c.isprintable() else ('<%04X>' % ord(c)) for c in str(s))
-
-    def delchar(s, i, remove=1):
-        'Delete `remove` characters from str `s` beginning at position `i`.'
-        return s if i < 0 else s[:i] + s[i+remove:]
-
-    class CompleteState:
-        def __init__(self, completer_func):
-            self.comps_idx = -1
-            self.completer_func = completer_func
-            self.former_i = None
-            self.just_completed = False
-
-        def complete(self, v, i, state_incr):
-            self.just_completed = True
-            self.comps_idx += state_incr
-
-            if self.former_i is None:
-                self.former_i = i
-            try:
-                r = self.completer_func(v[:self.former_i], self.comps_idx)
-            except Exception as e:
-                # beep/flash; how to report exception?
-                return v, i
-
-            if not r:
-                # beep/flash to indicate no matches?
-                return v, i
-
-            v = r + v[i:]
-            return v, len(v)
-
-        def reset(self):
-            if self.just_completed:
-                self.just_completed = False
-            else:
-                self.former_i = None
-                self.comps_idx = -1
-
-    class HistoryState:
-        def __init__(self, history):
-            self.history = history
-            self.hist_idx = None
-            self.prev_val = None
-
-        def up(self, v, i):
-            if self.hist_idx is None:
-                self.hist_idx = len(self.history)
-                self.prev_val = v
-            if self.hist_idx > 0:
-                self.hist_idx -= 1
-                v = self.history[self.hist_idx]
-            i = len(v)
-            return v, i
-
-        def down(self, v, i):
-            if self.hist_idx is None:
-                return v, i
-            elif self.hist_idx < len(self.history)-1:
-                self.hist_idx += 1
-                v = self.history[self.hist_idx]
-            else:
-                v = self.prev_val
-                self.hist_idx = None
-            i = len(v)
-            return v, i
 
     history_state = HistoryState(history)
     complete_state = CompleteState(completer)
@@ -166,7 +169,7 @@ def editline(scr, y, x, w, i=0, attr=curses.A_NORMAL, value='', fillchar=' ', tr
         elif ch == '^R':                           v = str(value)  # ^Reload initial value
         elif ch == '^T':                           v = delchar(splice(v, i-2, v[i-1]), i)  # swap chars
         elif ch == '^U':                           v = v[i:]; i = 0  # clear to beginning
-        elif ch == '^V':                           v = splice(v, i, until_get_wch()); i += 1  # literal character
+        elif ch == '^V':                           v = splice(v, i, until_get_wch(scr)); i += 1  # literal character
         elif ch == '^W':                           j = rfind_nonword(v, 0, i-1); v = v[:j+1] + v[i:]; i = j+1  # erase word
         elif ch == '^Z':                           suspend()
         elif history and ch == 'KEY_UP':           v, i = history_state.up(v, i)
