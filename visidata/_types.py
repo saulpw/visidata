0af53d264d710586151347b01d05820e8e16f646
Saul Pwanson <code@saul.pw>
2019-02-19 22:41:29 -0800

    [types] vlen can also init with float

--- visidata/_types.py
+++ visidata/_types.py
@@ -34,7 +34,7 @@ def currency(s=''):
 
 class vlen(int):
     def __new__(cls, v):
-        if isinstance(v, (vlen, int)):
+        if isinstance(v, (vlen, int, float)):
             return super(vlen, cls).__new__(cls, v)
         else:
             return super(vlen, cls).__new__(cls, len(v))
