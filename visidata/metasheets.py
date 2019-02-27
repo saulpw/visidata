e6fc291bd4ecd587b8bee09168490609cfac2679
Saul Pwanson <code@saul.pw>
2019-02-27 00:20:30 -0800

    [HelpSheet-] rearrange

--- visidata/metasheets.py
+++ visidata/metasheets.py
@@ -146,19 +146,20 @@ class HelpSheet(Sheet):
         from pkg_resources import resource_filename
         cmdlist = TsvSheet('cmdlist', source=Path(resource_filename(__name__, 'commands.tsv')))
         cmdlist.reload_sync()
-        self.cmddict = {}
-        for cmdrow in cmdlist.rows:
-            self.cmddict[(cmdrow.sheet, cmdrow.longname)] = cmdrow
 
-        self.revbinds = {
-            longname:keystrokes
-                for (keystrokes, _), longname in bindkeys.iter(self.source)
-                    if keystrokes not in self.revbinds
-        }
         self.rows = []
         for (k, o), v in commands.iter(self.source):
             self.addRow(v)
             v.sheet = o
+
+        self.cmddict = {}
+        for cmdrow in cmdlist.rows:
+            self.cmddict[(cmdrow.sheet, cmdrow.longname)] = cmdrow
+
+        self.revbinds = {}  # [longname] -> keystrokes
+        for (keystrokes, _), longname in bindkeys.iter(self.source):
+            if keystrokes not in self.revbinds:
+                self.revbinds[longname] = keystrokes
 
 
 class OptionsSheet(Sheet):
