53b922991fd587d68abfc7c43ffdcf5f1d8bcb1f
azjps <azjps@users.noreply.github.com>
2019-04-09 17:35:04 +1200

    [#266] Selection support for PandasSheet
    Override the core selection API in PandasSheet,
    keep track of selections using a bool dtype
    pd.Series instead. Currently looking up rows
    by index, which does have the requirement that
    the DataFrame's index is unique.
    
    The intended benefit is to be able to use vectorization
    for multi-row selection, although this hasn't been
    implemented yet.

--- visidata/loaders/_pandas.py
+++ visidata/loaders/_pandas.py
@@ -51,19 +51,23 @@ class PandasSheet(Sheet):
     #     raise ValueError(self.selectedRows)
 
     def reload(self):
-        import pandas
-        if isinstance(self.source, pandas.DataFrame):
+        import pandas as pd
+        if isinstance(self.source, pd.DataFrame):
             self.df = self.source
         elif isinstance(self.source, Path):
             filetype = getattr(self, 'filetype', self.source.ext[1:])
             if filetype == 'tsv':
                 readfunc = self.read_tsv 
             else:
-                readfunc = getattr(pandas, 'read_'+filetype) or error('no pandas.read_'+filetype)
+                readfunc = getattr(pd, 'read_'+filetype) or error('no pandas.read_'+filetype)
             self.df = readfunc(self.source.resolve(), **options('pandas_'+filetype+'_'))
 
+        # TODO: should we reset the index here and add it as a key column?
         self.columns = [ColumnItem(col, type=self.dtype_to_type(self.df[col])) for col in self.df.columns]
         self.rows = DataFrameAdapter(self.df)
+        self._selectedMask = pd.Series(False, index=self.df.index)
+        if self.df.index.nunique() != self.df.shape[0]:
+            warning("Non-unique index, row selection API may not work or may be incorrect")
 
     @asyncthread
     def sort(self):
@@ -75,6 +79,34 @@ class PandasSheet(Sheet):
             ascending += [not reverse] * len(cols)
         self.rows.sort_values(by=by_cols, ascending=ascending, inplace=True)
 
+    def _checkSelectedIndex(self):
+        if self._selectedMask.index is not self.df.index:
+            # self.df was modified inplace, so the selection
+            # is no longer valid. Likely to heavy but erring
+            # on side of correctness here.
+            self._selectedMask = pd.Series(False, index=self.df.index)
+
+    # TODO: add vectorized implementation for multiple rows
+    def isSelected(self, row):
+        if row is None:
+            return False
+        self._checkSelectedIndex()
+        return self._selectedMask.loc[row.name]
+    def selectRow(self, row):
+        'Select given row. O(log n)'
+        self._checkSelectedIndex()
+        self._selectedMask.loc[row.name] = True
+    def unselectRow(self, row):
+        self._checkSelectedIndex()
+        self._selectedMask.loc[row.name] = False
+    @property
+    def nSelected(self):
+        self._checkSelectedIndex()
+        return self._selectedMask.sum()
+    @property
+    def selectedRows(self):
+        self._checkSelectedIndex()
+        return self.df.loc[self._selectedMask]
 
 def view_pandas(df):
     run(PandasSheet('', source=df))
