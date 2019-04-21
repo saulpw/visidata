5c382547e8249e7d2c573c442364e6ff72615ea8
azjps <azjps@users.noreply.github.com>
2019-04-21 19:00:07 -0400

    [#266] Vectorized implementations for multi-row selection
    Change "gu" to bind to unselect() to use public API.
    Add methods for vectorized implementations for multi-row
    selection and un-selection for PandasSheet. Fix
    np.issubdtype() calls from #265.

--- visidata/loaders/_pandas.py
+++ visidata/loaders/_pandas.py
@@ -24,9 +24,9 @@ class PandasSheet(Sheet):
     def dtype_to_type(self, dtype):
         import numpy as np
         try:
-            if np.issubdtype(dtype, np.int):
+            if np.issubdtype(dtype, np.integer):
                 return int 
-            if np.issubdtype(dtype, np.float):
+            if np.issubdtype(dtype, np.floating):
               return float
             if np.issubdtype(dtype, np.datetime64):
                 return date
@@ -39,16 +39,6 @@ class PandasSheet(Sheet):
         """Partial function for reading TSV files using pd.read_csv"""
         import pandas as pd
         return pd.read_csv(path, sep='\t', **kwargs)
-
-    # @property
-    # def selectedRows(self):
-    #     if len(self._selectedRows) <= 1:
-    #         return list(self._selectedRows.values())
-    #     return [self.rows.iloc[i] for i in range(self.rows.shape[0]) if id(self.rows.iloc[i]) in self._selectedRows]
-
-    # def selectRow(self, row):
-    #     super().selectRow(row)
-    #     raise ValueError(self.selectedRows)
 
     def reload(self):
         import pandas as pd
@@ -82,23 +72,30 @@ class PandasSheet(Sheet):
     def _checkSelectedIndex(self):
         if self._selectedMask.index is not self.df.index:
             # self.df was modified inplace, so the selection
-            # is no longer valid. Likely to heavy but erring
+            # is no longer valid -- just delete it. Maybe too heavy but erring
             # on side of correctness here.
+            vd.status('pd.DataFrame.index updated, clearing selected {} rows'
+                      .format(self._selectedMask.sum()))
             self._selectedMask = pd.Series(False, index=self.df.index)
 
-    # TODO: add vectorized implementation for multiple rows
+    # Base selection API. Refer to GH #266: using id() will not identify
+    # pandas rows since iterating on rows / selecting rows will return
+    # different copies. Instead, re-implement the selection API by
+    # keeping a boolean pd.Series indicating the selected rows.
     def isSelected(self, row):
         if row is None:
             return False
         self._checkSelectedIndex()
         return self._selectedMask.loc[row.name]
     def selectRow(self, row):
-        'Select given row. O(log n)'
+        'Select given row'
         self._checkSelectedIndex()
         self._selectedMask.loc[row.name] = True
     def unselectRow(self, row):
         self._checkSelectedIndex()
+        is_selected = self._selectedMask.loc[row.name]
         self._selectedMask.loc[row.name] = False
+        return is_selected
     @property
     def nSelected(self):
         self._checkSelectedIndex()
@@ -106,7 +103,23 @@ class PandasSheet(Sheet):
     @property
     def selectedRows(self):
         self._checkSelectedIndex()
-        return self.df.loc[self._selectedMask]
+        return DataFrameAdapter(self.df.loc[self._selectedMask])
+    # Vectorized implementation of multi-row selections
+    def select(self, rows, status=True, progress=True):
+        self._checkSelectedIndex()
+        self._selectedMask.loc[[row.name for row in rows]] = True
+    def unselect(self, rows, status=True, progress=True):
+        self._checkSelectedIndex()
+        self._selectedMask.loc[[row.name for row in rows]] = False
+    def selectByIndex(self, start=None, end=None):
+        self._checkSelectedIndex()
+        self._selectedMask.loc[start:end] = True
+    def unselectByIndex(self, start=None, end=None):
+        self._checkSelectedIndex()
+        self._selectedMask.loc[start:end] = False
+    def toggleByIndex(self, start=None, end=None):
+        self._checkSelectedIndex()
+        self._selectedMask.loc[start:end] = ~select._selectedMask
 
 def view_pandas(df):
     run(PandasSheet('', source=df))
@@ -119,3 +132,17 @@ def open_dta(p):
     return PandasSheet(p.name, source=p, filetype='stata')
 
 open_stata = open_pandas
+
+# Override with vectorized implementations
+PandasSheet.addCommand('gt', 'stoggle-rows', 'toggle(rows)', undo=undoSheetSelection),
+PandasSheet.addCommand('gs', 'select-rows', 'select(rows)', undo=undoSheetSelection),
+PandasSheet.addCommand('gu', 'unselect-rows', 'unselect(rows)', undo=undoSheetSelection),
+
+PandasSheet.addCommand('zt', 'stoggle-before', 'toggle(rows[:cursorRowIndex])', undo=undoSheetSelection),
+PandasSheet.addCommand('zs', 'select-before', 'select(rows[:cursorRowIndex])', undo=undoSheetSelection),
+PandasSheet.addCommand('zu', 'unselect-before', 'unselect(rows[:cursorRowIndex])', undo=undoSheetSelection),
+PandasSheet.addCommand('gzt', 'stoggle-after', 'toggle(rows[cursorRowIndex:])', undo=undoSheetSelection),
+PandasSheet.addCommand('gzs', 'select-after', 'select(rows[cursorRowIndex:])', undo=undoSheetSelection),
+PandasSheet.addCommand('gzu', 'unselect-after', 'unselect(rows[cursorRowIndex:])', undo=undoSheetSelection),
+
+
