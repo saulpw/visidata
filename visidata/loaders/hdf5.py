d3fda8d83ec7dcdfdaed90c10b519c6651b585d4
saulpw <code@saul.pw>
2019-08-21 23:26:44 -0700

    [hdf5] SheetH5Obj rows now represent unloaded sheets #214
    This permits indexing into sub-sheets through CLI (`+toplevel:subsheet::`).

--- visidata/loaders/hdf5.py
+++ visidata/loaders/hdf5.py
@@ -5,13 +5,16 @@ class SheetH5Obj(Sheet):
     def reload(self):
         import h5py
         if isinstance(self.source, h5py.Group):
-            self.rowtype = 'objects'
+            self.rowtype = 'sheets'
             self.columns = [
-                Column(self.source.name, type=str, getter=lambda col,row: row.name.split('/')[-1]),
-                Column('type', type=str, getter=lambda col,row: type(row).__name__),
-                Column('nItems', type=int, getter=lambda col,row: len(row)),
+                Column(self.source.name, type=str, getter=lambda col,row: row.source.name.split('/')[-1]),
+                Column('type', type=str, getter=lambda col,row: type(row.source).__name__),
+                Column('nItems', type=int, getter=lambda col,row: len(row.source)),
             ]
-            self.rows = [ self.source[objname] for objname in self.source.keys() ]
+            self.rows = []
+            for k, v in self.source.items():
+                subname = joinSheetnames(self.name, k)
+                self.addRow(SheetH5Obj(subname, source=v))
         elif isinstance(self.source, h5py.Dataset):
             if len(self.source.shape) == 1:
                 self.columns = [ColumnItem(colname, colname) for colname in self.source.dtype.names or [0]]
@@ -26,7 +29,7 @@ class SheetH5Obj(Sheet):
             status('unknown h5 object type %s' % type(self.source))
         self.recalc()
 
-SheetH5Obj.addCommand(ENTER, 'dive-row', 'vd.push(SheetH5Obj(joinSheetnames(name,cursorRow.name), source=cursorRow))')
+SheetH5Obj.addCommand(ENTER, 'dive-row', 'vd.push(cursorRow)')
 SheetH5Obj.addCommand('A', 'dive-metadata', 'vd.push(SheetDict(cursorRow.name + "_attrs", source=cursorRow.attrs))')
 
 class open_hdf5(SheetH5Obj):
