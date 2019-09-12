9228dc27171fc7554ad72a1733fb0e55d041ded6
Saul Pwanson <code@saul.pw>
2019-09-11 22:03:55 -0700

    [http nfc] cleanup for book

--- visidata/loaders/http.py
+++ visidata/loaders/http.py
@@ -1,17 +1,25 @@
-from visidata import *
+from visidata import Path, RepeatFile, openSource
 
 content_filetypes = {
     'tab-separated-values': 'tsv'  # thanks @lindner
 }
 
 
-def openurl_http(p, filetype=None):
+def openurl_http(path, filetype=None):
     import requests
-    r = requests.get(p.given, stream=True)
+
+    response = requests.get(path.given, stream=True)
+
+    # if filetype not given, auto-detect with hacky mime-type parse
     if not filetype:
-        contenttype = r.headers['content-type']
+        contenttype = response.headers['content-type']
         subtype = contenttype.split(';')[0].split('/')[-1]
         filetype = content_filetypes.get(subtype, subtype)
-    return openSource(Path(p.given, fp=RepeatFile(iter_lines=r.iter_lines(decode_unicode=True))), filetype=filetype)
+
+    # create resettable iterator over contents
+    fp = RepeatFile(iter_lines=response.iter_lines(decode_unicode=True))
+
+    # call open_<filetype> with a usable Path
+    return openSource(Path(path.given, fp=fp, filetype=filetype))
 
 openurl_https = openurl_http
