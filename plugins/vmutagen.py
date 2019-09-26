ea8da07bfa9929dd131943d1a5945215752996c7
Saul Pwanson <code@saul.pw>
2019-09-25 23:18:23 -0700

    [plugin] vmutagen for audio tags on DirSheet
    - Alt+m to add the mutagen columns on the DirSheet

---
+++ plugins/vmutagen.py
@@ -0,0 +1,22 @@
+# requirements: mutagen
+
+import functools
+
+from visidata import *
+
+@functools.lru_cache(None)
+def get_mutagen(path):
+    import mutagen
+    m = mutagen.File(path)
+    return m.info
+
+
+def MutagenColumns():
+    for attr in 'bitrate channels encoder_info encoder_settings frame_offset length mode padding protected sample_rate track_gain'.split():
+        yield Column(attr, getter=lambda c,r,k=attr: getattr(get_mutagen(r), k))
+
+
+DirSheet.addCommand('^[m', 'addcol-mutagen', 'for c in MutagenColumns(): addColumn(c)')
+
+
+addGlobals(globals())
