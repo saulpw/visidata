7bb276e81e5ea3d960d2d24498cc0c5d87c91432
Saul Pwanson <code@saul.pw>
2019-10-15 00:04:55 -0700

    [csv] update to iterload api

--- visidata/loaders/csv.py
+++ visidata/loaders/csv.py
@@ -13,9 +13,6 @@ csv.field_size_limit(2**31-1) # Windows has max 32-bit
 
 options_num_first_rows = 10
 
-def open_csv(p):
-    return CsvSheet(p.name, source=p)
-
 def wrappedNext(rdr):
     try:
         return next(rdr)
@@ -27,74 +24,69 @@ def removeNulls(fp):
         yield line.replace('\0', '')
 
 class CsvSheet(Sheet):
-    _rowtype = list
-#    _coltype = ColumnItem
-    @asyncthread
-    def reload(self):
-        load_csv(self)
+    _rowtype = list  # rowdef: list of values
 
     def newRow(self):
         return [None]*len(self.columns)
 
-def csvoptions():
-    return options('csv_')
-
-def load_csv(vs):
+@CsvSheet.api
+def iterload(vs):
     'Convert from CSV, first handling header row specially.'
     with vs.source.open_text() as fp:
         for i in range(options.skip):
             wrappedNext(fp)  # discard initial lines
 
         if options.safety_first:
-            rdr = csv.reader(removeNulls(fp), **csvoptions())
+            rdr = csv.reader(removeNulls(fp), **options('csv_'))
         else:
-            rdr = csv.reader(fp, **csvoptions())
+            rdr = csv.reader(fp, **options('csv_'))
 
         vs.rows = []
 
         # headers first, to setup columns before adding rows
         headers = [wrappedNext(rdr) for i in range(int(options.header))]
 
+        vs.columns = []
         if headers:
             # columns ideally reflect the max number of fields over all rows
-            vs.columns = [ColumnItem(colname, i)
-                    for i, colname in enumerate('\\n'.join(x) for x in zip(*headers))]
+            for i, colname in enumerate('\\n'.join(x) for x in zip(*headers)):
+                vs.addColumn(ColumnItem(colname, i))
         else:
-            r = wrappedNext(rdr)
-            vs.addRow(r)
-            ncols = len(vs.rows[0])
-            vs.columns = [ColumnItem('', i, width=8) for i in range(ncols)]
+            row = wrappedNext(rdr)
+            ncols = len(row)
+            for i in range(ncols):
+                vs.addColumn(ColumnItem('', i, width=8))
+            yield row
 
         if not vs.columns:
-            vs.columns = [ColumnItem(0)]
+            vs.addColumn(ColumnItem(0))
 
-        vs.recalc()  # make columns usable
         with Progress(total=filesize(vs.source)) as prog:
             try:
                 samplelen = 0
                 for i in range(options_num_first_rows):  # for progress below
                     row = wrappedNext(rdr)
-                    vs.addRow(row)
+                    yield row
                     samplelen += sum(len(x) for x in row)
 
                 samplelen //= options_num_first_rows  # avg len of first n rows
 
                 while True:
-                    vs.addRow(wrappedNext(rdr))
+                    yield wrappedNext(rdr)
                     prog.addProgress(samplelen)
             except StopIteration:
                 pass  # as expected
 
-    vs.recalc()
-    return vs
 
 @Sheet.api
 def save_csv(sheet, p):
     'Save as single CSV file, handling column names as first line.'
     with p.open_text(mode='w') as fp:
-        cw = csv.writer(fp, **csvoptions())
+        cw = csv.writer(fp, **options('csv_'))
         colnames = [col.name for col in sheet.visibleCols]
         if ''.join(colnames):
             cw.writerow(colnames)
         for r in Progress(sheet.rows, 'saving'):
             cw.writerow([col.getDisplayValue(r) for col in sheet.visibleCols])
+
+vd.filetype('csv', CsvSheet)
