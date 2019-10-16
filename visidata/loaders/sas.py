792d38a7e6db69ff642e32632d0f06474ed2c255
Saul Pwanson <code@saul.pw>
2019-10-15 23:54:11 -0700

    [sas] update to iterload

--- visidata/loaders/sas.py
+++ visidata/loaders/sas.py
@@ -7,16 +7,8 @@ SASTypes = {
 }
 
 
-def open_xpt(p):
-    return XptSheet(p.name, source=p)
-
-def open_sas7bdat(p):
-    return SasSheet(p.name, source=p)
-
-
 class XptSheet(Sheet):
-    @asyncthread
-    def reload(self):
+    def iterload(self):
         import xport
         with open(self.source, 'rb') as fp:
             self.rdr = xport.Reader(fp)
@@ -25,14 +17,11 @@ class XptSheet(Sheet):
             for i, var in enumerate(self.rdr._variables):
                 self.addColumn(ColumnItem(var.name, i, type=float if var.numeric else str))
 
-            self.rows = []
-            for row in self.rdr:
-                self.addRow(row)
+            yield from self.rdr
 
 
 class SasSheet(Sheet):
-    @asyncthread
-    def reload(self):
+    def iterload(self):
         import sas7bdat
         self.dat = sas7bdat.SAS7BDAT(str(self.source), skip_header=True, log_level=logging.CRITICAL)
         self.columns = []
@@ -40,6 +29,8 @@ class SasSheet(Sheet):
             self.addColumn(ColumnItem(col.name.decode('utf-8'), col.col_id, type=SASTypes.get(col.type, anytype)))
 
         with self.dat as fp:
-            self.rows = []
-            for row in Progress(fp, total=self.dat.properties.row_count):
-                self.addRow(row)
+            yield from Progress(fp, total=self.dat.properties.row_count)
+
+
+vd.filetype('xpt', XptSheet)
+vd.filetype('sas7bdat', SasSheet)
