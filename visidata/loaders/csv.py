f63320b4b3073f41b62e0a31f4ac85fd98cd9e9b
Saul Pwanson <code@saul.pw>
2019-11-03 20:07:32 -0800

    [csv] update/cleanup/simplify to iterload/setCols

--- visidata/loaders/csv.py
+++ visidata/loaders/csv.py
@@ -29,54 +29,27 @@ class CsvSheet(Sheet):
     def newRow(self):
         return [None]*len(self.columns)
 
-@CsvSheet.api
-def iterload(vs):
-    'Convert from CSV, first handling header row specially.'
-    with vs.source.open_text() as fp:
-        for i in range(options.skip):
-            wrappedNext(fp)  # discard initial lines
+    def iterload(self):
+        'Convert from CSV, first handling header row specially.'
+        with self.source.open_text() as fp:
+            if options.safety_first:
+                rdr = csv.reader(removeNulls(fp), **options('csv_'))
+            else:
+                rdr = csv.reader(fp, **options('csv_'))
 
-        if options.safety_first:
-            rdr = csv.reader(removeNulls(fp), **options('csv_'))
-        else:
-            rdr = csv.reader(fp, **options('csv_'))
+            yield from rdr
 
-        vs.rows = []
-
-        # headers first, to setup columns before adding rows
-        headers = [wrappedNext(rdr) for i in range(int(options.header))]
-
-        vs.columns = []
+    def setCols(self, headers):
+        self.columns = []
         if headers:
             # columns ideally reflect the max number of fields over all rows
             for i, colname in enumerate('\\n'.join(x) for x in zip(*headers)):
-                vs.addColumn(ColumnItem(colname, i))
-        else:
-            row = wrappedNext(rdr)
-            ncols = len(row)
-            for i in range(ncols):
-                vs.addColumn(ColumnItem('', i, width=8))
-            yield row
+                self.addColumn(ColumnItem(colname, i))
 
-        if not vs.columns:
-            vs.addColumn(ColumnItem(0))
-
-        with Progress(total=filesize(vs.source)) as prog:
-            try:
-                samplelen = 0
-                for i in range(options_num_first_rows):  # for progress below
-                    row = wrappedNext(rdr)
-                    yield row
-                    samplelen += sum(len(x) for x in row)
-
-                samplelen //= options_num_first_rows  # avg len of first n rows
-
-                while True:
-                    yield wrappedNext(rdr)
-                    prog.addProgress(samplelen)
-            except StopIteration:
-                pass  # as expected
-
+    def addRow(self, row):
+        super().addRow(row)
+        for i in range(len(row)-self.nCols):
+            self.addColumn(ColumnItem('', self.nCols))
 
 @Sheet.api
 def save_csv(sheet, p):
