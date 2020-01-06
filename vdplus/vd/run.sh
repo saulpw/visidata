df4df1ee19a1a91e79132e1cd83fb9c0fcbc7d17
Thomas Buckley-Houston <tom@tombh.co.uk>
2020-01-06 16:22:12 +0800

    vd: use parallel instead of xargs to log commands

--- vdplus/vd/run.sh
+++ vdplus/vd/run.sh
@@ -11,4 +11,4 @@ GOTTY_PORT=${GOTTY_PORT:-9000}
 while ! ls $CMD_LOG > /dev/null 2>&1; do sleep 0.1; done
 
 # Add the command history to the container's output
-tail -f $CMD_LOG | xargs -I{} echo "VDCMD: {}"
+parallel --tagstring "VDCMD:" --line-buffer tail -f {} ::: $CMD_LOG
