20d6caf661c007815d4805c2d7c14113113b15b4
anjakefala <anja.kefala@gmail.com>
2020-01-05 20:37:42 -0800

    [slide-] fix sliding of reg col when >0 key cols
    - regular cols cannot slide to the left of a key col
    - otherwise, use visibleCol indexing

--- visidata/slide.py
+++ visidata/slide.py
@@ -53,7 +53,11 @@ def moveVisibleCol(sheet, fromVisColIdx, toVisColIdx):
     if 0 <= toVisColIdx < sheet.nVisibleCols:
         fromVisColIdx = min(max(fromVisColIdx, 0), sheet.nVisibleCols-1)
         fromColIdx = sheet.columns.index(sheet.visibleCols[fromVisColIdx])
-        toColIdx = sheet.columns.index(sheet.nonKeyVisibleCols[toVisColIdx])
+        # a regular column cannot move to the left of keycols
+        if toVisColIdx <= 0:
+            toColIdx = sheet.columns.index(sheet.nonKeyVisibleCols[toVisColIdx])
+        else:
+            toColIdx = sheet.columns.index(sheet.visibleCols[toVisColIdx])
         moveListItem(sheet.columns, fromColIdx, toColIdx)
         return toVisColIdx
     else:
