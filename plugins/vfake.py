85f68080ae2bed9deaff5aead337d2494cd044d0
AJ Kerrigan <kerrigan.aj@gmail.com>
2020-01-07 15:34:08 -0500

    Allow vfake to load additional providers
    After instantiating Faker, load additional providers from a user-defined
    list.

--- plugins/vfake.py
+++ plugins/vfake.py
@@ -6,11 +6,20 @@ __version__ = '0.9'
 
 option('locale', 'en_US', 'default locale to use for Faker', replay=True)
 
+# See also: https://faker.readthedocs.io/en/master/communityproviders.html
+option('vfake_extra_providers', [], 'list of additional Provider classes to load via add_provider()', replay=True)
+
 @Column.api
 @asyncthread
 def setValuesFromFaker(col, faketype, rows):
     import faker
     fake = faker.Faker(options.locale)
+    invalid_provider_warning = 'Custom vfake provider "{}" is not a valid Faker Provider class, skipping add'
+    for provider in options.vfake_extra_providers:
+        if not issubclass(provider, faker.providers.BaseProvider):
+            vd.warning(invalid_provider_warning.format(provider.__name__))
+            continue
+        fake.add_provider(provider)
     fakefunc = getattr(fake, faketype, None) or vd.error('no such faker function')
 
     fakeMap = {}
