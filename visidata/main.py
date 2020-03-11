saulpw/visidata

commit 16832ca0c7352d71e138d96b0a737575bb51e74e

[api] deprecate globalCommand; use BaseSheet.addCommand
- remove vd.addCommand
- remove custom globalCommand

visidata/settings.py
@@ -100,13 +100,6 @@ class Command:
         self.execstr = execstr
         self.helpstr = helpstr
 
-def globalCommand(keystrokes, longname, execstr, helpstr='', **kwargs):
-    commands.setdefault(longname, Command(longname, execstr, helpstr=helpstr, **kwargs))
-
-    if keystrokes:
-        assert not bindkeys._get(keystrokes), keystrokes
-        bindkeys.setdefault(keystrokes, longname)
-
 
 class Option:
     def __init__(self, name, value, helpstr=''):
@@ -219,10 +212,6 @@ def option(name, default, helpstr, replay=False):
 
 theme = option
 
-@VisiData.api
-def addCommand(vd, keystrokes, longname, execstr, helpstr='', **kwargs):
-    globalCommand(keystrokes, longname, execstr, helpstr=helpstr, **kwargs)
-
 @BaseSheet.class_api
 @classmethod
 def addCommand(cls, keystrokes, longname, execstr, helpstr='', **kwargs):
@@ -238,6 +227,7 @@ def _command(cls, binding, longname, helpstr, **kwargs):
     return decorator
 
 BaseSheet.command = classmethod(_command)
+globalCommand = BaseSheet.addCommand
 
 @BaseSheet.class_api
 @classmethod

visidata/undo.py
@@ -86,5 +86,5 @@ def addUndoSetValues(vd, cols, rows):
     vd.addUndo(_undo)
 
 
-vd.addCommand('U', 'undo-last', 'vd.undo(sheet)', 'undo the most recent modification (requires enabled options.undo)')
-vd.addCommand('R', 'redo-last', 'vd.redo(sheet)', 'redo the most recent undo (requires enabled options.undo)')
+BaseSheet.addCommand('U', 'undo-last', 'vd.undo(sheet)', 'undo the most recent modification (requires enabled options.undo)')
+BaseSheet.addCommand('R', 'redo-last', 'vd.redo(sheet)', 'redo the most recent undo (requires enabled options.undo)')

commit 789702e98bcef52dd7f399b7f22d03e579990263

[preplay-] use sheet as context obj to getCommand
Fixes bug introduced by: 16832ca0c7352d71e138d96b0a737575bb51e74e.
At -P preplay, longnames were not being found.

visidata/main.py
@@ -10,7 +10,7 @@ import io
 import sys
 import locale
 
-from visidata import vd, option, options, status, run, Sheet
+from visidata import vd, option, options, status, run, BaseSheet
 from visidata import Path, openSource, saveSheets, setDiffSheet, domotd
 import visidata
 
@@ -128,7 +128,7 @@ def main_vd():
         vd.execAsync = lambda func, *args, **kwargs: func(*args, **kwargs) # disable async
 
     for cmd in args.preplay:
-        Sheet('').execCommand(cmd)
+        BaseSheet('').execCommand(cmd)
 
     if not args.play:
         if flPipedInput and not inputs:  # '|vd' without explicit '-'

visidata/settings.py
@@ -245,16 +245,17 @@ def unbindkey(cls, keystrokes):
     vd.bindkeys.unset(keystrokes, cls)
 
 @BaseSheet.api
-def getCommand(self, keystrokes_or_longname):
-    if isinstance(keystrokes_or_longname, Command):
-        return keystrokes_or_longname
+def getCommand(sheet, cmd):
+    'Return the Command for the given arg, which may be keystrokes, longname, or a Command itself, within the context of `sheet`.'
+    if isinstance(cmd, Command):
+        return cmd
 
-    longname = vd.bindkeys._get(keystrokes_or_longname)
+    longname = vd.bindkeys._get(cmd, obj=sheet)
     try:
         if longname:
-            return vd.commands._get(longname) or vd.fail('no command "%s"' % longname)
+            return vd.commands._get(longname, obj=sheet) or vd.fail('no command "%s"' % longname)
         else:
-            return vd.commands._get(keystrokes_or_longname) or vd.fail('no binding for %s' % keystrokes_or_longname)
+            return vd.commands._get(cmd, obj=sheet) or vd.fail('no binding for %s' % cmd)
     except Exception as exc:
         return None
 