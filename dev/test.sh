56723a9b2062d99d8430bd7b9dc7d1ada9544c39
anjakefala <anja.kefala@gmail.com>
2020-04-19 18:38:45 -0700

    [tests] expand test.sh to work with non-tsv goldens
    - golden outputs are determined from existing files in tests/golden/
    - remove -nofail suffix

--- dev/test.sh
+++ dev/test.sh
@@ -7,8 +7,10 @@ set -e
 trap "echo aborted; exit;" SIGINT SIGTERM
 
 if [ -z "$1" ] ; then
+    # test.sh; run all .vds in tests/
     TESTS="tests/*.vd"
 else
+    # test.sh testname; run tests/testname.vd
     TESTS=tests/$1.vd
 fi
 
@@ -17,32 +19,12 @@ for i in $TESTS ; do
     outbase=${i##tests/}
     if [ "${i%-nosave.vd}-nosave" == "${i%.vd}" ];
     then
-        :
-    elif [ "${i%-nofail.vd}-nofail" == "${i%.vd}" ];
-    then
-        PYTHONPATH=. bin/vd --confirm-overwrite=False --play "$i" --batch --output tests/golden/"${outbase%.vd}".tsv
-    else
-        :
-    fi
-done
-
-echo '=== git diffs ==='
-git --no-pager diff tests/
-git --no-pager diff --numstat tests/
-git checkout tests/golden/
-echo '==================================================='
-
-for i in $TESTS ; do
-    echo "--- $i"
-    outbase=${i##tests/}
-    if [ "${i%-nosave.vd}-nosave" == "${i%.vd}" ];
-    then
         PYTHONPATH=. bin/vd --play "$i" --batch
-    elif [ "${i%-nofail.vd}-nofail" == "${i%.vd}" ];
-    then
-        :
     else
-        PYTHONPATH=. bin/vd --confirm-overwrite=False --play "$i" --batch --output tests/golden/"${outbase%.vd}".tsv
+        for goldfn in tests/golden/${outbase%.vd}.*; do
+            PYTHONPATH=. bin/vd --confirm-overwrite=False --play "$i" --batch --output "$goldfn"
+            echo "save: $goldfn"
+        done
     fi
 done
 
