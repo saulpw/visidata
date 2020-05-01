0fad1655e35e9d699a3209bcad50b39f6f7a2f3a
AJ Kerrigan <kerrigan.aj@gmail.com>
2020-05-01 01:33:18 -0400

    don't check for the keyCol of an empty cursorCol

--- visidata/sheets.py
+++ visidata/sheets.py
@@ -505,7 +505,7 @@ class TableSheet(BaseSheet):
                 if self.cursorRowIndex > bottomRowIndex and y+h > self.nScreenRows:
                     self._topRowIndex += bottomRowIndex-self.cursorRowIndex+2
 
-        if self.cursorCol.keycol:
+        if self.cursorCol and self.cursorCol.keycol:
             return
 
         if self.leftVisibleColIndex >= self.cursorVisibleColIndex:
