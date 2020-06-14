84deed19f0a6c01eac196c165b7cba11177c2829
Molley <matthew.holley@mandiant.com>
2020-06-14 10:39:31 +0400

    long sheet names now accomodated

--- visidata/loaders/xlsx.py
+++ visidata/loaders/xlsx.py
@@ -5,7 +5,7 @@ class XlsxIndexSheet(IndexSheet):
     'Load XLSX file (in Excel Open XML format).'
     rowtype = 'sheets'  # rowdef: xlsxSheet
     columns = [
-        Column('sheet', getter=lambda col,row: row.source.title),  # xlsx sheet title
+        Column('sheet', getter=lambda col,row: row.source.title or row.source.name),  # xlsx sheet title
         ColumnAttr('name', width=0),  # visidata Sheet name
         ColumnAttr('nRows', type=int),
         ColumnAttr('nCols', type=int),
@@ -53,7 +53,27 @@ class XlsSheet(SequenceSheet):
         for rownum in Progress(range(worksheet.nrows)):
             yield list(worksheet.cell(rownum, colnum).value for colnum in range(worksheet.ncols))
 
-            
+
+def excel_safename(name, used_names):
+    'convert sheet names to comply with Excel standards'
+
+    # name does not exceed 31 characters
+    excel_limit = 31
+    if len(name) > excel_limit:
+        short_name = name[:excel_limit]
+        used_names.append(short_name)
+        suffix = '_' + str(used_names.count(short_name))
+        name = short_name[:-len(suffix)] + suffix
+
+    # name does not contain any of the following characters:  :  \  /  ?  *  [  or  ]
+    name = ''.join([ '_' if c in '\\/?*[]' else c for c in name])
+
+    # strip leading and trailing _
+    name.strip("_")
+
+    return name, used_names
+
+
 @VisiData.api
 def save_xlsx(vd, p, *sheets):
     import openpyxl
@@ -61,8 +81,11 @@ def save_xlsx(vd, p, *sheets):
     wb = openpyxl.Workbook()
     wb.remove_sheet(wb['Sheet'])
 
+    used_names = []
     for vs in sheets:
-        ws = wb.create_sheet(title=vs.name)
+
+        vs_name, used_names = excel_safename(vs.name, used_names)
+        ws = wb.create_sheet(title=vs_name)
 
         headers = [col.name for col in vs.visibleCols]
         ws.append(headers)
@@ -79,10 +102,11 @@ def save_xlsx(vd, p, *sheets):
 
             ws.append(row)
 
-    wb.active = ws
-
     wb.save(filename=p)
     status(f'{p} save finished')
+
+    if len(used_names) > 0:
+        warning('long sheet names truncated during save')
 
 
 @VisiData.api
@@ -91,9 +115,11 @@ def save_xls(vd, p, *sheets):
 
     wb = xlwt.Workbook()
 
+    used_names = []
     for vs in sheets:
 
-        ws1 = wb.add_sheet(vs.name[:32]) # sheet name can not be longer than 32 characters
+        vs_name, used_names = excel_safename(vs.name, used_names)
+        ws1 = wb.add_sheet(vs_name)
 
         for col_i, col in enumerate(vs.visibleCols):
             ws1.write(0, col_i, col.name)
@@ -105,6 +131,10 @@ def save_xls(vd, p, *sheets):
 
     wb.save(p)
     status(f'{p} save finished')
+
+    if len(used_names) > 0:
+        warning('long sheet names truncated during save')
+
 
 vd.filetype('xlsx', XlsxIndexSheet)
 vd.filetype('xls', XlsIndexSheet)
