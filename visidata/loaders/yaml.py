9f525cd4ea9f9e0fa6132fb3f33cd70bc683ad01
Thomas Sibley <tom@zulutango.org>
2020-06-17 13:33:02 -0700

    [yaml] Support files containing multiple documents
    The loader previously threw an exception when given a YAML file with
    multiple documents.  Now it successfully loads the file, with each
    document presented as a row.  All other existing behaviour remains the
    same.

--- visidata/loaders/yaml.py
+++ visidata/loaders/yaml.py
@@ -1,19 +1,39 @@
 from visidata import *
+from itertools import chain
 
 
 class YamlSheet(JsonSheet):
     def iterload(self):
         import yaml
         with self.source.open_text() as fp:
-            data = yaml.safe_load(fp)
+            documents = yaml.safe_load_all(fp)
 
-        self.columns = []
-        self.colnames = {}
+            self.columns = []
+            self.colnames = {}
 
-        if not isinstance(data, list):
-            yield data
-        else:
-            yield from Progress(data)
+            # Peek at the document stream to determine how to best DWIM.
+            #
+            # This code is a bit verbose because it avoids slurping the generator
+            # all at once into memory.
+            try:
+                first = next(documents)
+            except StopIteration:
+                # Empty file‽
+                yield None
+                return
+
+            try:
+                second = next(documents)
+            except StopIteration:
+                if isinstance(first, list):
+                    # A file with a single YAML list: yield one row per list item.
+                    yield from Progress(first)
+                else:
+                    # A file with a single YAML non-list value, e.g a dict.
+                    yield first
+            else:
+                # A file containing multiple YAML documents: yield one row per document.
+                yield from Progress(chain([first, second], documents), total=0)
 
 
 vd.filetype('yml', YamlSheet)
