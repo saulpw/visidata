56b272f5472ac5f5c8984c7bfeba52cc4c903d12
anjakefala <anja.kefala@gmail.com>
2020-06-16 23:47:14 -0700

    [types] do not group numeric columns by thousands by default.
    - add tutorial for how to group by thousands

--- visidata/_types.py
+++ visidata/_types.py
@@ -40,7 +40,7 @@ def numericFormatter(fmtstr, typedval):
     try:
         fmtstr = fmtstr or options['disp_'+type(typedval).__name__+'_fmt']
         if fmtstr[0] == '%':
-            return locale.format_string(fmtstr, typedval, grouping=True)
+            return locale.format_string(fmtstr, typedval, grouping=False)
         else:
             return fmtstr.format(typedval)
     except ValueError:
