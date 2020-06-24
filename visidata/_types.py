dca6838dce6b97af931a8165dc54c31d50ebe944
saulpw <code@saul.pw>
2020-06-23 23:10:06 -0700

    [currency] handle default currency() case
    - calling int() and float() returns a default value; currency() should
    do so similar
    - in the process, simplify currency() to be a function that returns a
    new float, instead of sub-classing float
    - fixes numeric-binning bug with currency type columns

--- visidata/_types.py
+++ visidata/_types.py
@@ -81,12 +81,11 @@ def isNumeric(col):
 ##
 
 floatchars='+-0123456789.'
-class currency(float):
-    def __new__(self, s=''):
-        'dirty float (strip non-numeric characters)'
-        if isinstance(s, str):
-            s = ''.join(ch for ch in s if ch in floatchars)
-        return float.__new__(self, s)
+def currency(*args):
+    'dirty float (strip non-numeric characters)'
+    if args and isinstance(args[0], str):
+        args = [''.join(ch for ch in args[0] if ch in floatchars)]
+    return float(*args)
 
 
 class vlen(int):
