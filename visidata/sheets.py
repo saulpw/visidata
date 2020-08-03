ebffd142efd1a0f07bc0ddc856b0a1a403e557a6
Saul Pwanson <code@saul.pw>
2020-08-02 21:12:28 -0700

    [loaders-] do not error on uneven header rows

--- visidata/sheets.py
+++ visidata/sheets.py
@@ -832,7 +832,7 @@ class SequenceSheet(Sheet):
     'For sheets which use ColumnItem on rows that are Python sequences (list, namedtuple, etc).'
     def setCols(self, headerrows):
         self.columns = []
-        for i, colnamelines in enumerate(itertools.zip_longest(*headerrows)):
+        for i, colnamelines in enumerate(itertools.zip_longest(*headerrows, fillvalue='')):
             self.addColumn(ColumnItem(''.join(colnamelines), i))
 
         self._rowtype = namedlist('tsvobj', [(c.name or '_') for c in self.columns])
