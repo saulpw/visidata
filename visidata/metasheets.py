dc9b108b69f21efcef58938ab7853a336b3067b4
Saul Pwanson <code@saul.pw>
2020-08-15 23:12:10 -0700

    [columns] unhide height attribute by default #660

--- visidata/metasheets.py
+++ visidata/metasheets.py
@@ -34,7 +34,7 @@ class ColumnsSheet(Sheet):
             ColumnAttr('sheet', type=str),
             ColumnAttr('name', width=options.default_width),
             ColumnAttr('width', type=int),
-            ColumnAttr('height', type=int, width=0),
+            ColumnAttr('height', type=int),
             ColumnAttr('hoffset', type=int, width=0),
             ColumnAttr('voffset', type=int, width=0),
             ColumnEnum('type', getGlobals(), default=anytype),
