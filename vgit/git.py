759f0e07534040abe14254b3c9e57209069ab448
Saul Pwanson <code@saul.pw>
2020-09-02 00:33:18 -0700

    [vgit-] use <class>.class_options.opt_name

--- vgit/git.py
+++ vgit/git.py
@@ -218,7 +218,7 @@ class GitSheet(GitContext, Sheet):
 GitSheet.addCommand('f', 'git-force', 'extra_args.append("--force"); status("--force next git command")', 'add --force to next git command')
 GitSheet.addCommand('^A', 'git-abort', 'abortWhatever()', 'abort the current in-progress action')
 
-GitSheet.options.disp_note_none = ''
+GitSheet.class_options.disp_note_none = ''
 
 BaseSheet.addCommand('gD', 'git-output', 'vd.push(vd.gitcmdlog)', 'show output of git commands this session')
 BaseSheet.addCommand('gi', 'git-exec', 'sheet.git_exec(input("gi", type="git"))')
