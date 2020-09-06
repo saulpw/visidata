27f8eebfa9937a1d40ece1eb76d0dca06873ccc3
anjakefala <anja.kefala@gmail.com>
2020-09-05 21:24:50 -0700

    [SequenceSheet-] handle None column names
    Closes #680

--- visidata/sheets.py
+++ visidata/sheets.py
@@ -842,6 +842,7 @@ class SequenceSheet(Sheet):
     def setCols(self, headerrows):
         self.columns = []
         for i, colnamelines in enumerate(itertools.zip_longest(*headerrows, fillvalue='')):
+            colnamelines = ['' if c is None else c for c in colnamelines]
             self.addColumn(ColumnItem(''.join(colnamelines), i))
 
         self._rowtype = namedlist('tsvobj', [(c.name or '_') for c in self.columns])
