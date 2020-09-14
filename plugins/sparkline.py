51fc32060d87ed2ff362610dd1d72d9288ae60b8
Lucas Messenger <lucas.c.messenger@gmail.com>
2020-09-13 21:03:36 -0400

    Rework sparline for new 2.0 api

--- plugins/sparkline.py
+++ plugins/sparkline.py
@@ -1,37 +1,48 @@
-from visidata import Column, ColumnsSheet, Sheet, option, options, BaseException
+"""
+Generate sparkline column for numeric columns
+"""
+from visidata import Column, Sheet, option, options
 
 __author__ = 'Lucas Messenger @layertwo'
+__version__ = '0.2'
 
 
 option('disp_sparkline', '▁▂▃▄▅▆▇', 'characters to display sparkline')
 
 
-class SparklineColumn(Column):
+def sparkline(*values):
+    """
+    From *values generate string sparkline
+    """
+    lines = options.disp_sparkline
+    values = [v for v in values if isinstance(v, (int, float))]
+    mx = max(values)
+    mn = min(values)
+    w = (mx - mn) / len(lines)
+    bounds = [(mn + w * i) for i in range(len(lines))]
 
-    def calcValue(self, r):
-        return self.sparkline(*tuple(c.getTypedValue(r) for c in self.source))
-
-    def sparkline(self, *values):
-        lines = options.disp_sparkline
-        values = [v for v in values if isinstance(v, (int, float))]
-        mx = max(values)
-        mn = min(values)
-        w = (mx - mn) / len(lines)
-        bounds = [(mn + w * i) for i in range(len(lines))]
-
-        output = ''
-        for val in values:
-            for b in bounds:
-                if mn == 0 and val == 0:
-                    output += ' '
-                    break
-                if val < b:
-                    output += lines[bounds.index(b) - 1]
-                    break
-            else:
-                output += max(lines)
-        return output
+    output = ''
+    for val in values:
+        for b in bounds:
+            if mn == 0 and val == 0:
+                output += ' '
+                break
+            if val < b:
+                output += lines[bounds.index(b) - 1]
+                break
+        else:
+            output += max(lines)
+    return output
 
 
-ColumnsSheet.addCommand(None, 'addcol-sparkline', 'cursorRow.sheet.addColumn(SparklineColumn("sparkline", source=selectedRows))')
-Sheet.addCommand(None, 'addcol-sparkline', 'addColumn(SparklineColumn("sparkline", source=numericCols(nonKeyVisibleCols)))')
+@Sheet.api
+def sparkline_col(sheet, source):
+    """
+    Add sparkline column
+    """
+    c = Column('sparkline',
+               getter=lambda c,r: sparkline(*tuple(c.getTypedValue(r) for c in source)))
+    sheet.addColumn(c)
+
+
+Sheet.addCommand(None, 'addcol-sparkline', 'sheet.sparkline_col(numericCols(nonKeyVisibleCols))')
