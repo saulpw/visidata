269f6956405a7c1eb88421c7144eeb3a0a0ea116
anjakefala <anja.kefala@gmail.com>
2020-09-24 21:04:51 -0700

    [doc] tell sphinx about custom.css

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -52,3 +52,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+        'css/custom.css',
+        ]
