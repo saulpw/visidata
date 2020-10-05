a0fcc34df7280fa43a59b67a65a2009195898802
anjakefala <anja.kefala@gmail.com>
2020-10-04 17:24:40 -0700

    [docs api] add script for building page

---
+++ docs/api/mknetlify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # stop script on error
+
+# SRC is already cloned visidata repo, with no modifications
+SRC=$(pwd)
+export PYTHONPATH="$SRC"
+
+# BUILD is output directory (corresponding to webroot)
+BUILD="$SRC"/_build
+mkdir -p "$BUILD"
+
+sphinx-build -b html "$SRC" "$BUILD"
