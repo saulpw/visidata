3a5ec0de6ebe2a8f47778e4d8681a2037b77ec7b
anjakefala <anja.kefala@gmail.com>
2020-10-12 18:19:12 -0700

    [v2.0] update versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 # tox can't actually run python3 setup.py: https://github.com/tox-dev/tox/issues/96
 #from visidata import __version__
-__version__ = '2.-5dev'
+__version__ = '2.0'
 
 setup(name='visidata',
       version=__version__,
