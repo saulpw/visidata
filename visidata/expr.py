9fa8dd0a290a2741208e7f10547b368dedaf70f3
anjakefala <anja.kefala@gmail.com>
2020-11-02 19:18:29 -0800

    [expr] remove duplicate tabbing suggestions
    Closes #747

--- visidata/expr.py
+++ visidata/expr.py
@@ -22,6 +22,10 @@ class CompleteExpr:
         varnames = []
         varnames.extend(sorted((base+col.name) for col in self.sheet.columns if col.name.startswith(partial)))
         varnames.extend(sorted((base+x) for x in globals() if x.startswith(partial)))
+
+        # Remove duplicate tabbing suggestions
+        varnames_dict = {var:None for var in varnames}
+        varnames = list(varnames_dict.keys())
         return varnames[state%len(varnames)]
 
 
