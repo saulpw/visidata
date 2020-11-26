f9b6fd7812df8158490d91324f3da84b9d410221
Saul Pwanson <code@saul.pw>
2020-11-26 00:38:15 -0800

    [xlsx] only reload Workbook sheets to avoid error #797

--- visidata/loaders/xlsx.py
+++ visidata/loaders/xlsx.py
@@ -22,8 +22,10 @@ class XlsxIndexSheet(IndexSheet):
         import openpyxl
         self.workbook = openpyxl.load_workbook(str(self.source), data_only=True, read_only=True)
         for sheetname in self.workbook.sheetnames:
-            vs = XlsxSheet(self.name, sheetname, source=self.workbook[sheetname])
-            vs.reload()
+            src = self.workbook[sheetname]
+            vs = XlsxSheet(self.name, sheetname, source=src)
+            if isinstance(src, openpyxl.Workbook):
+                vs.reload()
             yield vs
 
 
