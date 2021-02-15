7c033cda4cf963f2d9c315983b4451db57f1d4af
C. McEnroe <june@causal.agency>
2021-02-15 15:01:21 -0500

    [airtable] Add empty Airtable module

--- vdplus/__init__.py
+++ vdplus/__init__.py
@@ -2,3 +2,4 @@ from .utils import *
 from .google import *
 from .seaborn import *
 from .scraper import *
+from .airtable import *
---
+++ vdplus/airtable.py
@@ -0,0 +1,5 @@
+from visidata import vd
+from airtable import Airtable
+
+vd.option('airtable_key', '', 'Airtable API key from https://airtable.com/account')
+vd.option('airtable_base', '', 'Airtable base ID from https://airtable.com/api')
