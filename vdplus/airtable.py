2aeb39d902d4d75294d4f7be49c5e530a7afaec9
C. McEnroe <june@causal.agency>
2021-02-15 16:59:12 -0500

    [airtable] Implement basic loader
    Without the meta API, we don't know the column definitions of the
    table, so they need to be populated from the data. Here the columns
    are populated from the first returned record, but this is not
    sufficient since an empty value for a column in a record will not
    be present at all. Later records need to be able to add missing
    columns.

--- vdplus/airtable.py
+++ vdplus/airtable.py
@@ -1,5 +1,25 @@
-from visidata import vd
+import re
+from visidata import vd, VisiData, Sheet, ItemColumn
 from airtable import Airtable
 
 vd.option('airtable_key', '', 'Airtable API key from https://airtable.com/account')
 vd.option('airtable_base', '', 'Airtable base ID from https://airtable.com/api')
+
+@VisiData.api
+def open_airtable(vd, p):
+    m = re.search(r'(tbl[A-z0-9]{14})/(viw[A-z0-9]{14})', p.given)
+    if m:
+        return AirtableSheet(m.groups()[0], source=m.groups()[0], view=m.groups()[1])
+
+
+class AirtableSheet(Sheet):
+    rowtype = 'records' # rowdef: dict
+    def iterload(self):
+        airtable = Airtable(vd.options.airtable_base, self.source, vd.options.airtable_key)
+        self.columns = []
+        for page in airtable.get_iter(view=self.view):
+            for record in page:
+                if not self.columns:
+                    for key, val in record['fields'].items():
+                        self.addColumn(ItemColumn(key, key, type=type(val)))
+                yield record['fields']
