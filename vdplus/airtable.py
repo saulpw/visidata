a1885fe201f46597878127da1490d7ef41ae1001
C. McEnroe <june@causal.agency>
2021-02-16 16:20:49 -0500

    [airtable] Add columns as needed for each record

--- vdplus/airtable.py
+++ vdplus/airtable.py
@@ -1,5 +1,5 @@
 import re
-from visidata import vd, VisiData, Sheet, ItemColumn
+from visidata import vd, date, VisiData, Sheet, Column, ItemColumn, deduceType
 from airtable import Airtable
 
 vd.option('airtable_key', '', 'Airtable API key from https://airtable.com/account')
@@ -17,9 +17,13 @@ class AirtableSheet(Sheet):
     def iterload(self):
         airtable = Airtable(vd.options.airtable_base, self.source, vd.options.airtable_key)
         self.columns = []
+        self.addColumn(ItemColumn('id', 'id', type=str, width=0))
+        self.addColumn(ItemColumn('createdTime', 'createdTime', type=date, width=0))
+        fields = set()
         for page in airtable.get_iter(view=self.view):
             for record in page:
-                if not self.columns:
-                    for key, val in record['fields'].items():
-                        self.addColumn(ItemColumn(key, key, type=type(val)))
-                yield record['fields']
+                for field, value in record['fields'].items():
+                    if field not in fields:
+                        self.addColumn(Column(field, expr=field, type=deduceType(value), getter=lambda c,r: r['fields'].get(c.expr)))
+                        fields.add(field)
+                yield record
