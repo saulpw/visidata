285e9f94b96ac081794319ccb76631f9aa70238c
anjakefala <anja.kefala@gmail.com>
2021-02-15 19:24:08 -0800

    [Path] fix sorting two visidata.Path objects
    Closes #897

--- visidata/path.py
+++ visidata/path.py
@@ -88,6 +88,8 @@ class Path(os.PathLike):
         return self._path.__fspath__()
 
     def __lt__(self, a):
+        if isinstance(a, visidata.Path):
+            return self._path.__lt__(a._path)
         return self._path.__lt__(a)
 
     def __truediv__(self, a):
