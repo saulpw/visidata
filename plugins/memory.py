a70a94fc86ba14ac09805b57d41affff0324b179
Saul Pwanson <code@saul.pw>
2021-02-26 22:11:02 -0800

    [memories] allow editing of both name/value (requires reload)

--- plugins/memory.py
+++ plugins/memory.py
@@ -1,4 +1,4 @@
-from visidata import Sheet, VisiData, ItemColumn, vd, AttrDict
+from visidata import Sheet, VisiData, ItemColumn, vd, AttrDict, Column, setitem
 
 
 @VisiData.lazy_property
@@ -7,20 +7,20 @@ def memory(vd):
 
 vd.contexts += [vd.memory]
 
-@Sheet.property
-def memories(sheet):
+@VisiData.property
+def memoriesSheet(vd):
     return MemorySheet("memories")
 
 
 class MemorySheet(Sheet):
     rowtype = 'memories'
     columns = [
-        ItemColumn('name', 0),
-        ItemColumn('value', 1),
+        Column('name', getter=lambda c,r: r[0], setter=lambda c,r,v: setitem(vd.memory, v, r[1])),
+        Column('value', getter=lambda c,r: r[1], setter=lambda c,r,v: setitem(vd.memory, r[0], v)),
     ]
     def reload(self):
         self.rows = list(vd.memory.items())
 
 
-Sheet.addCommand('^[M', 'open-memory', 'vd.push(sheet.memories)')
+Sheet.addCommand('^[M', 'open-memory', 'vd.push(vd.memoriesSheet)')
 Sheet.addCommand('^[m', 'memorize-cell', 'vd.memory[input("assign "+cursorCol.getDisplayValue(cursorRow)+" to: ")] = cursorCol.getTypedValue(cursorRow)')
