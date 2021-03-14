ae71b39af396d95fcda6077775730eb878193276
Saul Pwanson <code@saul.pw>
2021-03-13 22:55:47 -0800

    [darkdraw] groups and tags; resize cursor with ctrl+arrow or z+arrow

--- vdplus/darkdraw/drawing.py
+++ vdplus/darkdraw/drawing.py
@@ -1,4 +1,5 @@
 from unittest import mock
+import random
 import time
 import unicodedata
 from visidata import *
@@ -18,6 +19,31 @@ def open_ddw(vd, p):
     return Drawing(p.name, source=DrawingSheet(p.name, 'table', source=p))
 
 vd.save_ddw = vd.save_jsonl
+
+@VisiData.lazy_property
+def words(vd):
+    return [x.strip() for x in open('/usr/share/dict/words').readlines() if 3 <= len(x) < 8 and x.islower()]
+
+@VisiData.api
+def random_word(vd):
+    return random.choice(vd.words)
+
+def bounding_box(rows):
+    'Return (xmin, ymin, xmax, ymax) of rows.'
+    xmin, ymin, xmax, ymax = 9999, 9999, 0, 0
+    for r in rows:
+        if r.x is not None:
+            xmin = min(xmin, r.x)
+            xmax = max(xmax, r.x + (r.w or 0))
+        if r.y is not None:
+            ymin = min(ymin, r.y)
+            ymax = max(ymax, r.y + (r.h or 0))
+    return xmin, ymin, xmax, ymax
+
+def iterdeep(rows, x=0, y=0, toprow=None):
+    for r in rows:
+        yield r, x+r.x, y+r.y, toprow or r
+        yield from iterdeep(r.rows or [], x+r.x, x+r.y, toprow or r)
 
 def adj_xy(dx, dy, L):
     for r in L:
@@ -103,31 +129,72 @@ class DrawingSheet(JsonSheet):
         ItemColumn('text'),
         ItemColumn('color'),
         ItemColumn('group'),
-        ItemColumn('layer'),
+        ItemColumn('tags'),
     ]
     def newRow(self):
-        return AttrDict(x=None, y=None, text='', color='')
+        return AttrDict(x=None, y=None, text='', color='', tags=[])
 
     def addRow(self, row, **kwargs):
-        row = AttrDict(row)
-        super().addRow(row, **kwargs)
+        row = super().addRow(row, **kwargs)
         vd.addUndo(self.rows.remove, row)
+        return row
+
+    @drawcache_property
+    def groups(self):
+        return {r.group+'.':r for r in self.rows if not r.group.endswith('.')}
 
     def group_selected(self, gname):
-        for r in self.someSelectedRows:
-            r['group'] = (r.get('group', '') + ' ' + gname).strip()
-        vd.status('grouped %d objects' % self.nSelectedRows)
+        nr = self.newRow()
+        nr.rows = self.selectedRows
+        x1, y1, x2, y2 = bounding_box(nr.rows)
+        nr.x, nr.y, nr.w, nr.h = x1, y1, x2-x1, y2-y1
+        nr.group = gname
 
-    def degroup_selected(self, ung):
-        n = 0
-        for r in self.someSelectedRows:
-            gnames = r.get('group', '').split()
-            if ung in gnames:
-                r['group'] = ' '.join(g for g in gnames if g != ung)
-                n += 1
-        vd.status('ungrouped %d objects' % n)
+        for r in nr.rows:
+            r.group = gname + '.' + r.get('group', '')
+            r.x -= x1
+            r.y -= y1
 
-    def gatherGroup(self, gname):
+        self.deleteSelected()
+        nr = self.addRow(nr)
+        self.select([nr])
+        vd.status('group "%s" (%d objects)' % (gname, self.nSelectedRows))
+
+    def regroup_selected(self):
+        regrouped = []
+        groups = set()  # that items were grouped into
+        for r in self.someSelectedRows:
+            if r.group and r.group.endswith('.'):
+                regrouped.append(r)
+                g = self.groups[r.group]
+                g.rows.append(r)
+                groups.add(r.group)
+
+        self.deleteBy(lambda r,rows=regrouped: r in rows)
+        self.select(list(g for name, g in self.groups.items() if name in groups))
+
+        vd.status('regrouped %d %s' % (len(regrouped), self.rowtype))
+
+    def degroup_selected(self):
+        degrouped = []
+        groups = set()
+        for r, x, y, top in iterdeep(self.someSelectedRows):
+            r.x = x
+            r.y = y
+            if r.rows:
+                groups.add(r.group)
+            else:
+                self.addRow(r)
+                degrouped.append(r)
+
+        for g in groups:
+            self.groups[g+'.'].rows.clear()
+
+        self.clearSelected()
+        self.select(degrouped)
+        vd.status('ungrouped %d %s' % (len(degrouped), self.rowtype))
+
+    def gatherTag(self, gname):
         return list(r for r in self.rows if gname in r.get('group', ''))
 
     def slide_top(self, rows, index=0):
@@ -144,23 +211,51 @@ class Drawing(BaseSheet):
             for ny in range(y, y+h):
                 yield from self.drawing[(nx,ny)]
 
+    @property
+    def rows(self):
+        return self.source.rows
+
+    @rows.setter
+    def rows(self, v):
+        pass #        self.source.rows = v
+
     def __getattr__(self, k):
         return getattr(self.source, k)
+
+    def commandCursor(self, execstr):
+        if 'cursor' in execstr:
+            return '%s %s' % (self.cursorX, self.cursorX+self.cursorW), '%s %s' % (self.cursorY, self.cursorY+self.cursorH)
+        return '', ''
+
+    def moveToRow(self, rowstr):
+        self.refresh()
+        a, b = map(int, rowstr.split())
+        self.cursorY, self.cursorH = min(a,b), abs(b-a)
+        return True
+
+    def moveToCol(self, colstr):
+        self.refresh()
+        a, b = map(int, colstr.split())
+        self.cursorX, self.cursorW = min(a,b), abs(b-a)
+        return True
 
     def itercursor(self):
         return self.iterbox(self.cursorX, self.cursorY, self.cursorW+1, self.cursorH+1)
 
-    def within_cursor(self, r):
-        w = dispwidth(r.text)
-        if r.x+w-1 < self.cursorX: return False
-        if r.y < self.cursorY: return False
-        if r.y > self.cursorY+self.cursorH: return False
-        if r.x > self.cursorX+self.cursorW: return False
+    def within_cursor(self, x, y, w, h):
+        if x+w-1 < self.cursorX: return False
+        if y+h-1 < self.cursorY: return False
+        if y > self.cursorY+self.cursorH: return False
+        if x > self.cursorX+self.cursorW: return False
         return True
+
+    def refresh(self):
+        self._scr = mock.MagicMock(__bool__=mock.Mock(return_value=False))
+        self.draw(self._scr)
 
     def draw(self, scr):
         self.drawing = defaultdict(list)  # (x, y) -> list of rows; actual screen layout (topmost last in list)
-        self._groups = defaultdict(list)  # "groupname" -> list of rows in that group
+        self._tags = defaultdict(list)  # "groupname" -> list of rows in that group
 
         t = vd.fixed_t if vd.fixed_t is not None else (time.time() - vd.first_t)
         selectedGroups = set()  # any group with a selected element
@@ -170,47 +265,44 @@ class Drawing(BaseSheet):
             for j in range(self.cursorW+1):
                 clipdraw(scr, self.cursorY+i, self.cursorX+j, ' ', colors.color_current_row)
 
-        xmin, ymin, xmax, ymax = 9999, 9999, 0, 0
-        for row in self.source.rows:
-            r = AttrDict(row)
-            xmin = min(xmin, r.x)
-            ymin = min(ymin, r.y)
-            xmax = max(xmax, r.x)
-            ymax = max(ymax, r.y)
-            rgroups = (r.group or '').split()
-            for g in rgroups:
-                self._groups[g].append(row)
+        self.minX, self.minY, self.maxX, self.maxY = xmin, ymin, xmax, ymax = bounding_box(self.source.rows)
 
-            if r.text and not any_match(rgroups, self.disabled_groups):
-                if 0 <= r.y < self.windowHeight-1 and 0 <= r.x < self.windowWidth:  # inside screen
+        for row, x, y, toprow in iterdeep(self.source.rows):
+            r = AttrDict(row)
+            rtags = r.tags
+            for g in rtags:
+                self._tags[g].append(row)
+
+            if r.text and not any_match(rtags, self.disabled_tags):
+                if 0 <= y < self.windowHeight-1 and 0 <= x < self.windowWidth:  # inside screen
                     c = r.color
                     if r.t is not None:
                         if r.t > t:
                             continue
-                    if self.within_cursor(r):
+                    if self.within_cursor(x, y, r.w or dispwidth(r.text), r.h or 1):
                         c = self.options.color_current_row + ' ' + c
-                    if self.source.isSelected(row):
+                    if self.source.isSelected(toprow):
                         c = self.options.color_selected_row + ' ' + c
-                        selectedGroups |= set(rgroups)
+                        selectedGroups |= set(rtags)
                     a = colors[c]
-                    w = clipdraw(scr, r.y, r.x, r.text, a)
+                    w = clipdraw(scr, y, x, r.text, a)
                     for i in range(0, w):
-                        self.drawing[(r.x+i, r.y)].append(row)
-
-        self.minX, self.minY, self.maxX, self.maxY = xmin, ymin, xmax, ymax
+                        cellrows = self.drawing[(x+i, y)]
+                        if toprow not in cellrows:
+                            cellrows.append(toprow)
 
         if self.options.visibility > 0:
             defattr = self.options.color_default
             clipdraw(scr, i+1, self.windowWidth-20, '00: (reset)', defattr)
-            for i, gname in enumerate(self._groups.keys()):
+            for i, tag in enumerate(self._tags.keys()):
                 c = defattr
-                if gname in self.disabled_groups:
+                if tag in self.disabled_tags:
                     c = self.options.color_graph_hidden
-                if self.cursorRow and gname in self.cursorRow.get('group', ''):
+                if self.cursorRow and tag in self.cursorRow.get('group', ''):
                     c = self.options.color_current_row + ' ' + c
-                if gname in selectedGroups:
+                if tag in selectedGroups:
                     c = self.options.color_selected_row + ' ' + c
-                clipdraw(scr, i+1, self.windowWidth-20, '%02d: %s' % (i+1, gname), colors[c])
+                clipdraw(scr, i+1, self.windowWidth-20, '%02d: %s' % (i+1, tag), colors[c])
 
         self.last_t = t
 
@@ -257,12 +349,18 @@ class Drawing(BaseSheet):
     @property
     def cursorChar(self):
         cr = self.cursorRow
-        if cr: return cr['text']
-        return ' '
+        if cr: return cr.get('text', '')
+        return ''
 
     @property
-    def cursorCharOrd(self):
-        return ord(self.cursorChar[0])
+    def cursorDesc(self):
+        cr = self.cursorRow
+        if cr and cr.text:
+            return 'U+%04X' % ord(cr.text[0])
+        if cr.group:
+            n = len(list(iterdeep(cr.rows)))
+            return '%s (%s objects)' % (cr.group, n)
+        return '???'
 
     @property
     def cursorCharName(self):
@@ -368,14 +466,14 @@ class Drawing(BaseSheet):
         if dwg.source.nSelectedRows == 0: # auto-select newly pasted item
             dwg.source.select(newrows)
 
-    def select_group(dwg, groupname):
-        dwg.source.select(list(r for r in dwg.source.rows if groupname in (r.group or '')))
+    def select_tag(self, tag):
+        self.select(list(r for r in self.source.rows if tag in (r.tags or '')))
 
-    def unselect_group(dwg, groupname):
-        dwg.source.unselect(list(r for r in dwg.source.rows if groupname in (r.group or '')))
+    def unselect_tag(self, tag):
+        self.unselect(list(r for r in self.rows if tag in (r.tags or '')))
 
-    def align_selected(dwg, attrname):
-        rows = dwg.source.someSelectedRows
+    def align_selected(self, attrname):
+        rows = self.someSelectedRows
         for r in rows:
             r.x = rows[0].x
 
@@ -386,7 +484,7 @@ Drawing.init('cursorW', lambda: 0)
 Drawing.init('cursorH', lambda: 0)
 Drawing.init('drawing', dict)  # (x,y) -> list of rows
 Drawing.init('pendir', lambda: 'r')
-Drawing.init('disabled_groups', set)  # set of groupnames which should not be drawn or interacted with
+Drawing.init('disabled_tags', set)  # set of groupnames which should not be drawn or interacted with
 
 Drawing.addCommand(None, 'go-left',  'go_left()', 'go left one char')
 Drawing.addCommand(None, 'go-down',  'go_down()', 'go down one char')
@@ -421,8 +519,8 @@ Drawing.addCommand('y', 'yank-char', 'p=AttrDict(cursorRow); vd.memo_chars=adj_x
 Drawing.addCommand('gy', 'yank-selected', 'sel=[AttrDict(r) for r in source.selectedRows]; vd.memo_chars=adj_xy(-sel[0].x, -sel[0].y, sel)')
 Drawing.addCommand('x', 'cut-char', 'vd.memo_chars=remove_at(cursorX, cursorY, cursorW, cursorH)')
 Drawing.addCommand('zx', 'cut-char-top', 'r=list(itercursor())[-1]; vd.memo_chars=[r]; source.deleteBy(lambda r,row=r: r is row)')
-Drawing.addCommand('p', 'paste-chars', 'paste_chars(vd.memory.cliprows)')
-Drawing.addCommand('gp', 'paste-chars-selected', 'paste_chars(vd.memory.cliprows, selectedRows)', 'paste characters from clipboard over selection')
+Drawing.addCommand('p', 'paste-chars', 'paste_chars(vd.memory.cliprows[2])')
+Drawing.addCommand('gp', 'paste-chars-selected', 'paste_chars(vd.memory.cliprows[2], selectedRows)', 'paste characters from clipboard over selection')
 
 Drawing.addCommand('zh', 'go-left-obj', 'go_obj(-1, 0)')
 Drawing.addCommand('zj', 'go-down-obj', 'go_obj(0, +1)')
@@ -434,13 +532,15 @@ Drawing.addCommand('J', 'slide-down-obj', 'slide_selected(0, +1)')
 Drawing.addCommand('K', 'slide-up-obj', 'slide_selected(0, -1)')
 Drawing.addCommand('L', 'slide-right-obj', 'slide_selected(+1, 0)')
 
-Drawing.addCommand('gG', 'group-selected', 'source.group_selected(input("group name: "))')
-Drawing.addCommand('zG', 'degroup-selected', 'source.degroup_selected(input("group name to remove: "))')
-DrawingSheet.addCommand('gG', 'group-selected', 'sheet.group_selected(input("group name: "))')
-DrawingSheet.addCommand('zG', 'degroup-selected', 'sheet.degroup_selected(input("group name to remove: "))')
-Drawing.addCommand(',', 'select-equal-char', 'source.select(list(source.gatherBy(lambda r,ch=cursorChar: r.text==ch)))')
-Drawing.addCommand('|', 'select-group', 'select_group(input("select group: ", type="group"))')
-Drawing.addCommand('\\', 'unselect-group', 'unselect_group(input("unselect group: ", type="group"))')
+Drawing.addCommand('G', 'group-selected', 'sheet.group_selected(input("group name: ", value=random_word()))')
+Drawing.addCommand('gG', 'regroup-selected', 'sheet.regroup_selected()')
+Drawing.addCommand('zG', 'degroup-selected-temp', 'sheet.degroup_selected()')
+Drawing.addCommand('gzG', 'degroup-selected-perm', 'sheet.degroup_all()')
+DrawingSheet.addCommand('G', 'group-selected', 'sheet.group_selected(input("group name: ", value=random_word()))')
+DrawingSheet.addCommand('zG', 'degroup-selected', 'sheet.degroup_selected()')
+Drawing.addCommand(',', 'select-equal-char', 'sheet.select(list(source.gatherBy(lambda r,ch=cursorChar: r.text==ch)))')
+Drawing.addCommand('|', 'select-tag', 'sheet.select_tag(input("select tag: ", type="group"))')
+Drawing.addCommand('\\', 'unselect-tag', 'sheet.unselect_tag(input("unselect tag: ", type="group"))')
 
 Drawing.addCommand('gs', 'select-all', 'source.select(source.rows)')
 Drawing.addCommand('gt', 'toggle-all', 'source.toggle(source.rows)')
@@ -450,11 +550,11 @@ Drawing.addCommand('u', 'unselect-cursor', 'source.unselect(list(itercursor()))'
 Drawing.addCommand('zs', 'select-top-cursor', 'source.selectRow(list(itercursor())[-1])')
 Drawing.addCommand('gu', 'unselect-all', 'source.clearSelected()')
 
-Drawing.addCommand('00', 'enable-all-groups', 'disabled_groups.clear()')
+Drawing.addCommand('00', 'enable-all-groups', 'disabled_tags.clear()')
 for i in range(1, 99):
-    Drawing.addCommand('%02d'%i, 'toggle-group-%s'%i, 'g=list(_groups.keys())[%s]; disabled_groups.remove(g) if g in disabled_groups else disabled_groups.add(g)' %(i-1))
-    Drawing.addCommand('g%02d'%i, 'select-group-%s'%i, 'g=list(_groups.keys())[%s]; source.select(source.gatherGroup(g))' %(i-1))
-    Drawing.addCommand('z%02d'%i, 'unselect-group-%s'%i, 'g=list(_groups.keys())[%s]; source.unselect(source.gatherGroup(g))' %(i-1))
+    Drawing.addCommand('%02d'%i, 'toggle-group-%s'%i, 'g=list(_tags.keys())[%s]; disabled_tags.remove(g) if g in disabled_tags else disabled_tags.add(g)' %(i-1))
+    Drawing.addCommand('g%02d'%i, 'select-group-%s'%i, 'g=list(_tags.keys())[%s]; source.select(source.gatherTag(g))' %(i-1))
+    Drawing.addCommand('z%02d'%i, 'unselect-group-%s'%i, 'g=list(_tags.keys())[%s]; source.unselect(source.gatherTag(g))' %(i-1))
 
 
 BaseSheet.addCommand('A', 'new-drawing', 'vd.push(Drawing("untitled", source=DrawingSheet("", source=Path("untitled.ddw"))))')
@@ -462,7 +562,18 @@ BaseSheet.addCommand('M', 'open-unicode', 'vd.push(vd.unibrowser)')
 BaseSheet.addCommand('`', 'push-source', 'vd.push(sheet.source)')
 
 DrawingSheet.addCommand('^G', 'show-char', 'status(f"{sheet.cursorCharName}")')
+DrawingSheet.addCommand(ENTER, 'dive-group', 'cursorRow.rows or fail("not a group"); vd.push(DrawingSheet(source=sheet, rows=cursorRow.rows))')
+DrawingSheet.addCommand('g'+ENTER, 'dive-selected', 'ret=sum(((r.rows or []) for r in selectedRows), []) or fail("no groups"); vd.push(DrawingSheet(source=sheet, rows=ret))')
 Drawing.addCommand('&', 'join-selected', 'join_rows(source.selectedRows)')
+
+@Drawing.command('gm', 'tag-selected', '')
+def tag_selected(sheet):
+    tagstr = vd.input('tag selected as: ', type='tag')
+    tags = tagstr.split()
+    for r in sheet.someSelectedRows:
+        for tag in tags:
+            if tag not in (r.tags or ''):
+                r.tags.append(tag)
 
 Drawing.addCommand(ENTER, 'dive-cursor', 'vd.push(DrawingSheet(source=sheet, rows=cursorRows))')
 Drawing.addCommand('g'+ENTER, 'dive-selected', 'vd.push(DrawingSheet(source=sheet, rows=source.selectedRows))')
@@ -483,11 +594,21 @@ Drawing.addCommand('r', 'reset-time', 'vd.first_t=time.time()')
 Drawing.addCommand('>', 'time-next', 'vd.fixed_t += 1')
 Drawing.addCommand('<', 'time-prev', 'vd.fixed_t -= 1')
 
+Drawing.addCommand('kRIT5', 'resize-cursor-wider', 'sheet.cursorW += 1')
+Drawing.addCommand('kLFT5', 'resize-cursor-thinner', 'sheet.cursorW -= 1')
+Drawing.addCommand('kUP5', 'resize-cursor-shorter', 'sheet.cursorH -= 1')
+Drawing.addCommand('kDN5', 'resize-cursor-taller', 'sheet.cursorH += 1')
+
+Drawing.bindkey('zKEY_RIGHT', 'resize-cursor-wider')
+Drawing.bindkey('zKEY_LEFT', 'resize-cursor-thinner')
+Drawing.bindkey('zKEY_UP', 'resize-cursor-shorter')
+Drawing.bindkey('zKEY_DOWN', 'resize-cursor-taller')
+
 Drawing.bindkey('C', 'open-colors')
 
 Drawing.init('mark', lambda: (0,0))
-Drawing.class_options.disp_rstatus_fmt='{sheet.pendir}  <U+{sheet.cursorCharOrd:04X}> {sheet.options.disp_selected_note}{sheet.source.nSelectedRows}'
+Drawing.class_options.disp_rstatus_fmt='{sheet.pendir}  <{sheet.cursorDesc}> {sheet.options.disp_selected_note}{sheet.source.nSelectedRows}'
 Drawing.class_options.quitguard='modified'
 Drawing.class_options.null_value=''
-Drawing.quitguard=True
+Drawing.class_options.quitguard=True
 DrawingSheet.class_options.null_value=''
