e6719f42fb9ac93604220ee6939283a21e722c61
Saul Pwanson <code@saul.pw>
2021-04-06 17:27:54 -0700

    [xlsx-] stringify "header" row values for column names #921

--- visidata/sheets.py
+++ visidata/sheets.py
@@ -894,7 +894,7 @@ class SequenceSheet(Sheet):
         self.columns = []
         for i, colnamelines in enumerate(itertools.zip_longest(*headerrows, fillvalue='')):
             colnamelines = ['' if c is None else c for c in colnamelines]
-            self.addColumn(ColumnItem(''.join(colnamelines), i))
+            self.addColumn(ColumnItem(''.join(map(str, colnamelines)), i))
 
         self._rowtype = namedlist('tsvobj', [(c.name or '_') for c in self.columns])
 
