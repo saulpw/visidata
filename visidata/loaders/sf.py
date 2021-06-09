5d819f99d86a82ac4d5e40c8dc51e14d47f701e4
Christopher Ariza <ariza@flexatone.com>
2021-06-08 17:43:33 -0700

    added staticframe loader

---
+++ visidata/loaders/sf.py
@@ -0,0 +1,331 @@
+from functools import partial
+
+from visidata import *
+
+class StaticFrameAdapter:
+
+    def __init__(self, frame):
+        import static_frame as sf
+        if not isinstance(frame, sf.Frame):
+            vd.fail('%s is not a StaticFrame Frame' % type(frame).__name__)
+
+        self.frame = frame
+
+    def __len__(self):
+        if 'frame' not in self.__dict__:
+            return 0
+        return len(self.frame)
+
+    def __getitem__(self, k):
+        if isinstance(k, slice):
+            return StaticFrameAdapter(self.frame.iloc[k])
+        return self.frame.iloc[k]
+
+    def __getattr__(self, k):
+        if 'frame' not in self.__dict__:
+            raise AttributeError(f"'{self.__class__.__name__}' has no attribute '{k}'")
+        return getattr(self.frame, k)
+
+class StaticFrameSheet(Sheet):
+    '''Sheet sourced from a static_frame.Frame
+
+    Note:
+        Columns starting with "__vd_" are reserved for internal usage
+        by the VisiData loader.
+    '''
+
+    def dtype_to_type(self, dtype):
+        import numpy as np
+        # if np.issubdtype(dtype, np.str):
+        #     return str # NOTE: do we need this?
+        if np.issubdtype(dtype, np.integer):
+            return int
+        if np.issubdtype(dtype, np.floating):
+            return float
+        if np.issubdtype(dtype, np.datetime64):
+            return date
+        return anytype
+
+    @property
+    def frame(self):
+        if isinstance(getattr(self, 'rows', None), StaticFrameAdapter):
+            return self.rows.frame
+
+    @frame.setter
+    def frame(self, val):
+        if isinstance(getattr(self, 'rows', None), StaticFrameAdapter):
+            self.rows.frame = val
+        else:
+            self.rows = StaticFrameAdapter(val)
+
+    def getValue(self, col, row):
+        '''Look up column values in the underlying Frame.'''
+        return col.sheet.frame.loc[row.name, col.name]
+
+    def setValue(self, col, row, val):
+        '''
+        Update a column's value in the underlying Frame, loosening the
+        column's type as needed.
+        '''
+        dtype_old = col.sheet.frame.dtypes[col.name]
+        frame = col.sheet.frame.assign.loc[row.name, col.name](val)
+        dtype_new = frame.dtypes[col.name]
+        if dtype_old != dtype_new:
+            vd.warning(f'Type of {val} does not match column {col.name}. Changing type.')
+            col.type = self.dtype_to_type(dtype_new)
+
+    def reload(self):
+        import static_frame as sf
+        if isinstance(self.source, sf.Frame):
+            frame = self.source
+        elif isinstance(self.source, Path):
+            filetype = getattr(self, 'filetype', self.source.ext)
+            if filetype == 'tsv':
+                readfunc = sf.Frame.from_tsv
+            if filetype == 'csv':
+                readfunc = sf.Frame.from_csv
+            elif filetype == 'jsonl':
+                readfunc = sf.Frame.from_json
+            else:
+                vd.error('no supported import for:' + filetype)
+            frame = readfunc(str(self.source))
+        else:
+            # we might introspect self.source to introduce more handling
+            try:
+                frame = sf.Frame(self.source)
+            except ValueError as err:
+                vd.fail('error building Frame from source data: %s' % err)
+
+        # reset the index here
+        if frame.index._map: # if it is not an IndexAutoFactory
+            frame = frame.unset_index(drop=True)
+
+        # VisiData assumes string column names
+        if frame.columns.dtype != str:
+            frame = frame.relabel(columns=columns.astype(str))
+
+        dtypes = frame.dtypes
+
+        self.columns = []
+        for col in (c for c in frame.columns if not c.startswith("__vd_")):
+            self.addColumn(Column(
+                col,
+                type=self.dtype_to_type(dtypes[col]),
+                getter=self.getValue,
+                setter=self.setValue
+            ))
+
+        # if self.columns[0].name == 'index': # if the frame contains an index column
+        #     self.column('index').hide()
+
+        self.rows = StaticFrameAdapter(frame)
+        self._selectedMask = sf.Series.from_element(False, index=frame.index)
+
+
+    @asyncthread
+    def sort(self):
+        '''Sort rows according to the current self._ordering.'''
+        by_cols = []
+        ascending = []
+        for col, reverse in self._ordering[::-1]:
+            by_cols.append(col.name)
+            ascending.append(not reverse)
+
+        # NOTE: SF does not yet support ascending per column yet
+        self.rows = self.rows.sort_values(by_cols)
+
+    # NOTE: maybe this method is not needed
+    def _checkSelectedIndex(self):
+        import static_frame as sf
+        if self._selectedMask.index is not self.frame.index:
+            # selection is no longer valid
+            vd.status('sf.Frame.index updated, clearing {} selected rows'
+                      .format(self._selectedMask.sum()))
+            self._selectedMask = sf.Series(False, index=self.frame.index)
+
+    def rowid(self, row):
+        return getattr(row, 'name', None) or ''
+
+    # Base selection API. Refer to GH #266: re-implement the selection API by
+    # keeping a boolean sf.Series.from_element indicating the selected rows.
+    def isSelected(self, row):
+        if row is None:
+            return False
+        self._checkSelectedIndex()
+        return self._selectedMask.loc[row.name]
+
+    def selectRow(self, row):
+        'Select given row'
+        self._checkSelectedIndex()
+        self._selectedMask = self._selectedMask.assign.loc[row.name](True)
+
+    def unselectRow(self, row):
+        self._checkSelectedIndex()
+        is_selected = self._selectedMask.loc[row.name]
+        self._selectedMask = self._selectedMask.assign.loc[row.name](True)
+        return is_selected
+
+    @property
+    def nSelectedRows(self):
+        self._checkSelectedIndex()
+        return self._selectedMask.sum()
+
+    @property
+    def selectedRows(self):
+        self._checkSelectedIndex()
+        return StaticFrameAdapter(self.frame.loc[self._selectedMask])
+
+    # Vectorized implementation of multi-row selections
+    @asyncthread
+    def select(self, rows, status=True, progress=True):
+        self.addUndoSelection()
+        for row in (Progress(rows, 'selecting') if progress else rows):
+            self.selectRow(row)
+
+    @asyncthread
+    def unselect(self, rows, status=True, progress=True):
+        self.addUndoSelection()
+        for row in (Progress(rows, 'unselecting') if progress else rows):
+            self.unselectRow(row)
+
+    def clearSelected(self):
+        import static_frame as sf
+        self._selectedMask = sf.Series.from_element(False, index=self.frame.index)
+
+    def selectByIndex(self, start=None, end=None):
+        self._checkSelectedIndex()
+        self._selectedMask = self._selectedMask.assign.iloc[start:end](True)
+
+    def unselectByIndex(self, start=None, end=None):
+        self._checkSelectedIndex()
+        self._selectedMask = self._selectedMask.assign.iloc[start:end](False)
+
+    def toggleByIndex(self, start=None, end=None):
+        self._checkSelectedIndex()
+        self.addUndoSelection()
+        self._selectedMask = self._selectedMask.assign.iloc[start:end](
+                ~self._selectedMask.iloc[start:end])
+
+    def _selectByILoc(self, mask, selected=True):
+        self._checkSelectedIndex()
+        self._selectedMask = self._selectedMask.assign.iloc[mask](selected)
+
+
+
+    # TODO: below
+    @asyncthread
+    def selectByRegex(self, regex, columns, unselect=False):
+        '''
+        Find rows matching regex in the provided columns. By default, add
+        matching rows to the selection. If unselect is True, remove from the
+        active selection instead.
+        '''
+        import static_frame as sf
+        case_sensitive = 'I' not in vd.options.regex_flags
+        masks = pd.DataFrame([
+            self.frame[col.name].astype(str).str.contains(pat=regex, case=case_sensitive, regex=True)
+            for col in columns
+        ])
+        if unselect:
+            self._selectedMask = self._selectedMask & ~masks.any()
+        else:
+            self._selectedMask = self._selectedMask | masks.any()
+
+    def addUndoSelection(self):
+        vd.addUndo(undoAttrCopyFunc([self], '_selectedMask'))
+
+    @property
+    def nRows(self):
+        if self.frame is None:
+            return 0
+        return len(self.frame)
+
+    def newRows(self, n):
+        '''
+        Return n rows of empty data. Let pandas decide on the most
+        appropriate missing value (NaN, NA, etc) based on the underlying
+        DataFrame's dtypes.
+        '''
+
+        import static_frame as sf
+        return pd.DataFrame({
+            col: [None] * n for col in self.frame.columns
+        }).astype(self.frame.dtypes.to_dict(), errors='ignore')
+
+    def addRows(self, rows, index=None, undo=True):
+        import static_frame as sf
+        if index is None:
+            self.frame = self.frame.append(pd.DataFrame(rows))
+        else:
+            self.frame = pd.concat((self.frame.iloc[0:index], pd.DataFrame(rows), self.frame.iloc[index:]))
+        self.frame.index = pd.RangeIndex(self.nRows)
+        self._checkSelectedIndex()
+        if undo:
+            vd.addUndo(self._deleteRows, range(index, index + len(rows)))
+
+    def _deleteRows(self, which):
+        import static_frame as sf
+        self.frame.drop(which, inplace=True)
+        self.frame.index = pd.RangeIndex(self.nRows)
+        self._checkSelectedIndex()
+
+    def addRow(self, row, index=None):
+        self.addRows([row], index)
+        vd.addUndo(self._deleteRows, index or self.nRows - 1)
+
+    def delete_row(self, rowidx):
+        import static_frame as sf
+        oldrow = self.frame.iloc[rowidx:rowidx+1]
+
+        # Use to_dict() here to work around an edge case when applying undos.
+        # As an action is undone, its entry gets removed from the cmdlog sheet.
+        # If we use `oldrow` directly, we get errors comparing DataFrame objects
+        # when there are multiple deletion commands for the same row index.
+        # There may be a better way to handle that case.
+        vd.addUndo(self.addRows, oldrow.to_dict(), rowidx, False)
+        self._deleteRows(rowidx)
+        vd.memory.cliprows = [oldrow]
+
+    def deleteBy(self, by):
+        '''Delete rows for which func(row) is true.  Returns number of deleted rows.'''
+        import static_frame as sf
+        oldidx = self.cursorRowIndex
+        nRows = self.nRows
+        vd.addUndo(setattr, self, 'df', self.frame.copy())
+        self.frame = self.frame[~by]
+        self.frame.index = pd.RangeIndex(self.nRows)
+        ndeleted = nRows - self.nRows
+
+        vd.status('deleted %s %s' % (ndeleted, self.rowtype))
+        return ndeleted
+
+    def deleteSelected(self):
+        '''Delete all selected rows.'''
+        self.deleteBy(self._selectedMask)
+
+def view_pandas(df):
+    run(StaticFrameSheet('', source=df))
+
+
+# Override with vectorized implementations
+StaticFrameSheet.addCommand(None, 'stoggle-rows', 'toggleByIndex()', 'toggle selection of all rows')
+StaticFrameSheet.addCommand(None, 'select-rows', 'selectByIndex()', 'select all rows')
+StaticFrameSheet.addCommand(None, 'unselect-rows', 'unselectByIndex()', 'unselect all rows')
+
+StaticFrameSheet.addCommand(None, 'stoggle-before', 'toggleByIndex(end=cursorRowIndex)', 'toggle selection of rows from top to cursor')
+StaticFrameSheet.addCommand(None, 'select-before', 'selectByIndex(end=cursorRowIndex)', 'select all rows from top to cursor')
+StaticFrameSheet.addCommand(None, 'unselect-before', 'unselectByIndex(end=cursorRowIndex)', 'unselect all rows from top to cursor')
+StaticFrameSheet.addCommand(None, 'stoggle-after', 'toggleByIndex(start=cursorRowIndex)', 'toggle selection of rows from cursor to bottom')
+StaticFrameSheet.addCommand(None, 'select-after', 'selectByIndex(start=cursorRowIndex)', 'select all rows from cursor to bottom')
+StaticFrameSheet.addCommand(None, 'unselect-after', 'unselectByIndex(start=cursorRowIndex)', 'unselect all rows from cursor to bottom')
+StaticFrameSheet.addCommand(None, 'random-rows', 'nrows=int(input("random number to select: ", value=nRows)); vs=copy(sheet); vs.name=name+"_sample"; vs.rows=StaticFrameAdapter(sheet.df.sample(nrows or nRows)); vd.push(vs)', 'open duplicate sheet with a random population subset of N rows'),
+
+# Handle the regex selection family of commands through a single method,
+# since the core logic is shared
+StaticFrameSheet.addCommand('|', 'select-col-regex', 'selectByRegex(regex=input("select regex: ", type="regex", defaultLast=True), columns=[cursorCol])', 'select rows matching regex in current column')
+StaticFrameSheet.addCommand('\\', 'unselect-col-regex', 'selectByRegex(regex=input("select regex: ", type="regex", defaultLast=True), columns=[cursorCol], unselect=True)', 'unselect rows matching regex in current column')
+StaticFrameSheet.addCommand('g|', 'select-cols-regex', 'selectByRegex(regex=input("select regex: ", type="regex", defaultLast=True), columns=visibleCols)', 'select rows matching regex in any visible column')
+StaticFrameSheet.addCommand('g\\', 'unselect-cols-regex', 'selectByRegex(regex=input("select regex: ", type="regex", defaultLast=True), columns=visibleCols, unselect=True)', 'unselect rows matching regex in any visible column')
+
+# Override with a pandas/dataframe-aware implementation
+StaticFrameSheet.addCommand('"', 'dup-selected', 'vs=StaticFrameSheet(sheet.name, "selectedref", source=selectedRows.df); vd.push(vs)', 'open duplicate sheet with only selected rows'),
