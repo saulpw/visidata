d11146c89aaec69131fbb6068c351e6854bdd151
geekscrapy <github.7rhdeqzfu7@pkt5.com>
2021-12-09 22:15:15 +0400

    Non-ambiguios imports and addGlobals for ZipSheet, TarSheet and CsvSheet

--- visidata/loaders/archive.py
+++ visidata/loaders/archive.py
@@ -2,7 +2,8 @@ import codecs
 import tarfile
 import zipfile
 
-from visidata import *
+from visidata import vd, VisiData, asyncthread, Sheet, Progress, Menu, options
+from visidata import ColumnAttr, Column, date, datetime, Path
 
 @VisiData.api
 def open_zip(vd, p):
@@ -95,3 +96,8 @@ vd.addMenu(Menu('File', Menu('Extract',
         Menu('selected files', 'extract-selected'),
         Menu('selected files to', 'extract-selected-to'),
     )))
+
+vd.addGlobals({
+    'ZipSheet': ZipSheet,
+    'TarSheet': TarSheet
+})
