e72a3ba807dd9c4533f75c3430a40231244801a7
geekscrapy <github.7rhdeqzfu7@pkt5.com>
2022-05-03 09:30:06 +0400

    Notify when plugin update available (#1355)
    This will show a status message if an update to a plugin is available
    
    * plugins not stale if not installed
    
    Co-authored-by: Saul Pwanson <code@saul.pw>

--- visidata/plugins.py
+++ visidata/plugins.py
@@ -107,6 +107,14 @@ class PluginsSheet(JsonLinesSheet):
                 vd.addGlobals({funcname: func})
                 setattr(vd, funcname, func)
 
+        # check for plugins with newer versions
+        def is_stale(r):
+            v = _loadedVersion(r)
+            return v and v != r.latest_ver
+        
+        stale_plugins = list(filter(is_stale, self.rows))
+        if len(stale_plugins) > 0:
+            vd.warning(f'update available for {len(stale_plugins)} plugins')
 
     def installPlugin(self, plugin):
         # pip3 install requirements
