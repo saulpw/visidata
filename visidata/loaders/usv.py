2ec66d4e004030e835c4bdaa9c494b7880e236c9
Saul Pwanson <code@saul.pw>
2022-05-27 14:45:33 -0700

    [usv] swap delimiters #1383

--- visidata/loaders/usv.py
+++ visidata/loaders/usv.py
@@ -5,14 +5,14 @@ from visidata import Sheet, TsvSheet, options, vd, VisiData
 @VisiData.api
 def open_usv(vd, p):
     vs = TsvSheet(p.name, source=p)
-    vs.options.delimiter = '\u241e'
-    vs.options.row_delimiter = '\u241f'
+    vs.options.delimiter = '\u241f'
+    vs.options.row_delimiter = '\u241e'
     return vs
 
 @VisiData.api
 def save_usv(vd, p, vs):
     usvs = copy(vs)
     usvs.rows = vs.rows
-    usvs.options.delimiter = '\u241e'
-    usvs.options.row_delimiter = '\u241f'
+    usvs.options.delimiter = '\u241f'
+    usvs.options.row_delimiter = '\u241e'
     vd.save_tsv(p, usvs)
