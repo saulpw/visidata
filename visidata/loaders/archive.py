3d560ec6b0131a215d9c60426cae121794414f85
Saul Pwanson <code@saul.pw>
2022-07-20 20:21:12 -0700

    [zip-] use correct rowdef in extract #1430

--- visidata/loaders/archive.py
+++ visidata/loaders/archive.py
@@ -51,7 +51,7 @@ class ZipSheet(Sheet):
 
     @asyncthread
     def extract(self, *rows, path=None):
-        for r in Progress(rows):
+        for r, path in Progress(rows):
             self.zfp.extractall(members=[r.filename], path=path)
             vd.status(f'extracted {r.filename}')
 
