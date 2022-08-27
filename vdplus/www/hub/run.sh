553f06380bd598c09da6fd996ae0f617d470b7bf
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-08-27 18:25:17 -0400

    [vdwww] remove everything

--- vdplus/www/hub/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd api
-. $HOME/.poetry/env
-poetry run python app.py
--- vdplus/www/hub/spa/src/pages/About.ts
+++
@@ -1,9 +0,0 @@
-import m from "mithril";
-
-export default class implements m.ClassComponent {
-  constructor() {}
-
-  view() {
-    return [m("h1", "All about the VisiData Demo"), m("p", "details...")];
-  }
-}
