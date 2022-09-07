e6ea2a3e3825747f81485571d909eb2685feb866
Janek Schleicher <hanfried@posteo.net>
2022-09-07 11:16:59 +0200

    Update data.fixer.io using apilayer with requests
    The original currency conversion API data.fixer.io
    redirects already to apilayer.com and I couldn't find
    a way to create API keys for the former data.fixer.io API,
    so I updated the url and passing of parameters.
    Especially the api key is passed now as header instead as a url param
    what's certainly reasonable for security.
    
    Another problem was that apilayer blocks urllib requests as bot requests
    (for at least for the free account) and following
    https://www.pythonpool.com/urllib-error-httperror-http-error-403-forbidden/
    I changed the method to requests library as also this is officially
    supported by apilayer as can be seen in:
    https://apilayer.com/marketplace/fixer-api?live_demo=show
    
    It would be probably a good idea to change to requests in general,
    but I'm not sure how to test it accordingly, that's why a parameter to
    the urlcache function was added, so you can switch between urllib and
    requests.
    
    Still contains for convenience logging messages that will be removed
    next.

--- visidata/_urlcache.py
+++ visidata/_urlcache.py
@@ -3,35 +3,51 @@ import os.path
 import time
 from urllib.request import Request, urlopen
 import urllib.parse
+import requests
 
 from visidata import vd, VisiData, Path, options, modtime
 from visidata.settings import _get_cache_dir
 
+import logging
+logging.basicConfig(filename='/tmp/urlcache.log', level=logging.DEBUG)
+
 @VisiData.global_api
-def urlcache(vd, url, days=1, text=True, headers={}):
+def urlcache(vd, url, days=1, text=True, headers={}, http_library='urllib'):
     'Return Path object to local cache of url contents.'
+    assert http_library in ['urllib', 'requests'], "http_library %s should be one of 'urllib' or 'requests'"
+    assert not (http_library == 'requests' and text == False), "requests library only implemented for textual requests"
+
+    logging.info("urlcache url=%s days=%s text=%s headers=%s" % (url, days, text, headers))
     cache_dir = _get_cache_dir()
+    logging.info("cache_dir %s" % (cache_dir,))
     os.makedirs(cache_dir, exist_ok=True)
 
     p = Path(cache_dir / urllib.parse.quote(url, safe=''))
+    logging.info("Path %s" % (p, ))
     if p.exists():
+        logging.info("exists, use cached version")
         secs = time.time() - modtime(p)
         if secs < days*24*60*60:
             return p
 
-    req = Request(url)
-    for k, v in headers.items():
-        req.add_header(k, v)
+    if http_library == 'urllib':
+        req = Request(url)
+        for k, v in headers.items():
+            req.add_header(k, v)
 
-    with urlopen(req) as fp:
-        ret = fp.read()
-        if text:
-            ret = ret.decode('utf-8').strip()
-            with p.open_text(mode='w', encoding='utf-8') as fpout:
-                fpout.write(ret)
-        else:
-            with p.open_bytes(mode='w') as fpout:
-                fpout.write(ret)
+        with urlopen(req) as fp:
+            ret = fp.read()
+            if text:
+                ret = ret.decode('utf-8').strip()
+                with p.open_text(mode='w', encoding='utf-8') as fpout:
+                    fpout.write(ret)
+            else:
+                with p.open_bytes(mode='w') as fpout:
+                    fpout.write(ret)
+    elif http_library == 'requests':
+        response = requests.request("GET", url, headers=headers)
+        with p.open_text(mode="w", encoding="utf-8") as fpout:
+            fpout.write(response.text)
 
     return p
 
