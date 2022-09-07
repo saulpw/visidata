59829c4a6fcd6db295f65ca05b15a373546fcfb2
Janek Schleicher <hanfried@posteo.net>
2022-09-07 11:35:32 +0200

    Remove debug logging
    Wasn't sure how to properly debug it
    (import pdb; pdb.set_trace() collided with the curses frontend),
    so wrote a lot of logging debug outputs.
    Won't be needed any longer, but in case they would,
    it might be nice to have them in an own commit,
    so did not squeeze them with the other changes.

--- plugins/usd.py
+++ plugins/usd.py
@@ -6,9 +6,6 @@ from visidata import vd
 import functools
 import json
 
-import logging
-logging.basicConfig(filename='/tmp/visidata.log', level=logging.DEBUG)
-
 vd.option('fixer_key', '', 'API Key for fixer.io')
 
 currency_symbols = {
@@ -24,47 +21,33 @@ currency_symbols = {
 
 def currency_rates_json(date='latest', base='USD'):
     url = 'https://api.apilayer.com/fixer/%s?base=%s' % (date, base)
-    logging.info("currency_rates url=%s" % (url,))
-    payload = vd.urlcache(url, days=0, headers={'apikey': vd.options.fixer_key}, http_library='requests').read_text()
-    logging.info("currency_rates_json [url=%s] payload %s" % (url, payload))
-    return payload
+    return vd.urlcache(url, days=1, headers={'apikey': vd.options.fixer_key}, http_library='requests').read_text()
 
 @functools.lru_cache()
 def currency_rates():
-    rates = json.loads(currency_rates_json())['rates']
-    logging.info("currency_rates %s" % (rates, ))
-    return rates
+    return json.loads(currency_rates_json())['rates']
 
 @functools.lru_cache()
 def currency_multiplier(src_currency, dest_currency):
     'returns equivalent value in USD for an amt of currency_code'
-    callage = "currency_multiplier(%s, %s" % (src_currency, dest_currency)
     if src_currency == 'USD':
-        logging.info("%s = 1.0 [src_currency 'USD']" % (callage,))
         return 1.0
     eur_usd_mult = currency_rates()['USD']
     eur_src_mult = currency_rates()[src_currency]
     usd_mult = eur_usd_mult/eur_src_mult
     if dest_currency == 'USD':
-        logging.info("%s = %s [dst_currency 'USD']" % (callage,usd_mult))
         return usd_mult
     
-    calculated = usd_mult/currency_rates()[dest_currency]
-    logging.info("%s = %s [calculated]" % (callage,calculated))
-    return calculated
+    return usd_mult/currency_rates()[dest_currency]
 
 def USD(s):
     callage = "USD(%s)" % (s)
     for currency_symbol, currency_code in currency_symbols.items():
         if currency_symbol in s:
             amt = float(s.replace(currency_symbol, ''))
-            result = amt*currency_multiplier(currency_code, 'USD')
-            logging.info("%s = %s [amt=%s]" % (callage, result, amt))
-            return result
+            return amt*currency_multiplier(currency_code, 'USD')
 
     amtstr, currcode = s.split(' ')
-    result = float(amtstr) * currency_multiplier(currcode, 'USD')
-    logging.info("%s = %s [amtstr=%s, currcode=%s]" % (callage, result, amtstr, currcode))
-    return result
+    return float(amtstr) * currency_multiplier(currcode, 'USD')
 
 vd.addGlobals(globals())
--- visidata/_urlcache.py
+++ visidata/_urlcache.py
@@ -8,24 +8,17 @@ import requests
 from visidata import vd, VisiData, Path, options, modtime
 from visidata.settings import _get_cache_dir
 
-import logging
-logging.basicConfig(filename='/tmp/urlcache.log', level=logging.DEBUG)
-
 @VisiData.global_api
 def urlcache(vd, url, days=1, text=True, headers={}, http_library='urllib'):
     'Return Path object to local cache of url contents.'
     assert http_library in ['urllib', 'requests'], "http_library %s should be one of 'urllib' or 'requests'"
     assert not (http_library == 'requests' and text == False), "requests library only implemented for textual requests"
 
-    logging.info("urlcache url=%s days=%s text=%s headers=%s" % (url, days, text, headers))
     cache_dir = _get_cache_dir()
-    logging.info("cache_dir %s" % (cache_dir,))
     os.makedirs(cache_dir, exist_ok=True)
 
     p = Path(cache_dir / urllib.parse.quote(url, safe=''))
-    logging.info("Path %s" % (p, ))
     if p.exists():
-        logging.info("exists, use cached version")
         secs = time.time() - modtime(p)
         if secs < days*24*60*60:
             return p
@@ -44,6 +37,7 @@ def urlcache(vd, url, days=1, text=True, headers={}, http_library='urllib'):
             else:
                 with p.open_bytes(mode='w') as fpout:
                     fpout.write(ret)
+
     elif http_library == 'requests':
         response = requests.request("GET", url, headers=headers)
         with p.open_text(mode="w", encoding="utf-8") as fpout:
