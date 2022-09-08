0d72c5cdbeafa67736a2b8f42f822fe573e83165
anjakefala <anja.kefala@gmail.com>
2022-09-07 21:27:36 -0700

    [pyobj] rename open-row/open-cell to open-row-pyobj/open-cell-pyobj

--- visidata/cmdlog.py
+++ visidata/cmdlog.py
@@ -178,7 +178,7 @@ class _CommandLog:
             self.afterExecSheet(sheet, False, '')
 
         colname, rowname, sheetname = '', '', None
-        if sheet and not (cmd.longname.startswith('open-') and not cmd.longname in ('open-row', 'open-cell')):
+        if sheet and not (cmd.longname.startswith('open-') and not cmd.longname in ('open-row-pyobj', 'open-cell-pyobj')):
             sheetname = sheet.name
 
             colname, rowname = sheet.commandCursor(cmd.execstr)
