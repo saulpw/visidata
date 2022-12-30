a4f25ad54d734d1f660c809ade2c1d6d20cccd92
Saul Pwanson <code@saul.pw>
2022-12-29 18:11:15 -0800

    [customdate-] add to numericTypes; set fmtstr on existing cols #1613

--- visidata/customdate.py
+++ visidata/customdate.py
@@ -12,10 +12,12 @@ def customdate(sheet, fmtstr):
                 return super().__new__(cls, *time.strptime(args[0], fmtstr)[:6])
             return super().__new__(cls, *args, **kwargs)
 
-    vd.addType(_customdate, '@', '', formatter=lambda fmt,val: val.strftime(fmt or sheet.options.disp_date_fmt))
     _customdate.__name__ = 'customdate(%s)' % fmtstr
+
+    vd.addType(_customdate, '@', '', formatter=lambda fmt,val: val.strftime(fmt or sheet.options.disp_date_fmt))
+    vd.numericTypes.append(_customdate)
     return _customdate
 
 
-Sheet.addCommand('z@', 'type-customdate', 'cursorCol.type=customdate(input("date format: ", type="fmtstr"))', 'set type of current column to custom date format')
-ColumnsSheet.addCommand('gz@', 'type-customdate-selected', 'onlySelectedRows.type=customdate(input("date format: ", type="fmtstr"))', 'set type of selected columns to date')
+Sheet.addCommand('z@', 'type-customdate', 'fmt=input("date format: ", type="fmtstr"); cursorCol.type=customdate(fmt); cursorCol.fmtstr=fmt', 'set type of current column to custom date format')
+ColumnsSheet.addCommand('gz@', 'type-customdate-selected', 'fmt=input("date format: ", type="fmtstr"); onlySelectedRows.type=customdate(fmt); onlySelectedRows.fmtstr=fmt', 'set type of selected columns to date')
