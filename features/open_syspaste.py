9a6f39cd9dd6c37455aa89cc6b3b956875968187
David Wales <daviewales@disroot.org>
2023-02-04 09:16:01 +1100

    Enable filetype selection for open-syspaste
    Add gShift+P shortcut for open-syspaste

--- features/open_syspaste.py
+++ features/open_syspaste.py
@@ -1,14 +1,19 @@
+'''
+Load new table from system clipboard
+'''
+
 from visidata import vd, BaseSheet, Path
 
 
 @BaseSheet.api
-def open_syspaste(sheet, filetype='tsv'):
+def open_syspaste(sheet, filetype=None):
     import io
 
+    filetype = filetype or vd.input("paste as filetype: ", value='tsv')
     v = vd.sysclipValue().strip() or vd.fail('nothing to open')
 
     p = Path('syspaste'+'.'+filetype, fp=io.BytesIO(v.encode('utf-8')))
     return vd.openSource(p, filetype=filetype)
 
 
-BaseSheet.addCommand('', 'open-syspaste', 'vd.push(open_syspaste())', 'open clipboard as tsv')
+BaseSheet.addCommand('gShift+P', 'open-syspaste', 'vd.push(open_syspaste())', 'open clipboard as filetype')
