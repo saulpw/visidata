10180b91268ebae42f3b14a13025765dd7920855
David Wales <daviewales@disroot.org>
2023-02-04 21:13:47 +1100

    Move input to execstr for open-syspaste

--- features/open_syspaste.py
+++ features/open_syspaste.py
@@ -6,14 +6,13 @@ from visidata import vd, BaseSheet, Path
 
 
 @BaseSheet.api
-def open_syspaste(sheet, filetype=None):
+def open_syspaste(sheet, filetype='tsv'):
     import io
 
-    filetype = filetype or vd.input("paste as filetype: ", value='tsv')
     v = vd.sysclipValue().strip() or vd.fail('nothing to open')
 
     p = Path('syspaste'+'.'+filetype, fp=io.BytesIO(v.encode('utf-8')))
     return vd.openSource(p, filetype=filetype)
 
 
-BaseSheet.addCommand('gShift+P', 'open-syspaste', 'vd.push(open_syspaste())', 'open clipboard as filetype')
+BaseSheet.addCommand('gShift+P', 'open-syspaste', 'vd.push(open_syspaste(filetype=vd.input("paste as filetype: ", value="tsv")))', 'open clipboard as filetype')
