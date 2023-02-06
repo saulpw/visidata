4b1567b8af4afec1b0d5083bfcf12973aa90b42f
anjakefala <anja.kefala@gmail.com>
2023-02-05 21:39:07 -0800

    [replay-] turn off confirmQuit dialogs during replay

--- visidata/sheets.py
+++ visidata/sheets.py
@@ -1039,10 +1039,10 @@ def quit(vd, *sheets):
 
 @BaseSheet.api
 def confirmQuit(vs, verb='quit'):
-    if vs.options.quitguard and vs.precious and vs.hasBeenModified:
+    if vs.options.quitguard and vs.precious and vs.hasBeenModified and not vd.currentReplayRow:
         vd.draw_all()
         vd.confirm(f'{verb} modified sheet "{vs.name}"? ')
-    elif vs.options.getonly('quitguard', vs, False):  # if this sheet is specifically guarded
+    elif vs.options.getonly('quitguard', vs, False) and not vd.currentReplayRow:  # if this sheet is specifically guarded
         vd.draw_all()
         vd.confirm(f'{verb} guarded sheet "{vs.name}"? ')
 
