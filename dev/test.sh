5ff7883563eeac32fe192c5b2d4290a4e1e91cc2
anjakefala <anja.kefala@gmail.com>
2023-02-17 20:52:19 -0800

    [tests] add tests that only run above python 3.11

--- dev/test.sh
+++ dev/test.sh
@@ -3,6 +3,7 @@
 # Usage: test.sh [testname]
 
 set -e
+set -x
 shopt -s failglob
 
 trap "echo aborted; exit;" SIGINT SIGTERM
@@ -20,8 +21,22 @@ for i in $TESTS ; do
     outbase=${i##tests/}
     if [ "${i%-nosave.vd*}-nosave" == "${i%.vd*}" ];
     then
-        echo "$1"
+        TEST=false
+    elif [ "${i%-311.vd*}-311" == "${i%.vd*}" ];
+    then
+        if [ `python -c 'import sys; print(sys.version_info[:2] >= (3,11))'` == "True" ];
+        then
+            TEST=true
+        else
+            TEST=false
+        fi
+
     else
+        TEST=true
+    fi
+    if $TEST == true;
+    then
+        echo $TEST
         for goldfn in tests/golden/${outbase%.vd*}.*; do
             PYTHONPATH=. bin/vd --confirm-overwrite=False --play "$i" --batch --output "$goldfn" --config tests/.visidatarc --visidata-dir tests/.visidata
             echo "save: $goldfn"
