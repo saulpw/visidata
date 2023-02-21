694a2eb38d6148a80a99526b70efa428a118de78
Saul Pwanson <code@saul.pw>
2023-02-20 20:51:03 -0800

    [vgit] base usage: run git for all commands

---
+++ visidata/apps/vgit/__main__.py
@@ -0,0 +1,3 @@
+from .main import vgit_cli
+
+vgit_cli()
---
+++ visidata/apps/vgit/main.py
@@ -0,0 +1,31 @@
+'''
+# vgit: VisiData wrapper for git
+
+The syntax for vgit is the same as the syntax for git.
+By default, will pass the command to git verbatim, as quickly as possible.
+If vgit can provide an interactive interface for a particular subcommand,
+it will open the sheet returned by vd.git_<subcommand>().
+'''
+
+import sys
+
+
+def vgit_cli():
+    from visidata import vd, Path
+
+    args = sys.argv[1:]
+
+    if not args:
+#        return vd.run(vd.git_status())
+        return
+
+    func = getattr(vd, 'git_'+args[0], None)
+    if not func:
+        import subprocess
+        return subprocess.run(['git', *args]).returncode
+
+    vd.loadConfigAndPlugins()
+    vd.domotd()
+
+    vs = func(args[1:])
+    return vd.run(vs)
