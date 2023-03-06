9c73b98e4a89e310ce7b0c9bf2442440bfb650bf
anjakefala <anja.kefala@gmail.com>
2023-03-05 21:47:46 -0800

    [vgit] move bin/vgit and setup.py to visidata/apps/

--- visidata/apps/vgit-setup.py
+++ visidata/apps/vgit-setup.py
@@ -11,7 +11,6 @@ setup(name='vgit',
       description='a sleek terminal user interface for git',
       # long_description=open('README.md').read(),
       install_requires=['sh'], # visidata
-      package_dir={'':'visidata/apps'},
       packages=['vgit'],
       scripts=['bin/vgit'],
       author='Saul Pwanson',
