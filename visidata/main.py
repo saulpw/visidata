fa5aaa8593bc38969d0f1c5c695b17ad00e60853
anjakefala <anja.kefala@gmail.com>
2023-04-02 18:08:07 -0700

    [errors-] do not print ExceptedExceptions twice in batch-mode

--- visidata/main.py
+++ visidata/main.py
@@ -373,7 +373,7 @@ def vd_cli():
     except BrokenPipeError:
         os.dup2(os.open(os.devnull, os.O_WRONLY), sys.stdout.fileno()) # handle broken pipe gracefully
     except visidata.ExpectedException as e:
-        print(str(e))
+        pass
     except FileNotFoundError as e:
         print(e)
         if options.debug:
