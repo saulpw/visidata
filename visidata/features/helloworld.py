641bd058a358bb1c20a5167af530b6a85f6e22fc
anjakefala <anja@voltrondata.com>
2023-04-27 15:39:12 -0700

    [plugins] move helloworld.py into features

--- visidata/features/helloworld.py
+++ visidata/features/helloworld.py
@@ -3,12 +3,8 @@ Hello world minimal plugin.  Press F2 to show options.hello_world on the status 
 
 .visidatarc: `import plugins.hello`
 '''
-
-__author__ = 'Paul Swanson <paulsw@example.com>'
-__version__ = '1.0'
-
 from visidata import vd, BaseSheet
 
 vd.option('hello_world', '¡Hola mundo!', 'shown by the hello-world command')
 
-BaseSheet.addCommand('KEY_F(2)', 'hello-world', 'status(options.hello_world)')
+BaseSheet.addCommand('KEY_F(2)', 'hello-world', 'status(options.hello_world)', 'print greeting to status')
