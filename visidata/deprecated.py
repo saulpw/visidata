d0fa363370299ade991fce3f318168bb8ca2f0c5
anjakefala <anja.kefala@gmail.com>
2023-05-02 22:05:39 -0700

    [deprecated-] deprecate Path.open_text

--- visidata/deprecated.py
+++ visidata/deprecated.py
@@ -164,6 +164,7 @@ exceptionCaught = deprecated('2.6', 'vd.exceptionCaught')(vd.exceptionCaught)
 openSource = deprecated('2.6', 'vd.openSource')(vd.openSource)
 globalCommand = visidata.BaseSheet.addCommand
 visidata.Sheet.StaticColumn = deprecated('2.11', 'Sheet.freeze_col')(visidata.Sheet.freeze_col)
+visidata.Path.open_text = deprecated('3.0', 'visidata.Path.open')(visidata.Path.open)
 
 vd.sysclip_value = deprecated('3.0', 'vd.sysclipValue')(vd.sysclipValue)
 
