34a66fa8cfefce045d2f00eab068ac0827a62b20
Justin <justin2004@hotmail.com>
2023-06-13 16:40:30 -0400

    enable OAuth 2.0 for IMAP with gmail
    - add docs that help users set up a Google API App
    - delete "vdplus/api/google/google-creds.json" because that only works for
      email addresses designated by Saul's Google API App

--- vdplus/api/google/__init__.py
+++ vdplus/api/google/__init__.py
@@ -6,17 +6,29 @@ from .gdrive import *
 
 def _google_creds_fn():
     from pkg_resources import resource_filename
-    return resource_filename('vdplus.api.google', 'google-creds.json')
+    import os
+    if not os.path.exists(Path(resource_filename('vdplus.api.google', 'google-creds.json'))):
+        vd.error('google-creds.json file does not exist. create it by following this guide: https://github.com/saulpw/visidata/blob/develop/docs/gmail.md')
+    else:
+        return resource_filename('vdplus.api.google', 'google-creds.json')
 
 
 @VisiData.api
-def google_auth(vd, scopes='spreadsheets.readonly'):
+def google_auth(vd, scopes=None):
     import pickle
     import os.path
-    GSHEETS_TOKEN_FILE = Path(vd.options.visidata_dir)/f'google-{scopes}.pickle'
+    import urllib.parse
+
+    SCOPES = []
+    for scope in scopes.split():
+        if not scope.startswith('https://'):
+            scope = 'https://www.googleapis.com/auth/' + scope
+        SCOPES.append(scope)
+
+    GOOGLE_TOKEN_FILE = Path(vd.options.visidata_dir)/f'google-{urllib.parse.quote_plus(str(scopes))}.pickle'
     creds = None
-    if os.path.exists(GSHEETS_TOKEN_FILE):
-        with open(GSHEETS_TOKEN_FILE, 'rb') as fp:
+    if os.path.exists(GOOGLE_TOKEN_FILE):
+        with open(GOOGLE_TOKEN_FILE, 'rb') as fp:
             creds = pickle.load(fp)
 
     if not creds or not creds.valid:
@@ -25,11 +37,10 @@ def google_auth(vd, scopes='spreadsheets.readonly'):
             creds.refresh(Request())
         else:
             from google_auth_oauthlib.flow import InstalledAppFlow
-            SCOPES = [f'https://www.googleapis.com/auth/{x}' for x in scopes.split()]
             flow = InstalledAppFlow.from_client_secrets_file(_google_creds_fn(), SCOPES)
             creds = flow.run_local_server(port=0)
 
-        with open(GSHEETS_TOKEN_FILE, 'wb') as fp:
+        with open(GOOGLE_TOKEN_FILE, 'wb') as fp:
             pickle.dump(creds, fp)
 
     return creds
