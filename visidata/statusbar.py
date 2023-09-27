f1e2188c2e3c3371853c5f0e20e2d4b6709fca2c
Saul Pwanson <code@saul.pw>
2023-09-27 16:24:36 -0700

    [status] remove left-click for sheets-stack #2030 #1656

--- visidata/statusbar.py
+++ visidata/statusbar.py
@@ -168,8 +168,6 @@ def drawLeftStatus(vd, scr, vs):
         x = clipdraw(scr, y, 0, lstatus, attr, w=vs.windowWidth-1)
 
         vd.onMouse(scr, 0, y, x, 1,
-                        BUTTON1_PRESSED='sheets-stack',
-                        BUTTON1_RELEASED='sheets-stack',
                         BUTTON3_PRESSED='rename-sheet',
                         BUTTON3_CLICKED='rename-sheet')
     except Exception as e:
