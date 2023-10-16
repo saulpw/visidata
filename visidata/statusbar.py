5f75f1f926f2877043d7df8178efa5d88b61c0b1
Saul Pwanson <code@saul.pw>
2023-10-16 15:33:43 -0700

    [status] move replay status to right side for stability

--- visidata/statusbar.py
+++ visidata/statusbar.py
@@ -8,7 +8,7 @@ import visidata
 from visidata import vd, VisiData, BaseSheet, Sheet, ColumnItem, Column, RowColorizer, options, colors, wrmap, clipdraw, ExpectedException, update_attr, dispwidth, ColorAttr
 
 
-vd.option('disp_rstatus_fmt', '{sheet.keystrokeStatus}   {sheet.longname}  {sheet.nRows:9d} {sheet.rowtype} {sheet.modifiedStatus}{vd.replayStatus}{sheet.selectedStatus}{sheet.threadStatus}', 'right-side status format string')
+vd.option('disp_rstatus_fmt', '{sheet.threadStatus} {sheet.keystrokeStatus}   {sheet.longname}  {sheet.nRows:9d} {sheet.rowtype} {sheet.modifiedStatus}{sheet.selectedStatus}{vd.replayStatus}', 'right-side status format string')
 vd.option('disp_status_fmt', '{sheet.shortcut}› {sheet.name}| ', 'status line prefix')
 vd.option('disp_lstatus_max', 0, 'maximum length of left status line')
 vd.option('disp_status_sep', '│', 'separator between statuses')
