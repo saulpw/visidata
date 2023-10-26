a2405f0bd9a74a28a79b6ba3780195b6cc5a142b
Saul Pwanson <code@saul.pw>
2023-10-25 22:43:37 -0700

    [ui] integrate scrollfix from @geekscrapy #1441

--- visidata/features/scroll_context.py
+++ visidata/features/scroll_context.py
@@ -1,14 +1,60 @@
-from visidata import vd, TableSheet
+'''
+Enables two functions:
+1. options.disp_scroll_context: the minimum number of lines to keep visible above and below the cursor when scrolling
+2. toggle-scrollfix: Lock the cursor to a particular row number in the view
 
-vd.option('disp_scroll_context', 0, 'number of lines of context to display above/below cursor when scrolling', max_help=1)
+Allows the user to fix the position on the page where they would like the
+cursor to "stick". Helps to provide context about surrounding rows when
+near the top and bottom of the page.
 
+Usage: scroll a few lines down, `toggle-scrollfix' and scroll again!
 
-@TableSheet.after
+NOTE:
+    - disables scroll-middle command (zz)
+'''
+
+__author__ = 'Geekscrapy'
+__version__ = '1.2'
+
+from visidata import vd, Sheet
+
+vd.option('disp_scroll_context', 0, helpstr='minimum number of lines to keep visible above/below cursor when scrolling')
+vd.optalias('disp_scrolloff', 'disp_scroll_context')
+
+@Sheet.after
 def checkCursor(sheet):
-    n = sheet.options.disp_scroll_context
+    nctx = sheet.options.disp_scroll_context
+    if nctx:
+        if nctx >= int(sheet.nScreenRows/2):
+            if sheet.cursorRowIndex+1 > int(sheet.nScreenRows/2):
+                sheet.topRowIndex = sheet.cursorRowIndex - int(sheet.nScreenRows/2)
+        else:
+            if sheet.cursorRowIndex-sheet.topRowIndex < nctx:
+                sheet.topRowIndex = max(0, sheet.cursorRowIndex-nctx)
 
-    if sheet.cursorRowIndex-sheet.topRowIndex < n:
-        sheet.topRowIndex = max(0, sheet.cursorRowIndex-n)
+            if sheet.bottomRowIndex-sheet.cursorRowIndex < nctx:
+                sheet.bottomRowIndex = sheet.cursorRowIndex+nctx
 
-    if sheet.bottomRowIndex-sheet.cursorRowIndex < n:
-        sheet.bottomRowIndex = sheet.cursorRowIndex+n
+    nfix = getattr(sheet, 'disp_scrollfix', 0)
+    if nfix > 0:
+        cursorViewIndex = sheet.cursorRowIndex - sheet.topRowIndex
+        if cursorViewIndex > nfix:
+            sheet.topRowIndex += abs(nfix - cursorViewIndex)
+        if cursorViewIndex < nfix and sheet.topRowIndex > 0:
+            sheet.topRowIndex -= abs(nfix - cursorViewIndex)
+
+
+@Sheet.api
+def toggle_scrollfix(sheet):
+    if getattr(sheet, 'disp_scrollfix', -1) >= 0:
+        sheet.disp_scrollfix = -1
+        vd.status("cursor unlocked")
+    else:
+        sheet.disp_scrollfix = sheet.cursorRowIndex - sheet.topRowIndex
+        vd.status("cursor locked to screen row {sheet.disp_scrollfix}")
+
+
+Sheet.addCommand('', 'toggle-scrollfix', 'toggle_scrollfix()', helpstr='toggle cursor lock to current screen row')
+
+
+vd.addMenuItems('''View > Toggle display > lock cursor to screen row > toggle-scrollfix''')
