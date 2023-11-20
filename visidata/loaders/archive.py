saulpw/visidata

commit a1affe7d8b60c4e9bdbca2c43c30416e42875d7f

[api-] rename open_text to open, and open to _open  #1883

visidata/loaders/unzip_http.py
@@ -227,7 +227,7 @@ class RemoteZipFile:
         else:
             error(f'unknown compression method {method}')
 
-    def open_text(self, fn):
+    def open(self, fn):
         return io.TextIOWrapper(self.open(fn))
 
 

visidata/_open.py
@@ -164,7 +164,7 @@ def openSource(vd, p, filetype=None, create=False, **kwargs):
 def open_txt(vd, p):
     'Create sheet from `.txt` file at Path `p`, checking whether it is TSV.'
     if p.exists(): #1611
-        with p.open_text(encoding=vd.options.encoding) as fp:
+        with p.open(encoding=vd.options.encoding) as fp:
             delimiter = vd.options.delimiter
             try:
                 if delimiter and delimiter in next(fp):    # peek at the first line

visidata/_urlcache.py
@@ -29,7 +29,7 @@ def urlcache(vd, url, days=1, text=True, headers={}):
         ret = fp.read()
         if text:
             ret = ret.decode('utf-8').strip()
-            with p.open_text(mode='w', encoding='utf-8') as fpout:
+            with p.open(mode='w', encoding='utf-8') as fpout:
                 fpout.write(ret)
         else:
             with p.open_bytes(mode='w') as fpout:

visidata/cmdlog.py
@@ -34,7 +34,7 @@ VisiData.save_vd = VisiData.save_tsv
 
 @VisiData.api
 def save_vdj(vd, p, *vsheets):
-    with p.open_text(mode='w', encoding=vsheets[0].options.save_encoding) as fp:
+    with p.open(mode='w', encoding=vsheets[0].options.save_encoding) as fp:
         fp.write("#!vd -p\n")
         for vs in vsheets:
             vs.write_jsonl(fp)

visidata/help.py
@@ -121,7 +121,7 @@ def getHelpPane(vd, name, module='vdplus'):
     from pkg_resources import resource_filename
     ret = HelpPane(name)
     try:
-        ret.amgr.load(name, Path(resource_filename(module,'ddw/'+name+'.ddw')).open_text(encoding='utf-8'))
+        ret.amgr.load(name, Path(resource_filename(module,'ddw/'+name+'.ddw')).open(encoding='utf-8'))
         ret.amgr.trigger(name, loop=True)
     except FileNotFoundError as e:
         vd.debug(str(e))

visidata/loaders/csv.py
@@ -15,7 +15,7 @@ csv.field_size_limit(2**31-1) # Windows has max 32-bit
 
 @VisiData.api
 def guess_csv(vd, p):
-    line = next(p.open_text())
+    line = next(p.open())
     if ',' in line:
         dialect = csv.Sniffer().sniff(line)
         r = dict(filetype='csv', _likelihood=0)
@@ -39,7 +39,7 @@ class CsvSheet(SequenceSheet):
 
     def iterload(self):
         'Convert from CSV, first handling header row specially.'
-        with self.source.open_text(encoding=self.options.encoding) as fp:
+        with self.source.open(encoding=self.options.encoding) as fp:
             if options.safety_first:
                 rdr = csv.reader(removeNulls(fp), **options.getall('csv_'))
             else:
@@ -58,7 +58,7 @@ class CsvSheet(SequenceSheet):
 @VisiData.api
 def save_csv(vd, p, sheet):
     'Save as single CSV file, handling column names as first line.'
-    with p.open_text(mode='w', encoding=sheet.options.save_encoding, newline='') as fp:
+    with p.open(mode='w', encoding=sheet.options.save_encoding, newline='') as fp:
         cw = csv.writer(fp, **options.getall('csv_'))
         colnames = [col.name for col in sheet.visibleCols]
         if ''.join(colnames):

visidata/loaders/eml.py
@@ -16,7 +16,7 @@ class EmailSheet(TableSheet):
     def iterload(self):
         import email
         parser = email.parser.Parser()
-        with self.source.open_text(encoding='utf-8') as fp:
+        with self.source.open(encoding='utf-8') as fp:
             yield from parser.parse(fp).walk()
 
 @EmailSheet.api

visidata/loaders/fixed_width.py
@@ -76,7 +76,7 @@ class FixedWidthColumnsSheet(SequenceSheet):
 
 @VisiData.api
 def save_fixed(vd, p, *vsheets):
-    with p.open_text(mode='w', encoding=vsheets[0].options.save_encoding) as fp:
+    with p.open(mode='w', encoding=vsheets[0].options.save_encoding) as fp:
         for sheet in vsheets:
             if len(vsheets) > 1:
                 fp.write('%s\n\n' % sheet.name)

commit 037b0d1314a3cffa3ebed7121f0b8317b79aa5e3

[zip-] fix loading and extracting files from url zip files  #2127
Regression occured in a1affe7d8b60c4e9bdbca2c43c30416e42875d7f

visidata/loaders/archive.py
@@ -64,11 +64,11 @@ Commands:
     def openZipFile(self, fp, *args, **kwargs):
         '''Use VisiData input to handle password-protected zip files.'''
         try:
-            return fp.open(*args, **kwargs)
+            return fp._open(*args, **kwargs)
         except RuntimeError as err:
             if 'password required' in err.args[0]:
                 pwd = vd.input(f'{args[0].filename} is encrypted, enter password: ', display=False)
-                return fp.open(*args, **kwargs, pwd=pwd.encode('utf-8'))
+                return fp._open(*args, **kwargs, pwd=pwd.encode('utf-8'))
             vd.exceptionCaught(err)
 
     def openRow(self, row):

visidata/loaders/unzip_http.py
@@ -186,7 +186,7 @@ class RemoteZipFile:
 
             outpath = path/member
             os.makedirs(outpath.parent, exist_ok=True)
-            with self.open(member) as fpin:
+            with self._open(member) as fpin:
                 with open(path/member, mode='wb') as fpout:
                     while True:
                         r = fpin.read(65536)