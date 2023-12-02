1618aed086e2f5def15717903a9e176162009fc2
anjakefala <anja.kefala@gmail.com>
2023-12-01 22:14:31 -0800

    [perf-] Fallback to getattrdeep if len(row) < index
    Comes up with e.g. errors.csv where there are extra newlines and fields
    in a subset of csv rows. Some rows will be shorter, and will result in
    an IndexError when getitemdeep tries to grab their value for a later
    column.
    
    Builds on c324fca6ba24c1269f7686890a9a560ec3f408b6

--- visidata/utils.py
+++ visidata/utils.py
@@ -130,7 +130,10 @@ def setattrdeep(obj, attr, val, getter=getattr, setter=setattr):
 
 def getitemdeep(obj, k, *default):
     if not isinstance(k, str):
-        return obj[k]
+        try:
+            return obj[k]
+        except IndexError:
+            pass
     return getattrdeep(obj, k, *default, getter=getitem)
 
 def setitemdeep(obj, k, val):
