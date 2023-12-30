0258e0f1163ce499b5c599da8ca5bbedc242905c
AaronFeldman <AaronFeldmanEsq@GMail.com>
2023-12-30 16:21:02 -0500

    Create plasma.py
    Orangy plasma theme

---
+++ visidata/themes/plasma.py
@@ -0,0 +1,9 @@
+
+vd.themes['plasma'] = dict(
+    color_default      = '208 on black',  # the default fg and bg colors
+    color_key_col      = '208 on blue',   # color of key columns
+    color_menu    = 'black on 208', 
+    color_menu_active     = '208 on black', # color of menu 
+    color_edit_cell    = '234 black',     # cell color to use when editing cell
+    # ... etc
+)
