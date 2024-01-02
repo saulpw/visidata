490ad1bf798e6bb6ca200b8132ed98942753e063
AJ Kerrigan <aj@speckledmonkey.com>
2024-01-02 13:08:39 -0500

    [tests] parameterize feature tests
    Split feature tests into individual test cases for a single
    parameterized function.
    
    Co-authored-by: Elliott Sales de Andrade <quantum.analyst@gmail.com>

--- visidata/tests/test_features.py
+++ visidata/tests/test_features.py
@@ -1,17 +1,28 @@
 import pytest
 import visidata
 
-@pytest.mark.usefixtures('curses_setup')
-class TestFeatures:
-    def test_features(self, mock_screen):
-        tests = [
-            (mod, getattr(mod, k))
-                for mod in visidata.vd.importedModules
-                    for k in dir(mod)
-                        if k.startswith('test_')
-        ]
-        for mod, testfunc in tests:
-            print(mod, testfunc.__name__)
-            visidata.vd.resetVisiData()
-            visidata.vd.scr = mock_screen
-            testfunc(visidata.vd)
+def pytest_generate_tests(metafunc):
+    """Split feature tests into separate test cases
+
+    Look up test methods in imported modules. Turn each one into a single test_feature()
+    case, with "module::method" as the test id.
+    """
+    tests = [
+        (mod, getattr(mod, k))
+        for mod in visidata.vd.importedModules
+        for k in dir(mod)
+        if k.startswith("test_")
+    ]
+    argvalues = [[testfunc] for _, testfunc in tests]
+    testids = [
+        f"{mod.__name__}::{testfunc.__name__}"
+        for mod, testfunc in tests
+    ]
+    metafunc.parametrize(argnames=["testfunc"], argvalues=argvalues, ids=testids)
+
+
+@pytest.mark.usefixtures("curses_setup")
+def test_feature(mock_screen, testfunc):
+    visidata.vd.resetVisiData()
+    visidata.vd.scr = mock_screen
+    testfunc(visidata.vd)
