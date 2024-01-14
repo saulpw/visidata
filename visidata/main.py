f3eb226814efe745947a432e0142eb4338a5296a
Saul Pwanson <code@saul.pw>
2024-01-14 15:18:20 -0800

    [replay-] stop stderr batch progress when interactive #2251

--- visidata/main.py
+++ visidata/main.py
@@ -116,13 +116,13 @@ def parsePos(vd, arg:str, inputs=None):
 def outputProgressEvery(vd, sheet, seconds:float=0.5):
     import time
     t0 = time.time()
-    while True:
-        time.sleep(seconds)
+    while vd.currentReplay:
         t = time.time()
         print(f'\r[{t-t0:.1f}s] ', end='', file=sys.stderr)
         if sheet:
             print(f'{sheet.progressPct}  ', end='', file=sys.stderr)
         sys.stderr.flush()
+        time.sleep(seconds)
 
 @visidata.VisiData.api
 def moveToPos(vd, sources, startsheets, startrow, startcol):
