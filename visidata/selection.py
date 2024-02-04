1378f36db9753a8ca48684357e57eb6e9aa495f0
midichef <67946319+midichef@users.noreply.github.com>
2024-02-03 21:28:53 -0800

    [selection] make version of sortSelected for testing
    Use assertions to ensure no sheet data is lost.

--- visidata/selection.py
+++ visidata/selection.py
@@ -181,6 +181,10 @@ def sortSelected(self, ordering):
         vd.status('undo added')
         vd.addUndo(setattr, self, 'rows', copy(self.rows))
 
+# temporary:  save data to test integrity after the sort
+    rows_initial = { self.rowid(r) for r in self.rows }
+    selected_initial = set(self._selectedRows.keys())
+
     with Progress(gerund='sorting', total=self.nSelectedRows) as prog:
         selected = list(self._selectedRows.values())
         selected_rowids = set(self._selectedRows.keys())
@@ -201,6 +205,12 @@ def sortSelected(self, ordering):
             vd.exceptionCaught(e, status=False)
         for selected_idx, r in zip(selected_indices, sorted_selected, strict=True):
             self.rows[selected_idx] = r
+
+# temporary:  make sure we haven't lost any rows in the sheet or the selection
+    rows_final = { self.rowid(r) for r in self.rows }
+    selected_final = set(self._selectedRows.keys())
+    assert(rows_initial == rows_final)
+    assert(selected_initial == selected_final)
 
 Sheet.addCommand('t', 'stoggle-row', 'toggle_row(cursorRow); cursorDown(1)', 'toggle selection of current row')
 Sheet.addCommand('s', 'select-row', 'select_row(cursorRow); cursorDown(1)', 'select current row')
