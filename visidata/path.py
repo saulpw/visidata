bfeb99966e25fce995b237f32e8d50f9d7171031
midichef <67946319+midichef@users.noreply.github.com>
2024-04-20 23:36:11 -0700

    [misc-] fix minor errors in symbol imports/references

--- visidata/path.py
+++ visidata/path.py
@@ -229,7 +229,7 @@ class Path(os.PathLike):
         return self._path.__fspath__()
 
     def __lt__(self, a):
-        if isinstance(a, visidata.Path):
+        if isinstance(a, Path):
             return self._path.__lt__(a._path)
         return self._path.__lt__(a)
 
