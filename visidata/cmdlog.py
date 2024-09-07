saulpw/visidata

commit f7f24d784f53efd06cc1acf18f4c82897a7cb183

[undo-] cmdlog always use rowidx, not rowkey #2246

visidata/cmdlog.py
@@ -125,8 +125,7 @@ def commandCursor(sheet, execstr):
     colname, rowname = '', ''
     contains = lambda s, *substrs: any((a in s) for a in substrs)
     if contains(execstr, 'cursorTypedValue', 'cursorDisplay', 'cursorValue', 'cursorCell', 'cursorRow') and sheet.nRows > 0:
-        k = sheet.rowkey(sheet.cursorRow)
-        rowname = keystr(k) if k else sheet.cursorRowIndex
+        rowname = sheet.cursorRowIndex
 
     if contains(execstr, 'cursorTypedValue', 'cursorDisplay', 'cursorValue', 'cursorCell', 'cursorCol', 'cursorVisibleCol', 'ColumnAtCursor'):
         if sheet.cursorCol:

commit db75a25ff8c64a47c730f0fcdb14ce66239fc94b

[undo-] fix row input vars
error introduced in f7f24d784f53efd06cc1acf18f4c82897a7cb183

visidata/cmdlog.py
@@ -94,17 +94,17 @@ def moveToRow(vs, rowstr):
     return True
 
 @Sheet.api
-def getRowIndexFromStr(vs, row:str):
+def getRowIndexFromStr(vs, row):
     prefix = vd.options.rowkey_prefix
     index = None
     if isinstance(row, int):
         index = row
     elif isinstance(row, str) and row.startswith(prefix):
         rowk = row[len(prefix):]
-        index = indexMatch(vs.rows, lambda r,vs=vs,rowstr=rowstr: rowk == ','.join(map(str, vs.rowkey(r))))
+        index = indexMatch(vs.rows, lambda r,vs=vs,rowk=rowk: rowk == ','.join(map(str, vs.rowkey(r))))
     else:
         try:
-            index = int(rowstr)
+            index = int(row)
         except ValueError:
             vd.warning('invalid type for row index')
 