9ac0a78cd95c188be3da250968723aca2eb7c4b6
midichef <67946319+midichef@users.noreply.github.com>
2024-09-13 11:56:40 -0700

    [help-] widen command description column

--- visidata/help.py
+++ visidata/help.py
@@ -46,7 +46,7 @@ class HelpSheet(MetaSheet):
         Column('menupath', width=0, cache=True, getter=lambda col,row: vd.menuPathsByLongname.get(row.longname, None)),
         Column('keystrokes', getter=lambda col,row: col.sheet.revbinds.get(row.longname, [None])[0]),
         Column('all_bindings', width=0, cache=True, getter=lambda col,row: list(set(col.sheet.revbinds.get(row.longname, [])))),
-        Column('description', width=40, getter=lambda col,row: col.sheet.cmddict[(row.sheet, row.longname)].helpstr),
+        Column('description', width=70, getter=lambda col,row: col.sheet.cmddict[(row.sheet, row.longname)].helpstr),
         ColumnAttr('execstr', width=0),
         Column('logged', width=0, getter=lambda col,row: vd.isLoggableCommand(row)),
     ]
