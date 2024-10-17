36c16606f27a6eb29bd8104fc3f7d1b32883d74b
midichef <67946319+midichef@users.noreply.github.com>
2024-10-17 02:08:36 -0700

    [features-] move reload-rows from BaseSheet to Sheet

--- visidata/features/reload_every.py
+++ visidata/features/reload_every.py
@@ -49,7 +49,7 @@ def reload_rows(self):
 
 BaseSheet.addCommand('', 'reload-every', 'sheet.reload_every(input("reload interval (sec): ", value=1))', 'schedule sheet reload every N seconds') #683
 BaseSheet.addCommand('', 'reload-modified', 'sheet.reload_modified()', 'reload sheet when source file modified (tail-like behavior)')  #1686
-BaseSheet.addCommand('z^R', 'reload-rows', 'preloadHook(); reload_rows(); status("reloaded")', 'Reload current sheet')
+Sheet.addCommand('z^R', 'reload-rows', 'preloadHook(); reload_rows(); status("reloaded")', 'Reload current sheet')
 
 vd.addMenuItems('''
     File > Reload > rows only > reload-rows
