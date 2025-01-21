3ca3053cca66d4279240721dd9163a73d34a37ad
midichef <67946319+midichef@users.noreply.github.com>
2025-01-20 20:34:05 -0800

    [sort-] allow z[ and z] to reverse col sort dir or ignore col
    They now modify the sort order of one column, while preserving sort
    criteria for columns that had higher priority.

--- visidata/sort.py
+++ visidata/sort.py
@@ -2,8 +2,14 @@ from copy import copy
 from visidata import vd, asyncthread, Progress, Sheet, options, UNLOADED
 
 @Sheet.api
-def orderBy(sheet, *cols, reverse=False):
-    'Add *cols* to internal ordering and re-sort the rows accordingly.  Pass *reverse* as True to order these *cols* descending.  Pass empty *cols* (or cols[0] of None) to clear internal ordering.'
+def orderBy(sheet, *cols, reverse=False, change_column=False):
+    '''Add *cols* to internal ordering and re-sort the rows accordingly.
+    Pass *reverse* as True to order these *cols* descending.
+    Pass empty *cols* (or cols[0] of None) to clear internal ordering.
+    Set *change_column* to True to change the sort status of a single column: add/remove/invert it.
+    *cols* must have length 1. Sort columns that had higher priority are unchanged. Lower-priority columns are removed.
+    If *change_column* is False, *cols* will add to the existing ordering columns.'''
+
     if options.undo:
         vd.addUndo(setattr, sheet, '_ordering', copy(sheet._ordering))
         if sheet._ordering:
@@ -17,9 +23,16 @@ def orderBy(sheet, *cols, reverse=False):
         cols = cols[1:]
         do_sort = True
 
-    for c in cols:
-        sheet._ordering.append((c, reverse))
+    if change_column:
+        if len(cols) > 1:
+            vd.fail('sort order edit must only be applied to a single column')
+        new_ordering = edit_ordering(sheet._ordering, cols[0], reverse)
+        sheet._ordering = new_ordering
         do_sort = True
+    else:
+        for c in cols:
+            sheet._ordering.append((c, reverse))
+            do_sort = True
 
     if do_sort:
         sheet.sort()
@@ -35,6 +48,26 @@ class Reversor:
         return other.obj < self.obj
 
 
+def edit_ordering(ordering, col, reverse):
+    '''Return a modified ordering based on editing a single column *col*:   add it, remove it, or flip its direction.
+    Columns after *col* in the ordering (with lower sort priority) are also removed from the ordering.
+    *ordering* is a list of tuples:  (Column, boolean), where the boolean defines the sort direction.
+    '''
+    new_ordering = []
+    # handle changes to status of columns that are already in the ordering:  add/remove/flip
+    changed = False
+    for c, old_reverse in ordering:
+        if c == col:
+            if reverse != old_reverse: # reverse the column's sort direction
+                new_ordering.append((c, reverse))
+            # if the sort direction is unchanged, remove the column from the ordering
+            changed = True
+            # columns after the edited column will be dropped from the ordering
+            break
+        new_ordering.append((c, old_reverse))
+    if not changed:
+        new_ordering.append((col, reverse))
+    return new_ordering
 
 @Sheet.cached_property
 def ordering(sheet) -> 'list[tuple[Column, bool]]':
@@ -84,8 +117,10 @@ Sheet.addCommand('g[', 'sort-keys-asc', 'orderBy(None, *keyCols)', 'sort ascendi
 Sheet.addCommand('g]', 'sort-keys-desc', 'orderBy(None, *keyCols, reverse=True)', 'sort descending by all key columns; replace any existing sort criteria')
 
 # add to existing sort criteria
-Sheet.addCommand('z[', 'sort-asc-add', 'orderBy(cursorCol)', 'sort ascending by current column; add to existing sort criteria')
-Sheet.addCommand('z]', 'sort-desc-add', 'orderBy(cursorCol, reverse=True)', 'sort descending by current column; add to existing sort criteria')
+Sheet.addCommand('', 'sort-asc-add', 'orderBy(cursorCol)', 'sort ascending by current column; add to existing sort criteria')
+Sheet.addCommand('', 'sort-desc-add', 'orderBy(cursorCol, reverse=True)', 'sort descending by current column; add to existing sort criteria')
+Sheet.addCommand('z[', 'sort-asc-change', 'orderBy(cursorCol, change_column=True)', 'sort ascending by current column; keep higher priority sort criteria')
+Sheet.addCommand('z]', 'sort-desc-change', 'orderBy(cursorCol, reverse=True, change_column=True)', 'sort descending by current column; keep higher priority sort criteria')
 Sheet.addCommand('gz[', 'sort-keys-asc-add', 'orderBy(*keyCols)', 'sort ascending by all key columns; add to existing sort criteria')
 Sheet.addCommand('gz]', 'sort-keys-desc-add', 'orderBy(*keyCols, reverse=True)', 'sort descending by all key columns; add to existing sort criteria')
 
