50f08cda15756759aa0efc8efd9e334c17c4077b
anjakefala <anja.kefala@gmail.com>
2025-06-15 14:56:18 -0700

    [dev] bump visidata to v3.3dev

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def all_requirements():
 
 # tox can't actually run python3 setup.py: https://github.com/tox-dev/tox/issues/96
 # from visidata import __version__
-__version__ = "3.2"
+__version__ = "3.3dev"
 install_requires = [
     "python-dateutil",
     'importlib_resources; python_version<"3.9"',
