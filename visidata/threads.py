1382c9a1a5c9721ba9b4240b9428bc53ecb1c42b
midichef <67946319+midichef@users.noreply.github.com>
2025-06-26 18:36:24 -0700

    [threads-] remove unnecessary subtraction of empty set

--- visidata/threads.py
+++ visidata/threads.py
@@ -356,7 +356,6 @@ def sync(self, *joiningThreads):
         deads = set()  # dead threads
         threads = joiningThreads or set(self.unfinishedThreads)
         threads -= set([threading.current_thread(), getattr(vd, 'drawThread', None), getattr(vd, 'outputProgressThread', None)])
-        threads -= deads
         threads -= set([None])
         for t in threads:
             try:
