23c5851bd610545c1c220efe4a9bdcedd9a01bbb
midichef <67946319+midichef@users.noreply.github.com>
2025-07-17 19:27:21 -0700

    [sheets-] remove calcColLayout from cursorRight
    it is redundant now that cursorCheck runs calcColLayout

--- visidata/sheets.py
+++ visidata/sheets.py
@@ -560,7 +560,6 @@ class TableSheet(BaseSheet):
     def cursorRight(self, n=1):
         'Move cursor right `n` visible columns (or left if `n` is negative).'
         self.cursorVisibleColIndex += n
-        self.calcColLayout()
 
     def addColumn(self, *cols, index=None):
         '''Insert all *cols* into columns at *index*, or append to end of columns if *index* is None.
