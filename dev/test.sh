afbaa1b41f6d6c7f1610eca8aef1552b39fe511d
Saul Pwanson <code@saul.pw>
2025-11-30 07:51:49 +0000

    no space for issue number comments

--- dev/test.sh
+++ dev/test.sh
@@ -7,7 +7,7 @@ shopt -s failglob
 
 trap "echo aborted; exit;" SIGINT SIGTERM
 
-export LC_NUMERIC="en_US.UTF-8" # 2867
+export LC_NUMERIC="en_US.UTF-8" #2867
 export LC_TIME="en_US.UTF-8"
 
 MAX_PARALLEL_JOBS=1
