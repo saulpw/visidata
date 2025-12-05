e033651f7a8e656f074d8b38cb6a6d6461faa8ec
Saul Pwanson <code@saul.pw>
2025-12-05 14:28:33 -0800

    [input] convert internal keynames to prettykeys

--- visidata/_input.py
+++ visidata/_input.py
@@ -254,29 +254,29 @@ class InputWidget:
         v = self.value
 
         if ch == '':                               return False
-        elif ch == 'KEY_IC':                       self.insert_mode = not self.insert_mode
-        elif ch == '^A' or ch == 'KEY_HOME':       i = 0
-        elif ch == '^B' or ch == 'KEY_LEFT':       i -= 1
-        elif ch in ('^C', '^Q', '^['):             raise EscapeException(ch)
-        elif ch == '^D' or ch == 'KEY_DC':         v = delchar(v, i)
-        elif ch == '^E' or ch == 'KEY_END':        i = len(v)
-        elif ch == '^F' or ch == 'KEY_RIGHT':      i += 1
-        elif ch == '^G':
+        elif ch == 'Ins':                          self.insert_mode = not self.insert_mode
+        elif ch == 'Ctrl+A' or ch == 'Home':       i = 0
+        elif ch == 'Ctrl+B' or ch == 'Left':       i -= 1
+        elif ch in ('Ctrl+C', 'Ctrl+Q', 'Ctrl+['): raise EscapeException(ch)
+        elif ch == 'Ctrl+D' or ch == 'Del':        v = delchar(v, i)
+        elif ch == 'Ctrl+E' or ch == 'End':        i = len(v)
+        elif ch == 'Ctrl+F' or ch == 'Right':      i += 1
+        elif ch == 'Ctrl+G':
             vd.cycleSidebar()
             return False # not considered a first keypress
-        elif ch in ('^H', 'KEY_BACKSPACE', '^?'):  i -= 1; v = delchar(v, i)
-        elif ch == '^I':                           v, i = self.completion(v, i, +1)
-        elif ch == 'KEY_BTAB':                     v, i = self.completion(v, i, -1)
-        elif ch in ['^J', '^M']:                   return True # ENTER to accept value
-        elif ch == '^K':                           v = v[:i]  # ^Kill to end-of-line
-        elif ch == '^N':
+        elif ch in ('Ctrl+H', 'Bksp', 'Ctrl+?'):   i -= 1; v = delchar(v, i)
+        elif ch == 'Tab':                       v, i = self.completion(v, i, +1)
+        elif ch == 'Shift+Tab':                     v, i = self.completion(v, i, -1)
+        elif ch == 'Enter':                        return True # ENTER to accept value
+        elif ch == 'Ctrl+K':                       v = v[:i]  # Ctrl+Kill to end-of-line
+        elif ch == 'Ctrl+N':
             c = ''
             while not c:
                 c = vd.getkeystroke(scr)
             c = vd.prettykeys(c)
             i += len(c)
             v += c
-        elif ch == '^O':
+        elif ch == 'Ctrl+O':
             edit_v = vd.launchExternalEditor(v)
             if self.value == edit_v:
                 # leave cell unmodified when the editor exits with no change
@@ -284,14 +284,14 @@ class InputWidget:
             else:
                 self.value = edit_v
                 return True
-        elif ch == '^R':                           v = self.orig_value  # ^Reload initial value
-        elif ch == '^T':                           v = delchar(splice(v, i-2, v[i-1:i]), i)  # swap chars
-        elif ch == '^U':                           v = v[i:]; i = 0  # clear to beginning
-        elif ch == '^V':                           v = splice(v, i, until_get_wch(scr)); i += 1  # literal character
-        elif ch == '^W':                           j = find_word(v, 0, i-1, -1); v = v[:j+1] + v[i:]; i = j+1  # erase word
+        elif ch == 'Ctrl+R':                           v = self.orig_value  # Ctrl+Reload initial value
+        elif ch == 'Ctrl+T':                           v = delchar(splice(v, i-2, v[i-1:i]), i)  # swap chars
+        elif ch == 'Ctrl+U':                           v = v[i:]; i = 0  # clear to beginning
+        elif ch == 'Ctrl+V':                           v = splice(v, i, until_get_wch(scr)); i += 1  # literal character
+        elif ch == 'Ctrl+W':                           j = find_word(v, 0, i-1, -1); v = v[:j+1] + v[i:]; i = j+1  # erase word
         elif ch in ('KEY_DC5','kDC5','kDC3'):      j = find_word(v, i, len(v), +1); v = v[:i] + v[j+1:]  # erase word forward
-        elif ch == '^Y':                           v = splice(v, i, str(vd.memory.clipval))
-        elif ch == '^Z':                           vd.suspend()
+        elif ch == 'Ctrl+Y':                           v = splice(v, i, str(vd.memory.clipval))
+        elif ch == 'Ctrl+Z':                           vd.suspend()
         # CTRL+arrow
         elif ch == 'kLFT5':                        i = find_word(v, 0, i-1, -1)+1;  # word left
         elif ch == 'kRIT5':                        i = find_word(v, i, len(v)-1, +1);  # word right
@@ -510,12 +510,10 @@ def inputMultiple(vd, updater=lambda val: None, record=True, **kwargs):
                                              updater=_drawPrompt,
                                              record=False,
                                              bindings={
-                'KEY_BTAB': change_input(-1),
-                '^I':       change_input(+1),
-                'KEY_SR':   change_input(-1),
-                'KEY_SF':   change_input(+1),
-                'kUP':      change_input(-1),
-                'kDN':      change_input(+1),
+                'Shift+Tab':   change_input(-1),
+                'Tab':     change_input(+1),
+                'Shift+Up':   change_input(-1),
+                'Shift+Down': change_input(+1),
             })
             break
         except ChangeInput as e:
@@ -661,21 +659,19 @@ def editCell(self, vcolidx=None, rowidx=None, value=None, **kwargs):
         value = value or col.getDisplayValue(self.rows[self.cursorRowIndex])
 
     bindings={
-        'kUP':        acceptThenFunc('go-up', 'rename-col' if rowidx < 0 else 'edit-cell'),
-        'KEY_SR':     acceptThenFunc('go-up', 'rename-col' if rowidx < 0 else 'edit-cell'),
-        'kDN':        acceptThenFunc('go-down', 'rename-col' if rowidx < 0 else 'edit-cell'),
-        'KEY_SF':     acceptThenFunc('go-down', 'rename-col' if rowidx < 0 else 'edit-cell'),
-        'KEY_SRIGHT': acceptThenFunc('go-right', 'rename-col' if rowidx < 0 else 'edit-cell'),
-        'KEY_SLEFT':  acceptThenFunc('go-left', 'rename-col' if rowidx < 0 else 'edit-cell'),
-        '^I':         acceptThenFunc('go-right', 'rename-col' if rowidx < 0 else 'edit-cell'),
-        'KEY_BTAB':   acceptThenFunc('go-left', 'rename-col' if rowidx < 0 else 'edit-cell'),
+        'Shift+Up':     acceptThenFunc('go-up', 'rename-col' if rowidx < 0 else 'edit-cell'),
+        'Shift+Down':     acceptThenFunc('go-down', 'rename-col' if rowidx < 0 else 'edit-cell'),
+        'Shift+Right': acceptThenFunc('go-right', 'rename-col' if rowidx < 0 else 'edit-cell'),
+        'Shift+Left':  acceptThenFunc('go-left', 'rename-col' if rowidx < 0 else 'edit-cell'),
+        'Tab':         acceptThenFunc('go-right', 'rename-col' if rowidx < 0 else 'edit-cell'),
+        'Shift+Tab':   acceptThenFunc('go-left', 'rename-col' if rowidx < 0 else 'edit-cell'),
     }
 
     if vcolidx == self.nVisibleCols-1 or vcolidx >= self.nCols-1:
-        bindings['^I'] = acceptThenFunc('go-down', 'go-leftmost', 'edit-cell')
+        bindings['Tab'] = acceptThenFunc('go-down', 'go-leftmost', 'edit-cell')
 
     if vcolidx <= 0:
-        bindings['KEY_BTAB'] = acceptThenFunc('go-up', 'go-rightmost', 'edit-cell')
+        bindings['Shift+Tab'] = acceptThenFunc('go-up', 'go-rightmost', 'edit-cell')
 
     # update local bindings with kwargs.bindings instead of the inverse, to preserve kwargs.bindings for caller
     bindings.update(kwargs.get('bindings', {}))
--- visidata/tests/test_edittext.py
+++ visidata/tests/test_edittext.py
@@ -11,36 +11,36 @@ class TestEditText:
         visidata.vd.getkeystroke = Mock(side_effect=self.chars)
 
     @pytest.mark.parametrize('keys, result, kwargs', [
-        ('^J', '', {}),
-        ('a b KEY_HOME c d ^A e f ^J', 'efcdab', {}),
-        ('a b KEY_LEFT 1 KEY_LEFT KEY_LEFT KEY_LEFT 2 ^J', '2a1b', {}), # Left, past home
-        ('a b ^C', None, dict(exception=visidata.EscapeException)),
-        ('a b ^[', None, dict(exception=visidata.EscapeException)),
-        ('a KEY_DC ^J', 'a', {}),
-        ('a b KEY_LEFT KEY_DC ^J', 'a', {}),
-        ('a b KEY_LEFT c KEY_END d ^J', 'acbd', {}),
-        ('a b KEY_HOME KEY_RIGHT c ^J', 'acb', {}),
-        ('a b KEY_BACKSPACE c ^J', 'ac', {}),
+        ('Enter', '', {}),
+        ('a b Home c d Ctrl+A e f Enter', 'efcdab', {}),
+        ('a b Left 1 Left Left Left 2 Enter', '2a1b', {}), # Left, past home
+        ('a b Ctrl+C', None, dict(exception=visidata.EscapeException)),
+        ('a b Ctrl+[', None, dict(exception=visidata.EscapeException)),
+        ('a Del Enter', 'a', {}),
+        ('a b Left Del Enter', 'a', {}),
+        ('a b Left c End d Enter', 'acbd', {}),
+        ('a b Home Right c Enter', 'acb', {}),
+        ('a b Bksp c Enter', 'ac', {}),
 
         # Backspace no longer deletes the first character at the start
-        ('a b KEY_HOME KEY_BACKSPACE c ^J', 'cab', {}),
+        ('a b Home Bksp c Enter', 'cab', {}),
 
         # Backspace works in different combos, including on the mac.
-        ('a b c KEY_BACKSPACE ^H KEY_LEFT KEY_DC ^J', '', {}),
+        ('a b c Bksp Ctrl+H Left Del Enter', '', {}),
 
-        ('a b c ^B ^B ^K ^J', 'a', {}),
+        ('a b c Ctrl+B Ctrl+B Ctrl+K Enter', 'a', {}),
 
-        ('a ^R ^J', '', {}),
-        ('a ^R ^J', 'foo', dict(value='foo')),
+        ('a Ctrl+R Enter', '', {}),
+        ('a Ctrl+R Enter', 'foo', dict(value='foo')),
 
         # Two characters swaps characters
-        ('a b ^T ^J', 'ba', {}),
+        ('a b Ctrl+T Enter', 'ba', {}),
 
         # Home with multiple characters acts like delete
-        ('a b KEY_HOME ^T ^J', 'b', {}),
+        ('a b Home Ctrl+T Enter', 'b', {}),
 
-        ('a b KEY_LEFT ^U ^J', 'b', {}),
-        ('a b ^U c ^J', 'c', {}),
+        ('a b Left Ctrl+U Enter', 'b', {}),
+        ('a b Ctrl+U c Enter', 'c', {}),
     ])
     def test_keys(self, mock_screen, keys, result, kwargs):
         self.chars.extend(keys.split())
