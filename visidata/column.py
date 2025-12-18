aacae39bd5704382c77ddb4905093e523dbddf42
midichef <67946319+midichef@users.noreply.github.com>
2025-12-18 12:48:20 -0800

    [hlsearch-] move highlight code to features/

--- visidata/column.py
+++ visidata/column.py
@@ -80,7 +80,6 @@ class Column(Extensible):
         self.displayer = ''
         self.defer = False
         self.disp_expert = 0    # do not show if 'nometacols' in options.disp_help_flags
-        self.highlight_regex = None
 
         self.setCache(cache)
         for k, v in kwargs.items():
