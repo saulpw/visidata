ed32f017b00b09526bffcfb1116b3a8327a9cb36
Saul Pwanson <code@saul.pw>
2025-12-30 14:52:44 -0800

    [open-row-] allow pandas to open row as sheet #2925

--- visidata/pyobj.py
+++ visidata/pyobj.py
@@ -310,7 +310,7 @@ Sheet.addCommand('zCtrl+Y', 'pyobj-cell', 'status(type(cursorValue).__name__); v
 BaseSheet.addCommand('gCtrl+Y', 'pyobj-sheet', 'status(type(sheet).__name__); vd.push(PyobjSheet(sheet.name+"_sheet", source=sheet))', 'open current sheet as Python object')
 
 Sheet.addCommand('', 'open-row-basic', 'vd.push(TableSheet.openRow(sheet, cursorRow))', 'dive into current row as basic table (ignoring subsheet dive)')
-Sheet.addCommand('Enter', 'open-row', 'vd.push(openRow(cursorRow)) if cursorRow else vd.fail("no row to open")', 'open current row with sheet-specific dive')
+Sheet.addCommand('Enter', 'open-row', 'vd.push(openRow(cursorRow)) if cursorRow is not None else vd.fail("no row to open")', 'open current row with sheet-specific dive')
 Sheet.addCommand('zEnter', 'open-cell', 'vd.push(openCell(cursorCol, cursorRow))', 'open sheet with copies of rows referenced in current cell')
 openRows
 Sheet.addCommand('gEnter', 'dive-selected', 'openRows(selectedRows)', 'open all selected rows')
