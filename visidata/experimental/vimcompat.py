2379f4d2be296d45bd422c6e4217902a130dedbd
Saul Pwanson <code@saul.pw>
2026-01-06 14:10:03 -0800

    [movement] move go-page-half bindings to vimcompat #2927

--- visidata/experimental/vimcompat.py
+++ visidata/experimental/vimcompat.py
@@ -7,3 +7,7 @@ from visidata import BaseSheet
 
 # Jump to first row and column with 'go'
 BaseSheet.bindkey('go', 'go-home')
+
+# standard half-page movement
+BaseSheet.bindkey('Ctrl+D', 'go-pagedown-half')
+BaseSheet.bindkey('Ctrl+U', 'go-pageup-half')
