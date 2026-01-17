c1eb88e99fe837886364cbd81d20b82a05bba3eb
midichef <67946319+midichef@users.noreply.github.com>
2026-01-16 18:37:10 -0800

    [palette-] align choices with prompt before Tab is pressed

--- visidata/features/go_col.py
+++ visidata/features/go_col.py
@@ -32,7 +32,7 @@ def nextColName(sheet, show_cells=True):
     def _fmt_colname(match, row, trigger_key):
         name = match.formatted.get('name', row.name) if match else row.name
         r = ' '*(dispwidth(prompt)-3)
-        r += f'[:keystrokes]{trigger_key}[/]  '
+        r += f'[:keystrokes]{trigger_key}[/]  ' if trigger_key else '   '
         if show_cells and len(sheet.rows) > 0:
             # pad the right side with spaces
             # use row.name, because name from match contains
