6d0683135e6b471b261d84dfc34ca92995d548d7
Saul Pwanson <code@saul.pw>
2026-01-21 15:52:11 -0800

    [xlsx-] add hlink/folHLink to color list #2948

--- visidata/loaders/xlsx.py
+++ visidata/loaders/xlsx.py
@@ -258,7 +258,7 @@ def theme_colors(sheet):
 
     theme_colors = []
 
-    for c in ['lt1', 'dk1', 'lt2', 'dk2', 'accent1', 'accent2', 'accent3', 'accent4', 'accent5', 'accent6']:
+    for c in ['lt1', 'dk1', 'lt2', 'dk2', 'accent1', 'accent2', 'accent3', 'accent4', 'accent5', 'accent6', 'hlink', 'folHlink']:
         accent = firstColorScheme.find(QName(xlmns, c).text)
         for i in list(accent): # walk all child nodes, rather than assuming [0]
             if 'window' in i.attrib['val']:
