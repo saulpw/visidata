e0dd4380733870d82d80de90576442bc561985eb
midichef <67946319+midichef@users.noreply.github.com>
2026-05-29 07:25:06 -0700

    [statusbar-] remove leading space in helpstr

--- visidata/statusbar.py
+++ visidata/statusbar.py
@@ -25,7 +25,7 @@ vd.theme_option('color_status', 'bold on 238', 'status line color')
 vd.theme_option('color_error', '202 1', 'error message color')
 vd.theme_option('color_warning', '166 15', 'warning message color')
 vd.theme_option('color_top_status', 'underline', 'top window status bar color')
-vd.theme_option('color_active_status', 'black on 68 blue', ' active window status bar color')
+vd.theme_option('color_active_status', 'black on 68 blue', 'active window status bar color')
 vd.theme_option('color_inactive_status', '8 on black', 'inactive window status bar color')
 vd.theme_option('color_highlight_status', 'black on green', 'color of highlighted elements in statusbar')
 
