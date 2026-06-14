62ccf7fe72094e68e89bf4d559f5f0414625b133
midichef <67946319+midichef@users.noreply.github.com>
2026-06-14 08:33:16 -0700

    [expand-] remove nonexistent sortcols after contract-cols

--- visidata/features/expand_cols.py
+++ visidata/features/expand_cols.py
@@ -146,6 +146,7 @@ def contract_cols(sheet, cols, depth=1):  # depth == 0 means contract all the wa
             col.width = sheet.options.default_width
 
         sheet.columns = [col for col in sheet.columns if getattr(col, 'origCol', None) not in origCols]
+    sheet.validate_sortcols()
 
 
 @Sheet.api
