db6f323a29faed5545f0c6dc585094015e4df667
Saul Pwanson <code@saul.pw>
2026-06-22 22:15:03 -0700

    slight simplification

--- visidata/sidebar.py
+++ visidata/sidebar.py
@@ -1,11 +1,9 @@
-from typing import Optional, Union, Callable, TYPE_CHECKING
+from typing import Optional, Union, Callable
 import textwrap
 
 from visidata import vd, VisiData, BaseSheet, colors, TextSheet, clipdraw, wraptext, dispwidth, AttrDict, wrmap, ColorAttr
 from visidata import CommandHelpGetter, OptionHelpGetter
-
-if TYPE_CHECKING:
-    from visidata.help import HelpPane
+from visidata.help import HelpPane
 
 
 vd.option('disp_sidebar', True, 'whether to display sidebar')
