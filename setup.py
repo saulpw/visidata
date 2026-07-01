a4c4a72c84807fd12118103ddafef1c72c01ba8d
anjakefala <anja.kefala@gmail.com>
2026-06-30 21:59:22 -0700

    [dev] bump to v3.5dev

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def all_requirements():
 
 # tox can't actually run python3 setup.py: https://github.com/tox-dev/tox/issues/96
 # from visidata import __version__
-__version__ = "3.4"
+__version__ = "3.5dev"
 install_requires = [
     "python-dateutil",
     'importlib_resources; python_version<"3.9"',
